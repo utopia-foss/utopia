8ea065a4e299bede0eafc5204eedb372a38d46da
root <mail@lukasriedel.com>
2016-10-05 23:09:12 +0000

    added preliminary cell test

---
+++ dune/citcat/test/cell_test.hh
@@ -0,0 +1,10 @@
+#include <cassert>
+
+template<typename Cell, typename Position, typename Index>
+void assert_cell_members (const Cell& c, const Position& pos, const Index& id, const bool boundary)
+{
+	for(std::size_t i=0; i<pos.size(); i++)
+		assert(c.position()[i]==pos[i]);
+	assert(c.index()==id);
+	assert(c.boundary()==boundary);
+}
--- dune/citcat/test/entity_test.cc
+++ dune/citcat/test/entity_test.cc
@@ -1,24 +1,8 @@
-#include <cassert>
 #include <random>
 #include <dune/citcat/citcat.hh>
 #include <dune/common/exceptions.hh>
 
-/** \param e Entity to test
- *  \param s State
- *  \param s_n New State
- *  \param t Traits
- *  \param t_n New Traits
- *  \param tag Tag
- */
-template<typename Entity, typename State, typename Traits>
-void assert_entity_members (Entity& e, const State& s, const State& s_n, const Traits& t, const Traits& t_n, const int tag)
-{
-	assert(e.state()==s);
-	assert(e.new_state()==s_n);
-	assert(e.traits()==t);
-	assert(e.new_traits()==t_n);
-	assert(e.tag()==tag);
-}
+#include "entity_test.hh"
 
 /// Choose random states and traits. Verify Entity members before and after update
 int main(int argc, char const *argv[])
