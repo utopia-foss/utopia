134d0ac2ec1b3241ae9610ebc82bf633ed9d4ed6
Lukas Riedel <mail@lukasriedel.com>
2018-03-05 18:08:25 +0100

    Fix bug in tags_test.cc

--- dune/utopia/test/tags_test.cc
+++ dune/utopia/test/tags_test.cc
@@ -8,7 +8,7 @@ int main(int args, char** argv)
         Utopia::DefaultTag t_true(true);
         assert(t_true.is_tagged);
         t_true.is_tagged = false;
-        assert(!t_true.is_tagged());
+        assert(!t_true.is_tagged);
         return 0;
     }
     catch(...){
