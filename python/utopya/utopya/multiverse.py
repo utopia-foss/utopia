04a223141ef396d92099630dc5afa2132fa458ec
H_Leusmann <hleusma@iup.uni-heidelberg.de>
2018-03-05 16:20:38 +0100

    Changed default base directory for output to home
    Additional added some comments

--- python/utopya/utopya/multiverse.py
+++ python/utopya/utopya/multiverse.py
@@ -11,7 +11,8 @@ class Multiverse:
     def __init__(self):
         self.path_dict = None
         # read in the dictionary above with #14, base from user specific file #15
-        self.simulation_path = None
+        # {base : absolute path (default is home directory), out_dir : , ....}
+        self.path_simulation = None
         self._create_sim_dir()
 
     def _create_sim_dir(self):
@@ -21,19 +22,24 @@ class Multiverse:
         if not they are created. If the exact same folder tree (with timestamp)
         already exists a Error is thrown.
         """
-        self.path_dict = dict(base='./', out_dir='utopia_output', model_name='', model_note='test')
+        self.path_dict = dict(base=None, out_dir='utopia_output', model_name='', model_note='test')
 
         # use recursive makedirs
-        self.simulation_path = self.path_dict['base']+"/"+self.path_dict['out_dir']+"/"+self.path_dict['model_name']
-        self.simulation_path += "./"+time.strftime("%Y")+time.strftime("%m")+time.strftime("%d")+"_"+time.strftime("%H")+time.strftime("%M")+time.strftime("%S")
-        self.simulation_path += self.path_dict['model_note']
+        if self.path_dict['base'] is None:
+            self.path_simulation = os.path.expanduser("~")
+        else:
+            self.path_simulation = self.path_dict['base']
+        self.path_simulation += "/"+self.path_dict['out_dir']+"/"+self.path_dict['model_name']
+        self.path_simulation += "./"+time.strftime("%Y")+time.strftime("%m")+time.strftime("%d")+"_"+time.strftime("%H")+time.strftime("%M")+time.strftime("%S")
+        if self.path_dict['model_note']:
+            self.path_simulation += "_"+self.path_dict['model_note']
         # recursive folder creation automatic error if already existing
-        os.makedirs(self.simulation_path)
+        os.makedirs(self.path_simulation)
         # make Subfolders
         # folder list expandable or configured by cfg yaml ?
         folder_list = ["config", "eval", "universes"]  
         for folder in folder_list:
-            os.mkdir(self.simulation_path+"/"+folder)
+            os.mkdir(self.path_simulation+"/"+folder)
 
     def _create_uni_dir(self, uni_no, max_dim_no):
         """ The _create_uni_dir generates the folder for a single univers.
@@ -42,8 +48,8 @@ class Multiverse:
         for the given universe number, preceding zeros are added.
         Thus they are sortable.
         """
-        universe_path = "uni"+str(uni_no).zfill(max_dim_no)
-        pathname = self.simulation_path+"universes"+universe_path
+        path_universe = "uni"+str(uni_no).zfill(max_dim_no)
+        pathname = self.path_simulation+"/universes/"+path_universe
         # recursive folder creation automatic error if already existing
         os.makedirs(pathname)
 
