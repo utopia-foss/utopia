e3975815a8818862c6e10e786fccb0772b882772
H_Leusmann <hleusma@iup.uni-heidelberg.de>
2018-03-05 16:52:51 +0100

    Improved Docstrings, added Args

--- python/utopya/utopya/multiverse.py
+++ python/utopya/utopya/multiverse.py
@@ -9,6 +9,12 @@ import time
 
 class Multiverse:
     def __init__(self):
+        """ Initialisation of the Multiverse
+
+        The __init__ reads out the yaml cfg files,
+        creates folders,
+        ...
+        """
         self.path_dict = None
         # read in the dictionary above with #14, base from user specific file #15
         # {base : absolute path (default is home directory), out_dir : , ....}
@@ -21,6 +27,20 @@ class Multiverse:
         The function checks, if the folders are already there,
         if not they are created. If the exact same folder tree (with timestamp)
         already exists a Error is thrown.
+
+        Folder Tree from Wiki
+        utopia_output/   # all utopia output should go here
+            model_a/
+            180301125410_my_first_sim/
+                config/
+                eval/
+                universes/
+                    uni000/
+                    uni001/
+                    ...
+            model_b/
+            180301125412_my_first_sim/
+            180301125413_my_second_sim/
         """
         self.path_dict = dict(base=None, out_dir='utopia_output', model_name='', model_note='test')
 
@@ -47,26 +67,14 @@ class Multiverse:
         Within the universes directory, create a subdirectory uni<###>
         for the given universe number, preceding zeros are added.
         Thus they are sortable.
+
+        Args:
+            uni_no: Number of the universe, created by the workermanager?,
+                restoreable from meta_cfg.yaml.
+            max_dim:no: Maximal number of universes calculated
+                from param sweep from meta_cfg.yaml.
         """
         path_universe = "uni"+str(uni_no).zfill(max_dim_no)
         pathname = os.path.join(self.path_simulation, "universes", path_universe)
         # recursive folder creation automatic error if already existing
         os.makedirs(pathname)
-
-'''
-Folder Tree from Wiki
-utopia_output/   # all utopia output should go here
-    model_a/
-       180301125410_my_first_sim/
-          config/
-          eval/
-          universes/
-             uni000/
-             uni001/
-             ...
-       180301125411_my_second_sim/
-          ...
-    model_b/
-       180301125412_my_first_sim/
-       180301125413_my_second_sim/
-#'''
