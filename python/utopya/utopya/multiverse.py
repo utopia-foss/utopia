e1a407d7dc049074f2178f3014afc2676a27a2ba
Fabian Krautgasser <fkrautga@iup.uni-heidelberg.de>
2018-03-06 10:25:10 +0100

    Outsourced reading yml files to tools.
    Moved the whole block of reading yml files and catching the FileNotFoundError into a function in tools. Callable via read_yml.
    + code cleanup. sorry for the long strings :D

--- python/utopya/utopya/multiverse.py
+++ python/utopya/utopya/multiverse.py
@@ -2,12 +2,11 @@
 
 The Multiverse supplies the main user interface of the frontend.
 """
-import yaml
-# from .tools import recursive_update, read_yaml
+from tools import recursive_update, read_yml
 
 
 class Multiverse:
-    def __init__(self, metaconfig="metaconfig.yml", userconfig=None):
+    def __init__(self, metaconfig: str="metaconfig.yml", userconfig: str=None):
         """Initialize the setup.
 
         Load default configuration file and adjust parameters given
@@ -23,36 +22,19 @@ class Multiverse:
         metaconfig is adjusted accordingly to create a single output file.
         """
         # read in all configuration files
-        # TODO: outsource the statements below into a function in tools?
-        try:
-            with open("default_metaconfig.yml", 'r') as ymlfile:
-                self._defaults = yaml.load(ymlfile)
+        self._defaults = read_yml("default_metaconfig.yml", error_msg="default_metaconfig.yml is not present.")
+        self._metaconfig = read_yml(metaconfig, error_msg="Path to metaconfig was given but metaconfig could not be found.")
 
-        except FileNotFoundError as err:
-            raise FileNotFoundError("default_metaconfig.yml is not present.") from err
-
-        try:
-            with open(metaconfig, 'r') as ymlfile:
-                self._metaconfig = yaml.load(ymlfile)
-
-        except FileNotFoundError:
-            raise FileNotFoundError("Path to metaconfig was given but\
-                                    metaconfig could not be found.")
-
-        if(userconfig is not None):
-            try:
-                with open(userconfig, 'r') as ymlfile:
-                    self._userconfig = yaml.load(ymlfile)
-            except FileNotFoundError:
-                raise FileNotFoundError("Path to userconfig was given but\
-                                        userconfig could not be found.")
+        if userconfig is not None:
+            self._userconfig = read_yml(userconfig, error_msg="Path to userconfig was given but userconfig could not be found.")
 
         # TODO: typechecks of values should be completed below here.
         # after this point it is assumed that all values are valid
 
-        if userconfig is not None:
-            self._defaults = self._recursive_update(self._defaults,
-                                                    self._userconfig)
+        if userconfig is not None:  # update default with user spec
+            self._defaults = recursive_update(self._defaults,
+                                              self._userconfig)
 
-        self._defaults = self._recursive_update(self._defaults,
-                                                self._metaconfig)
+        # update default_metaconfig with metaconfig
+        self._defaults = recursive_update(self._defaults,
+                                          self._metaconfig)
--- python/utopya/utopya/tools.py
+++ python/utopya/utopya/tools.py
@@ -2,7 +2,7 @@
 import yaml
 
 
-def recursive_update(self, d: dict, u: dict) -> dict:
+def recursive_update(d: dict, u: dict) -> dict:
     """Update dict d with values from dict u."""
     for key, val in u.items():
         if isinstance(val, dict):
@@ -11,4 +11,20 @@ def recursive_update(self, d: dict, u: dict) -> dict:
         else:
             # Not a mapping -> at leaf -> update value
             d[key] = val 	# ... which is just u[key]
+    return d
+
+
+def read_yml(path: str, error_msg: str=None) -> dict:
+    """Read yaml configuration file and return dict.
+
+    The given error_msg is given upon FileNotFoundError.
+    """
+    try:
+        with open(path, 'r') as ymlfile:
+            d = yaml.load(ymlfile)
+    except FileNotFoundError as err:
+        if error_msg:  # is None by default
+            raise FileNotFoundError(error_msg) from err
+        else:
+            raise err
     return d
