9563ea052d1ad7f7ebb7e77e8d3869f4e2c26151
Fabian Krautgasser <fkrautga@iup.uni-heidelberg.de>
2018-03-06 11:43:37 +0100

    Added 3 testfiles and tests for reading config files

--- python/utopya/test/test_multiverse.py
+++ python/utopya/test/test_multiverse.py
@@ -5,4 +5,13 @@ from utopya import Multiverse
 
 def test_init():
     """Tests the initialsation of the Multiverse."""
-    Multiverse()
+    Multiverse()  # fails, if neither default nor metaconfig are present
+    Multiverse(metaconfig="metaconfig.yml")
+    Multiverse(metaconfig="metaconfig.yml", userconfig="userconfig.yml")
+
+    # Testing errors
+    with pytest.raises(FileNotFoundError):
+        Multiverse(metaconfig="metaconfig2.yml")
+
+    with pytest.raises(FileNotFoundError):
+        Multiverse(userconfig="userconfig2.yml")
