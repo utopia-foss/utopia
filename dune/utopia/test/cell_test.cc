efb2e1b8b869b376e4250f172d7093f9f7f7c900
Lukas Siebert <lsiebert@iup.uni-heidelberg.de>
2018-03-06 11:26:33 +0100

    cleaned up the cell_test

--- dune/utopia/test/cell_test.cc
+++ dune/utopia/test/cell_test.cc
@@ -27,31 +27,37 @@ int main(int argc, char *argv[])
     try{
         Dune::MPIHelper::instance(argc, argv);
 
+        //some typedefinitions
         using State = int;
-        using Traits = double;
         using Position = Dune::FieldVector<double,2>;
         using Index = int;
         
+        //create random devices
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_int_distribution<State> dist_int(std::numeric_limits<State>::min(),std::numeric_limits<State>::max());
-        std::uniform_real_distribution<Traits> dist_real(std::numeric_limits<Traits>::min(),std::numeric_limits<Traits>::max()); 
-        const Position pos({dist_real(gen),dist_real(gen)});
-        const Index index = 2;
-        const bool boundary = true;
-        Value A;
-        A.value=dist_int(gen);;
+        std::uniform_real_distribution<double> dist_real(std::numeric_limits<double>::min(),std::numeric_limits<double>::max()); 
         
-        Utopia::Cell<Value,true,Position,Utopia::DefaultTag,Index> c1(A,pos,boundary,index);
-        Utopia::DefaultTag tag;
+        //Create Objects that are handed to the constructor
+        const Position pos({dist_real(gen),dist_real(gen)});
+        const Index index = dist_int(gen);
+        const bool boundary = true;
+        const State A=dist_int(gen);;
+        
+        //build a cell
+        Utopia::Cell<State,true,Position,Utopia::DefaultTag,Index> c1(A,pos,boundary,index);
+       
+        //assert that the content of the cell is correct
         assert(c1.position()==pos);
         assert(c1.is_boundary()==boundary);
         assert(c1.state()==A);
-        //Prüfe ob der DefaultTag() gesetzt ist
-        assert(c1.is_tagged==tag.is_tagged);
         assert(c1.id()==index);
-        //assert_cell_members(c1,pos,index,boundary);
-
+        
+        //test whether the default flag is set correctly
+        //it was created by the default constructor and not set explicitly here
+        Utopia::DefaultTag tag;
+        assert(c1.is_tagged==tag.is_tagged);
+        
         return 0;
     }
     catch(Dune::Exception c){
