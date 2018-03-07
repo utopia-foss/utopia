80d126ad9f22407286f56da3a586bb6c27093466
Yunus Sevinchan <YunusSevinchan@gmail.com>
2018-03-07 22:50:33 +0100

    Add cli script as script argument to setup.py
    This install it into the virtual env, from where it could be
    linked to, e.g., /usr/local/bin/utopia

--- python/utopya/setup.py
+++ python/utopya/setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Setups the utopya package, its test dependencies, and a script."""
 
 from setuptools import setup
 
@@ -6,18 +7,18 @@ from setuptools import setup
 INSTALL_DEPS = ['numpy>=1.13', 'PyYAML>=3.12', 'paramspace>=1.0b']
 TEST_DEPS    = ['pytest>=3.4.0', 'pytest-cov>=2.5.1']
 
-
 setup(name='utopya',
       description='The Utopia frontend package.',
       url='https://ts-gitlab.iup.uni-heidelberg.de/utopia/utopia',
       classifiers=[
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Utilities'
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Utilities'
       ],
       packages=['utopya'],
       include_package_data=True,
       install_requires=INSTALL_DEPS,
       tests_require=TEST_DEPS,
       test_suite='py.test',
-      extras_require=dict(test_deps=TEST_DEPS)
+      extras_require=dict(test_deps=TEST_DEPS),
+      scripts=['bin/utopia'] # installs this into the virtual env!
       )
