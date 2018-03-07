37c5fab6950a179d166a9f9d4b2c79e9f206d63e
Lukas Riedel <mail@lukasriedel.com>
2018-03-07 21:19:27 +0100

    Change signature of 'generate_model_info' to resemble 'configure_file'
    Added short docstring

--- cmake/modules/UtopiaGenerateModelInfo.cmake
+++ cmake/modules/UtopiaGenerateModelInfo.cmake
@@ -1,8 +1,16 @@
-function(generate_model_info filename)
+# Configure utopya with information on model targets
+#
+# .. cmake_function:: generate_model_info
+#
+#   This function calls 'configure_file' with the arguments given
+#   after deleting a possibly already generated output file
+#   to ensure that 'configure_file' is executed again.
+#
+function(generate_model_info file_in file_out)
     # remove existing file
-    file(REMOVE ${filename})
+    file(REMOVE ${file_out})
 
     # write new file
     string(TIMESTAMP CURRENT_TIME)
-    configure_file(${filename}.in ${filename} @ONLY)
+    configure_file(${file_in} ${file_out} @ONLY)
 endfunction()
