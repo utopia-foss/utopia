42c6edba06e1066c6152b1f8d069c5ee74755c01
H_Leusmann <hleusma@iup.uni-heidelberg.de>
2018-03-07 15:46:23 +0100

    Enabled test to find now fixed bug

--- python/utopya/test/test_multiverse.py
+++ python/utopya/test/test_multiverse.py
@@ -17,20 +17,20 @@ def mv_config():
     return dict(paths=dict(out_dir='~/utopia_output', model_name='', model_note=''))
 
 # Tests --------------------------------------------------------------------
-@pytest.mark.skip("Not far enough to initialise this.")
+#@pytest.mark.skip("Not far enough to initialise this.")
 def test_init():
     """Tests the initialsation of the Multiverse."""
     # FIXME
-    Multiverse()
+    #Multiverse()
 
-@pytest.mark.skip("To be re-implemented when the Multiverse is further developed.")
+#@pytest.mark.skip("To be re-implemented when the Multiverse is further developed.")
 def test_create_sim_dir(tmpdir, mv_config):
     """Tests the folder creation in the initialsation of the Multiverse."""
     # has to be adapted if user specific path
     # read path names from meta_cfg.yaml
 
     # adapt cfg to special needs
-    mv_config['paths']['out_dir'] = tmpdir.dirpath()
+    #mv_config['paths']['out_dir'] = tmpdir.dirpath()
     mv_config['paths']['model_name'] = "test_outer_folder_structure"
 
     # Init Multiverse
@@ -48,27 +48,27 @@ def test_create_sim_dir(tmpdir, mv_config):
     for folder in folder_list:
         assert os.path.isdir(os.path.join(path_base, latest, folder)) is True
 
-@pytest.mark.skip("To be re-implemented when the Multiverse is further developed.")
+#@pytest.mark.skip("To be re-implemented when the Multiverse is further developed.")
 def test_detect_doubled_folders(tmpdir, mv_config):
     # adapt cfg to special needs
-    mv_config['paths']['out_dir'] = tmpdir.dirpath()
+    #mv_config['paths']['out_dir'] = tmpdir.dirpath()
     mv_config['paths']['model_name'] = "test_universes_doubling"
     # create two Multiverses after another (within one second) 
     # expect error due to existing folders
     Multiverse(mv_config)
-    with pytest.raises(FileExistsError):
+    with pytest.raises(RuntimeError):
         Multiverse(mv_config)
 
-@pytest.mark.skip("To be re-implemented when the Multiverse is further developed.")
-def test_create_uni_dir(tmpdir, mv_config, maximum=10):
+#@pytest.mark.skip("To be re-implemented when the Multiverse is further developed.")
+def test_create_uni_dir(tmpdir, mv_config, maximum=9):
     # adapt cfg to special needs
-    mv_config['paths']['out_dir'] = tmpdir.dirpath()
+    #mv_config['paths']['out_dir'] = tmpdir.dirpath()
     mv_config['paths']['model_name'] = "test_universes_folder_structure"
 
     # Init Multiverse
     instance = Multiverse(mv_config)
     # Create the universe directories
-    for i in range(0, maximum):
+    for i in range(0, maximum+1):
         instance._create_uni_dir(i, maximum)
     # get the path of the universes folder
     path = instance.dirs['universes']
@@ -78,6 +78,6 @@ def test_create_uni_dir(tmpdir, mv_config, maximum=10):
     path_uni_last = os.path.join(path, "uni"+str(maximum-1).zfill(number_filling_zeros))
     assert path_uni_last[4] != '0'
     # check if all universe directories are created
-    for i in range(0, maximum):
-        path_uni = os.path.join(path, "uni"+str(i).zfill(number_filling_zeros))
+    for i in range(0, maximum+1):
+        path_uni = os.path.join(path, str(i).zfill(number_filling_zeros))
         assert os.path.isdir(path_uni) is True
