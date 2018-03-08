102b8b03063bc4b5ece935a6f7c2ee480975b308
Lukas Siebert <lsiebert@iup.uni-heidelberg.de>
2018-03-08 09:52:16 +0100

    minor changes in apply, still not functional

--- dune/utopia/test/apply_test.cc
+++ dune/utopia/test/apply_test.cc
@@ -1,4 +1,4 @@
-#include "apply.hh"
+#include <dune/utopia/apply.hh>
 #include <cassert>
 #include <dune/utopia/utopia.hh>
 #include <dune/common/exceptions.hh>
@@ -8,24 +8,13 @@ int main()
 {
     auto grid = Utopia::Setup::create_grid<2>(5);
     auto cells = Utopia::Setup::create_cells_on_grid<true>(grid);
-    
     using Pos = typename Utopia::GridTypeAdaptor<typename decltype(grid._grid)::element_type>::Position;
     // structured, periodic
     auto mc = Utopia::Setup::create_manager_cells<true, true>(grid, cells);
     
     
-    void apply_rule <true>(my_rule, mc.cells(), mc)
-    {
-        
-        for_each(container.begin(),container.end(), rule);
-        
-        /*
-        for (auto entity: container){
-            entity->state_new() = rule(entity);
-            entity->update();
-        }
-        */
-    }
+    apply_rule <true>(my_rule, mc.cells(), mc);
+    apply_rule <true>(my_update,mc.cells(),mc);
     
     
     return 0;
