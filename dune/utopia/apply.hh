251739ed6b61c3fc32f3e7717bdd08a39c81fcc1
Lukas Siebert <lsiebert@iup.uni-heidelberg.de>
2018-03-08 14:21:27 +0100

    overloaded apply rule

--- dune/utopia/apply.hh
+++ dune/utopia/apply.hh
@@ -3,35 +3,38 @@
 
 namespace Utopia {
     
-    template<bool sync, class Rule, class Container, class Manager>
-    void apply_rule(Rule rule, Container& container, Manager& manager);
-    
     ///synchronous update rule
-    template<class Rule, class Container, class Manager>
-    void apply_rule<true> (Rule rule, Container& container, Manager& manager)
+    template<class Container,bool sync= Container::value_type::element_type::is_sync(), class Rule , class Manager>
+    std::enable_if_t<sync, void>
+    apply_rule(Rule rule, Container& container, Manager& manager)
     {
+       
         //reduce the number of parameters in rule 
         //by binding parameter 2 to manager
         auto bind_rule=std::bind(rule, std::placeholders::_1, manager);
         
         for_each(container.begin(),container.end(),
-            [&bind_rule](const auto cell){ cell->state_new() = bind_rule(cell); });
+                [&bind_rule](const auto cell){ cell->state_new() = bind_rule(cell); });
         for_each(container.begin(),container.end(),
-            [](const auto cell){ cell->update(); });         
+                [](const auto cell){ cell->update(); });  
+          
+         
     }  
     
-    ///asynchronous update rule
-    template<class Rule, class Container, class Manager>
-    void apply_rule<false> (Rule rule, Container& container, Manager& manager)
+    template<class Container,bool sync= Container::value_type::element_type::is_sync(), class Rule , class Manager>
+    std::enable_if_t<!sync, void>
+    apply_rule(Rule rule, Container& container, Manager& manager)
     {
+    
         //reduce the number of parameters in rule 
         //by binding parameter 2 to manager
         auto bind_rule=std::bind(rule, std::placeholders::_1, manager);
         
-        std::random_shuffle(container.begin(),container.end());
+       // std::random_shuffle(container.begin(),container.end());
         for_each(container.begin(),container.end(),
-            [&bind_rule](const auto cell){ cell->state_new() = bind_rule(cell); });
+                [&bind_rule](const auto cell){ cell->state() = bind_rule(cell); });
                
-    }     
+    }  
+             
 }
 #endif // APPLY_HH    
