fe4a8814964c5c9fae230a8261046629b974d92a
H_Leusmann <hleusma@iup.uni-heidelberg.de>
2018-03-08 16:55:51 +0100

    Adapting test_multiverse to updated multiverse
    First tests running
    
    Using bad test, will be fixed

--- python/utopya/test/test_multiverse.py
+++ python/utopya/test/test_multiverse.py
@@ -4,50 +4,61 @@ import os
 import logging
 import math
 
+import pkg_resources
+
 import pytest
 
 from utopya import Multiverse
 
+import time
+
 log = logging.getLogger(__name__)
 
+BASE_RUNCFG_PATH = pkg_resources.resource_filename('test', 'cfg/run_cfg.yml')
+BASE_USERCFG_PATH = pkg_resources.resource_filename('test', 'cfg/user_cfg.yml')
+
 # Fixtures ----------------------------------------------------------------
-#@pytest.fixture
-
 @pytest.fixture
-def mv_kwargs() -> dict:
+def mv_kwargs(tmpdir) -> dict:
     """Returns a dict that can be passed to Multiverse for initialisation"""
-    return dict(run_cfg_path="", user_cfg_path=None)
-
+    return dict(model_name="dummy", run_cfg_path=BASE_RUNCFG_PATH, user_cfg_path=BASE_USERCFG_PATH)
 
 # Tests --------------------------------------------------------------------
 #@pytest.mark.skip("Not far enough to initialise this.")
-def test_init():
+def test_config_handling(mv_kwargs, tmpdir):
     """Tests the initialization of the Multiverse."""
-    #Multiverse()  # fails, if neither default nor metaconfig are present
-    instance1 = Multiverse(run_cfg_path="./test/run_config.yml")
-    print(instance1.meta_config)
-    instance2 = Multiverse(run_cfg_path="./test/run_config.yml", user_cfg_path="./test/user_config.yml")
-    print(instance2.meta_config)
-
+    mv_local = mv_kwargs
+    local_config = dict(paths=dict(out_dir=tmpdir.dirpath(), model_note="test_user_and_run_cfg"))
+    #
+    # Multiverse()  with special user and run config
+    Multiverse(**mv_kwargs, update_meta_cfg=local_config)
+    # Multiverse() with default user config and run config
+    mv_local['user_cfg_path'] = None
+    local_config['model_note'] = "test_run_cfg"
+    time.sleep(1)
+    Multiverse(**mv_local, update_meta_cfg=local_config)
     # Testing errors
+    # Multiverse with wrong run config
     with pytest.raises(FileNotFoundError):
-        Multiverse(run_cfg_path="not_existing_run_config.yml")
+        mv_local['run_cfg_path'] = './unvalid_run_cfg.yml'
+        local_config['model_note'] = "test_not existing_run_cfg"
+        Multiverse(**mv_local, update_meta_cfg=local_config)
 
     with pytest.raises(FileNotFoundError):
-        Multiverse(user_cfg_path="not_existing_user_config.yml")
+        mv_local['run_cfg_path'] = './unvalid_user_cfg.yml'
+        local_config['model_note'] = "test_not existing_user_cfg"
+        Multiverse(**mv_local, update_meta_cfg=local_config)
+
 
 #@pytest.mark.skip("To be re-implemented when the Multiverse is further developed.")
-def test_create_run_dir(tmpdir,model_name="test_outer_folder_structure"):
+def test_create_run_dir(mv_kwargs, tmpdir):
     """Tests the folder creation in the initialsation of the Multiverse."""
+    local_config = dict(paths=dict(out_dir=tmpdir.dirpath(), model_note="test_outer_directories"))
     # Init Multiverse
-    instance = Multiverse(model_name=model_name, run_cfg_path="./test/run_config.yml")
-    # set out dir to temp
-    instance._meta_config['paths']['out_dir'] = tmpdir.dirpath()
-    # make output folder
-    instance.create_run_dir()
+    instance = Multiverse(**mv_kwargs, update_meta_cfg=local_config)
     # Reconstruct path from settings for testing
     path_base = os.path.expanduser(instance.meta_config['paths']['out_dir'])
-    path_base = os.path.join(path_base, model_name)
+    path_base = os.path.join(path_base, mv_kwargs['model_name'])
     # get all folders in the output dir
     all_stuff = os.listdir(path_base)
     # take the latest one
@@ -57,8 +68,9 @@ def test_create_run_dir(tmpdir,model_name="test_outer_folder_structure"):
     for folder in ["config", "eval", "universes"]:  # may need to adapt
         assert os.path.isdir(os.path.join(path_base, latest, folder)) is True
 
-#@pytest.mark.skip("To be re-implemented when the Multiverse is further developed.")
-def test_detect_doubled_folders(tmpdir, model_name="test_universes_doubling"):
+@pytest.mark.skip("To be re-implemented when the Multiverse is further developed.")
+def test_detect_doubled_folders(mv_kwargs, tmpdir):
+    local_config = dict(paths=dict(out_dir=tmpdir.dirpath(), model_note="test_universes_doubling"))
     # Init Multiverse
     instance = Multiverse(model_name=model_name, run_cfg_path="./test/run_config.yml")
     # set out dir to temp
@@ -71,7 +83,7 @@ def test_detect_doubled_folders(tmpdir, model_name="test_universes_doubling"):
         # make output folder
         instance.create_run_dir()
 
-#@pytest.mark.skip("To be re-implemented when the Multiverse is further developed.")
+@pytest.mark.skip("To be re-implemented when the Multiverse is further developed.")
 def test_create_uni_dir(tmpdir):
     for i, max_uni in enumerate([1, 9, 10, 11, 99, 100, 101]):
         single_create_uni_dir(tmpdir, "test_universes_folder_structure_{}".format(i), max_uni)
