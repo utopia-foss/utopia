f5b66bae4e872a90e1c239d31abd252253d8b740
Lukas Siebert <lsiebert@iup.uni-heidelberg.de>
2018-03-08 11:54:54 +0100

    started specializing apply rule

--- dune/utopia/test/apply_test.cc
+++ dune/utopia/test/apply_test.cc
@@ -1,52 +1,43 @@
+#include <functional>
 #include <dune/utopia/apply.hh>
 #include <cassert>
 #include <dune/utopia/utopia.hh>
 #include <dune/common/exceptions.hh>
 #include "grid_cells_test.hh"
 #include <dune/utopia/neighborhoods.hh>
+
 int main()
 {
+    //build the ingredients for a manager
     auto grid = Utopia::Setup::create_grid<2>(5);
     auto cells = Utopia::Setup::create_cells_on_grid<true>(grid);
     using Pos = typename Utopia::GridTypeAdaptor<typename decltype(grid._grid)::element_type>::Position;
-    // structured, periodic
+   
+    // build a manager - structured, periodic
     auto mc = Utopia::Setup::create_manager_cells<true, true>(grid, cells);
     
+    //check cell values
     for(auto some_cell:mc.cells())
     {
     assert(some_cell->state()!=22);
     }
     
-    
-    auto my_rule = [&mc](const auto cell)
+    //define a rule
+    auto my_rule = [](const auto cell, auto mc)
     {
         auto nb = Utopia::Neighborhoods::NextNeighbor::neighbors(cell, mc);
-        cell->state_new()=22;
         return 22; 
     };
-    
-    auto my_update = [&mc](const auto cell)
-    {
-        cell->update();
-        return 1;
-    };
-    
-    
-    
+   
+    //apply the rule in a synchronous fashion
     Utopia::apply_rule <true>(my_rule, mc.cells(), mc);
     
+    //make sure the rule was applied correctly
     for(auto some_cell:mc.cells())
     {
-    assert(some_cell->state()!=22);
+    assert(some_cell->state()==22);
     assert(some_cell->state_new()==22);
     }
-    
-    Utopia::apply_rule <true>(my_update,mc.cells(),mc);
-    for(auto some_cell:mc.cells())
-    {
-    assert(some_cell->state()==some_cell->state_new());
-    assert(some_cell->state()==22);
-    }
-    
+   
     return 0;
 }
