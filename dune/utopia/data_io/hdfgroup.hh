7d8a229d0da79819340c9ad94b49a69c146b7a75
Benjamin Herdeanu <herdeanu@iup.uni-heidelberg.de>
2018-03-08 16:19:41 +0100

    add part of the documentation

--- dune/utopia/data_io/hdfgroup.hh
+++ dune/utopia/data_io/hdfgroup.hh
@@ -17,16 +17,18 @@ namespace DataIO
 
 class HDFFile;
 
-/**
- *
+
+/// An object representing a HDFGroup 
+/** 
+ * 
 */
 class HDFGroup
 {
 protected:
-    hid_t _group;
-    std::string _path;
-    std::unordered_map<std::string, std::shared_ptr<HDFGroup>> _open_groups;
-    std::unordered_map<std::string, std::shared_ptr<HDFGroup>> _open_datasets;
+    hid_t _group; /// Storage of group id
+    std::string _path; /// Storage of the path
+    std::unordered_map<std::string, std::shared_ptr<HDFGroup>> _open_groups; /// Storage of child groups and their path
+    std::unordered_map<std::string, std::shared_ptr<HDFGroup>> _open_datasets; /// Storage of child datasets and their path
 
 public:
     void info()
@@ -35,17 +37,31 @@ public:
         // H5Gget_info(_group);
     }
 
+    /// Get the id
+    /**
+    *  \return The hdf5 id
+    */
     hid_t get_id()
     {
         return _group;
     }
 
+    /// Get the path
+    /**
+    *  \return The hdf5 path
+    */
     std::string get_path()
     {
         return _path;
     }
 
 
+    /// Add an attribute to the HDFGroup object
+    /** Adds an attribute to a group
+    *  \tparam Attrdata Attribute type 
+    *  \param name Attribute name
+    *  \param attribute_data Attribute data
+    */
     template <typename Attrdata>
     void add_attribute(std::string name, Attrdata attribute_data) {
 
@@ -55,6 +71,10 @@ public:
         attribute.write(attribute_data);
     }
 
+    /// Get open groups 
+    /** Adds an attribute to a group
+    *  return open_groups A vector containing std::shared_pointer pointing at the HDFGroup objects
+    */
     std::vector<std::shared_ptr<HDFGroup>> get_open_groups()
     {
         std::vector<std::shared_ptr<HDFGroup>> open_groups;
@@ -65,6 +85,11 @@ public:
         return open_groups;
     }
 
+    /// Open a new HDFGroup
+    /**
+    * \param path The path
+    * \return A pointer to the newly created HDFGroup
+    */
     std::shared_ptr<HDFGroup> open_group(std::string path)
     {
         auto found = _open_groups.find(path);
@@ -77,6 +102,10 @@ public:
         }
     }
 
+    /// Close a group
+    /** Closes a child group if it is a child. 
+    * \param path The path
+    */
     void close_group(std::string path)
     {
         auto found = _open_groups.find(path);
@@ -91,9 +120,9 @@ public:
 
     }
 
+    /// Close the group if there are no more child groups or datasets
     void close() 
-    { 
-        // TODO close only if 
+    {         
         if (_open_groups.size() == 0 && _open_datasets.size() == 0)
         {
             H5Gclose(_group); 
@@ -103,7 +132,6 @@ public:
             throw std::runtime_error("The group still contains child groups or datasets");
         }
     }
-
 
 
     void open_dataset(std::string path)
@@ -116,6 +144,11 @@ public:
         // TODO
     }
 
+    /// Swap two HDFGroup objects
+    /**
+    *  \param group1 First HDFGroup object
+    *  \param group2 Second HDFGroup object
+    */
     friend void swap(HDFGroup& group1, HDFGroup& group2)
     {
         using std::swap;
@@ -126,30 +159,41 @@ public:
         swap(group1._open_datasets, group2._open_datasets);
     }
 
-    // default constructor
+    /// Default constructor
     HDFGroup() = default;
 
-    // copy constructor
+    /// Copy constructor
+    /** Constructs an HDFGroup object by copying another HDFGroup object
+    *  \param group The HDFGroup object that is copied
+    */
     HDFGroup(const HDFGroup& group) :   _group(group._group),
                                         _path(group._path),
                                         _open_groups(group._open_groups),
                                         _open_datasets(group._open_datasets)
     {};
 
-    // move constructor
+    /// Move constructor
+    /** 
+    * \param group The rvalue reference
+    */
     HDFGroup(HDFGroup&& group):     _group(std::move(group._group)),
                                     _path(std::move(group._path)),
                                     _open_groups(std::move(group._open_groups)),
                                     _open_datasets(std::move(group._open_datasets))
     {};
 
+    /// Assign a HDFGroup object using an assignment operator
+    /** Implement assignment operation using the copy-and-swap idiom
+    *
+    *  \return the copied HDFGroup object
+    */ 
     HDFGroup& operator=(HDFGroup group)
     {
         swap(group, *this);
         return *this;
     }
     
-    // constructor
+    /// Constructor
     template <typename Object>
     HDFGroup(Object &object, std::string name) : _path(name) 
     {
@@ -183,17 +227,16 @@ public:
         }
     }
 
+    /// Destructor
     virtual ~HDFGroup()
     {
         if (H5Iis_valid(_group) == 0) { 
-            H5Gclose(_group);
+            // H5Gclose(_group);
+            close();
         }
     }
 
 }; // class HDFGroup
-
-
-
 
 } // namespace DataIO 
 
