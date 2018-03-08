94ade8a7927672334a482f62b40f06065bdef98e
Lukas Siebert <lsiebert@iup.uni-heidelberg.de>
2018-03-08 16:31:50 +0100

    finalized both apply_rule functions and tests

--- dune/utopia/apply.hh
+++ dune/utopia/apply.hh
@@ -3,7 +3,11 @@
 
 namespace Utopia {
     
-    ///synchronous update rule
+    ///synchronous application of rules
+    /**\param rule - function that takes an entity and returns a new state 
+     * \param Container  a container with the entities upon whom rule is applied
+     * \param manager holds the entities to be modified
+     */
     template<class Container,bool sync= Container::value_type::element_type::is_sync(), class Rule , class Manager>
     std::enable_if_t<sync, void>
     apply_rule(Rule rule, Container& container, Manager& manager)
@@ -21,19 +25,24 @@ namespace Utopia {
          
     }  
     
+    
+   ///asynchronous application of rules
+   /**\param rule - function that takes an entity and returns a new state 
+    * \param Container  a container with the entities upon whom rule is applied
+    * \param manager holds the entities to be modified
+    */
     template<class Container,bool sync= Container::value_type::element_type::is_sync(), class Rule , class Manager>
     std::enable_if_t<!sync, void>
     apply_rule(Rule rule, Container& container, Manager& manager)
     {
-    
         //reduce the number of parameters in rule 
         //by binding parameter 2 to manager
         auto bind_rule=std::bind(rule, std::placeholders::_1, manager);
-        
-       // std::random_shuffle(container.begin(),container.end());
-        for_each(container.begin(),container.end(),
-                [&bind_rule](const auto cell){ cell->state() = bind_rule(cell); });
-               
+        //Utopia::DefaultRNG something(42);
+        auto copy_container(container);
+        std::shuffle(copy_container.begin(),copy_container.end(), *manager.rng());
+        for_each(copy_container.begin(),copy_container.end(),
+                [&bind_rule](const auto cell){ cell->state() = bind_rule(cell); });     
     }  
              
 }
