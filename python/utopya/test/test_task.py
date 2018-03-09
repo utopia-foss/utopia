09c7c29cf48454181a7247b6753dd9e62f29752c
H_Leusmann <hleusma@iup.uni-heidelberg.de>
2018-03-09 18:15:35 +0100

    Add Documentation Strings and Test_task.py

---
+++ python/utopya/test/test_task.py
@@ -0,0 +1,47 @@
+import pytest
+
+from utopya import Task
+
+
+# Fixtures ----------------------------------------------------------------
+@pytest.fixture
+def mv_kwargs(tmpdir) -> dict:
+    """Returns a dict that can be passed to Task for initialisation"""
+    return dict(model_name="dummy",
+                run_cfg_path=RUN_CFG_PATH,
+                user_cfg_path=USER_CFG_PATH)
+
+
+# Initialisation tests --------------------------------------------------------
+
+def test_simple_init(mv_kwargs):
+    """Tests whether initialisation works."""
+    Task(**mv_kwargs)
+
+def test_invalid_model_name_and_operation(mv_kwargs, tmpdir):
+    """Tests for correct behaviour upon invalid model names"""
+    mv_local = mv_kwargs
+
+    # Try to change the model name
+    local_config = dict(paths=dict(out_dir=tmpdir.dirpath(),
+                                   model_note="test_try_change_model_name"))
+    instance = Multiverse(**mv_local, update_meta_cfg=local_config)
+    with pytest.raises(RuntimeError):
+        instance.model_name = "dummy"
+
+    # Try invalid model name  
+    local_config = dict(paths=dict(out_dir=tmpdir.dirpath(),
+                                   model_note="test_invalid_model_name"))
+    mv_local['model_name'] = "invalid_model_RandomShit_bgsbjkbkfvwuRfopiwehGEP"
+
+    with pytest.raises(ValueError):
+        Multiverse(**mv_local, update_meta_cfg=local_config)
+
+'''
+tasks=[]
+for i in range(10):
+    tasks.append(Task(i,rd.randint(-10,10)))
+    
+print(tasks)
+print(sorted(tasks))
+#'''
--- python/utopya/utopya/task.py
+++ python/utopya/utopya/task.py
@@ -1,18 +1,45 @@
 """Implementation of the Task class.
 
-The Task supplies a container for a Task handled by the WorkerManager.
+The Task supplies a container for a task handled by the WorkerManager.
 """
 
 class Task:
+    """The Task is a container for a task handled by the WorkerManager.
+    
+    It aims to provide necessary interfaces
+    
+    Attributes:
+        id (int): Task_ID (positive, read only)
+        priority (int): The model name associated with this Multiverse
+    """
     def __init__(self, task_id: int, priority: int=0):
-        self.id=task_id
-        self._priority=priority #(possible between -int and + int)
+        self.id = task_id
+        self._priority = priority  # (possible between -int and + int)
         self.setup_func = None
         self.setup_kwargs = None
         
     def __repr__(self):
         return "ID: {}, Pri: {}".format(self.id, self._priority)
     
-    #compariosn operator for priority
+    # compariosn operator for priority
     def __lt__(self, other):
         return self._priority < other._priority
+
+    # Properties --------------------------------------------------------------
+    @property
+    def id(self):
+        return self._id
+    @id.setter
+    def id(self, task_id: int):
+        """Checks if the model name is valid, then sets it and makes it read-only."""
+        if task_id < 0:
+            raise ValueError("Negative task Id not allowed, chosen ID: {}".format(task_id))
+        elif self.id:
+            raise RuntimeError("A Task's ID cannot be changed!")
+        else:
+            self._id = task_id
+            log.debug("Set Task ID:  %s", task_id)
+
+    # Public API --------------------------------------------------------------
+
+    # Non-Public API ----------------------------------------------------------
