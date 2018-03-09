cc87c38e0d89c075e107a92bc00df3c82f3934af
Lukas Riedel <mail@lukasriedel.com>
2018-03-09 10:12:50 +0100

    Fix cosmetics and docs of apply_rule, remove commented static assert

--- dune/utopia/apply.hh
+++ dune/utopia/apply.hh
@@ -3,7 +3,7 @@
 
 namespace Utopia {
 
-///synchronous application of rules
+/// synchronous application of rules
 /**\param rule Function that takes an entity and manager and returns a new state
  * \param Container A container with the entities upon whom rule is applied
  * \param Manager holds the entities to be modified
@@ -18,12 +18,6 @@ apply_rule(Rule rule, Container& container, Manager& manager)
 {
     //reduce the number of parameters in rule
     //by binding parameter 2 to manager
-    // using EntityPtr = typename impl::pointer_t<Container>;
-    // using Entity = typename impl::entity_t<Container>;
-    // static_assert(std::is_convertible_v<std::invoke_result<
-    //     Rule(EntityPtr, Manager)>, typename Entity::State>,
-    //     "There's something wrong with your rule! "
-    //     "Check signature and return type!");
     auto bind_rule = std::bind(rule, std::placeholders::_1, manager);
     
     for_each(container.begin(), container.end(),
@@ -35,11 +29,11 @@ apply_rule(Rule rule, Container& container, Manager& manager)
 }
 
 
-///asynchronous application of rules
-/**\param rule - function that takes an entity and returns a new state
-* \param Container  a container with the entities upon whom rule is applied
-* \param manager holds the entities to be modified
-*/
+/// aynchronous application of rules
+/**\param rule Function that takes an entity and manager and returns a new state
+ * \param Container A container with the entities upon whom rule is applied
+ * \param Manager holds the entities to be modified
+ */
 template<
     bool shuffle=true,
     class Rule,
