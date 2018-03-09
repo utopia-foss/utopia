25035010a37f56f72a4aff9076c6526a3edb1742
Harald Mack <harald.mack@iup.uni-heidelberg.de>
2018-03-09 22:03:58 +0100

    added group testfile

--- dune/utopia/data_io/test/hdffile_test.cc
+++ dune/utopia/data_io/test/hdffile_test.cc
@@ -8,39 +8,37 @@ int file_open_tester() {
     auto file2 = std::make_shared<HDFFile>("hdf5testfile.h5", "r");
     auto file3 = std::make_shared<HDFFile>("hdf5testfile.h5", "r+");
     bool caught = false;
-    // try {
-    //     HDFFile file4("hdf5testfile.h5", "x");
+    try {
+        HDFFile file4("hdf5testfile.h5", "x");
 
-    // } catch (const std::exception &e) {
-    //     caught = true;
-    //     std::cerr << e.what() << std::endl;
-    // }
-    // if (caught == false) {
-    //     return -1;
-    // }
+    } catch (const std::exception &e) {
+        caught = true;
+        std::cerr << e.what() << std::endl;
+    }
+    if (caught == false) {
+        return -1;
+    }
     auto file5 = std::make_shared<HDFFile>("hdftestfile.h5", "a");
     caught = false;
-    // try {
-    //     HDFFile("hdftestfile.h5", "n");
-    // } catch (const std::exception &e) {
-    //     caught = true;
-    //     std::cerr << e.what() << std::endl;
-    // }
-    // if (caught == false) {
-    //     return -1;
-    // }
+    try {
+        HDFFile("hdftestfile.h5", "n");
+    } catch (const std::exception &e) {
+        caught = true;
+        std::cerr << e.what() << std::endl;
+    }
+    if (caught == false) {
+        return -1;
+    }
     return 0;
 }
 
 int file_func_tester() {
-    auto file = std::make_shared<HDFFile>(
-        "hdf5testfile_func.h5", "w");
+    auto file = std::make_shared<HDFFile>("hdf5testfile_func.h5", "w");
 
     // check that group opening works
     auto testgroup2 = file->open_group("/testgroup1/testgroup2");
     file->close();
-    file = std::make_shared<HDFFile>(
-        "hdf5testfile_func.h5", "r+");
+    file = std::make_shared<HDFFile>("hdf5testfile_func.h5", "r+");
     hid_t group_test =
         H5Gopen(file->get_id(), "/testgroup1/testgroup2", H5P_DEFAULT);
 
@@ -52,11 +50,11 @@ int file_func_tester() {
     H5Gclose(group_test);
 
     file->delete_group("/testgroup1/testgroup2");
-    // hid_t group_test2 =
-    //     H5Gopen(file.get_id(), "/testgroup1/testgroup2", H5P_DEFAULT);
+    hid_t group_test2 =
+        H5Gopen(file->get_id(), "/testgroup1/testgroup2", H5P_DEFAULT);
 
-    // // group may not exist anymore
-    // assert(group_test2 < 0);
+    // group may not exist anymore
+    assert(group_test2 < 0);
 
     // check base group stuff
     auto basegroup = file->get_basegroup();
@@ -69,5 +67,8 @@ int main() {
     int sentinel = 0;
     sentinel = file_open_tester();
     sentinel = file_func_tester();
+    if (sentinel == 0) {
+        std::cout << "SUCCESSFUL TEST RUN" << std::endl;
+    }
     return sentinel;
 }
---
+++ dune/utopia/data_io/test/hdfgroup_test.cc
@@ -0,0 +1,5 @@
+#include "../hdfgroup.hh"
+#include <iostream>
+using namespace Utopia::DataIO;
+
+int main() { return 0; }
