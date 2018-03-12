0c2398c2d67c4daca52026cfe6da0865bdcb289a
Yunus Sevinchan <YunusSevinchan@gmail.com>
2018-03-12 13:38:18 +0100

    Implement syntax for stop condition check
    Adapted base config accordingly

--- python/utopya/utopya/stopcond.py
+++ python/utopya/utopya/stopcond.py
@@ -1,10 +1,12 @@
 """This module implements the StopCondition class, which is used by the
 WorkerManager to stop a worker process in certain situations."""
 
-import logging
-from subprocess import Popen
-
+import copy
+import time
 import yaml
+import logging
+from typing import List
+from subprocess import Popen
 
 # Initialise logger
 log = logging.getLogger(__name__)
@@ -17,14 +19,49 @@ class StopCondition:
     of this class store the information required to deduce whether the
     condition if fulfilled or not.
     """
-    def __init__(self, name: str, description: str=None, enabled: bool=True):
+    def __init__(self, *, to_check: List[dict], name: str=None, description: str=None, enabled: bool=True):
         """Create a new stop condition"""
-        # Carry over attributes
-        self.enabled = enabled
-        self.name = name
-        self.description = description
 
-        # Attributes that define the stop condition
+        # Resolve functions that are to be checked
+        self.to_check = self._resolve_sc_funcs(to_check)
+
+        # Carry over descriptive attributes
+        self.enabled = enabled
+        self.description = description
+        self.name = name if name else " && ".join([e.get('func_name')
+                                                   for e in to_check])
+
+        log.debug("Initialized stop condition '%s' with %d checking "
+                  "function(s).", self.name, len(self.to_check))
+
+    @staticmethod
+    def _resolve_sc_funcs(to_check: List[dict]) -> List[tuple]:
+        """Resolves the functions and kwargs"""
+        funcs_and_kws = []
+
+        for func_dict in to_check:
+            # Work on a copy
+            func_dict = copy.deepcopy(func_dict)
+
+            # Pop the name and resolve the function
+            func_name = func_dict.pop('func_name')
+            _func_name = '_sc_' + func_name
+
+            log.debug("Resolving function '%s' from global scope ...",
+                      _func_name)
+            sc_func = globals().get(_func_name)
+
+            # Check it
+            if not (sc_func and callable(sc_func)):
+                raise ValueError("Could not find function '{}' in local "
+                                 "scope!".format(_func_name))
+
+            # Valid. Append the information to the list
+            funcs_and_kws.append((sc_func, func_name, func_dict))
+        
+        log.debug("Resolved %d stop condition function(s).",
+                  len(funcs_and_kws))
+        return funcs_and_kws
 
     def __str__(self) -> str:
         return ("StopCondition '{name:}':\n"
@@ -33,15 +70,30 @@ class StopCondition:
     def fulfilled(self, proc: Popen, *, worker_info: dict) -> bool:
         """Checks if the stop condition is fulfilled for the given worker, using the information from the dict.
         
+        All given stop condition functions are evaluated; if all of them return
+        True, this method will also return True.
+        
         Args:
             proc (Popen): Worker object that is to be checked
             worker_info (dict): The information dict for this specific worker
+        
+        Returns:
+            bool: If all stop condition functions returned true for the given
+                worker and its current information
         """
-        if not self.enabled:
+        if not self.enabled or not self.to_check:
+            # Never fulfilled
             return False
 
-        # Now perform the check
-        # TODO continue here
+        # Now perform the check on all stop condition functions
+        for sc_func, name, kws in self.to_check:
+            if not sc_func(worker_info=worker_info, proc=proc, **kws):
+                # One was not True -> not fulfilled, need not check the others
+                log.debug("%s not fulfilled! Returning False ...", name)
+                return False
+
+        # All were True -> fulfilled
+        return True
 
 
 # -----------------------------------------------------------------------------
@@ -60,3 +112,13 @@ def stop_cond_constructor(loader, node) -> StopCondition:
                         "with value:\n{}".format(type(node), node))
 
     return stop_cond
+
+# -----------------------------------------------------------------------------
+# Stop condition functions
+# These all get passed the worker process, information and additional kwargs
+# Required signature:  (*, worker_info, proc, **kws)
+# If `proc` or `worker_info` is not needed, this can be indicated via default arguments
+
+def _sc_timeout_wall(*, worker_info: dict, seconds: float, proc: Popen=None) -> bool:
+    """Checks the wall timeout of the given worker"""
+    return bool(time.time() - worker_info['create_time'] > seconds)
