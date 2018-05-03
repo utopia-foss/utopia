890c09c400c4c176f59b2380f15064c5cbac06b4
Yunus Sevinchan <YunusSevinchan@gmail.com>
2018-05-03 17:29:07 +0200

    Fix wrong config for dummy model and extend data manager test
    The run_cfg and sweep_cfg were not passing the seed to the dummy
    model but defined it at the top level. While that is the desired
    behaviour, it would require changes to the dummy model, which are
    not part of this MR.

--- python/utopya/test/test_datamanager.py
+++ python/utopya/test/test_datamanager.py
@@ -1,5 +1,6 @@
 """Tests the UtopiaDataManager and involved functions and classes."""
 
+import os
 import uuid
 from pkg_resources import resource_filename
 
@@ -28,7 +29,22 @@ def mv_kwargs(tmpdir) -> dict:
     return dict(model_name='dummy',
                 run_cfg_path=RUN_CFG_PATH,
                 user_cfg_path=False,  # to omit the user config
-                update_meta_cfg=dict(paths=unique_paths))
+                # update_meta_cfg=dict(paths=unique_paths)
+                )
+
+@pytest.fixture
+def dm_after_single(mv_kwargs) -> DataManager:
+    """Initialises a Multiverse with a DataManager, runs a simulation with
+    output going into a temporary directory, then returns the DataManager."""
+    # Initialise the Multiverse
+    mv_kwargs['run_cfg_path'] = RUN_CFG_PATH
+    mv = Multiverse(**mv_kwargs)
+
+    # Run a sweep
+    mv.run_single()
+    
+    # Return the data manager
+    return mv.dm
 
 @pytest.fixture
 def dm_after_sweep(mv_kwargs) -> DataManager:
@@ -38,19 +54,11 @@ def dm_after_sweep(mv_kwargs) -> DataManager:
     mv_kwargs['run_cfg_path'] = SWEEP_CFG_PATH
     mv = Multiverse(**mv_kwargs)
 
-    # Run the sweep
+    # Run a sweep
     mv.run_sweep()
     
     # Return the data manager
     return mv.dm
-
-@pytest.fixture
-def load_cfg() -> dict:
-    """Returns a dict that specifies a load configuration"""
-    lc = dict()
-    lc['cfg'] = dict(loader='yaml', glob_str='config/*.yml')
-
-    return lc
 
 # Tests -----------------------------------------------------------------------
 
@@ -58,17 +66,24 @@ def test_init(tmpdir):
     """Tests initialisation of the Utopia data manager"""
     DataManager(str(tmpdir))
 
-def test_load_sweep(dm_after_sweep, load_cfg):
+def test_load_sweep(dm_after_sweep):
     """Tests the loading of simulation data for a sweep"""
     dm = dm_after_sweep
 
-    dm.load_from_cfg(load_cfg=load_cfg)
+    # Load and plot a tree of the loaded data
+    dm.load_from_cfg()
 
     print("{:tree}".format(dm))
 
+    # Check that the config is loaded as expected
     assert 'cfg' in dm
     assert 'cfg/base_cfg' in dm
     assert 'cfg/meta_cfg' in dm
     assert 'cfg/model_cfg' in dm
     assert 'cfg/run_cfg' in dm
-    assert 'cfg/update_cfg' in dm
+
+    # Check that the uni config is also loaded
+    assert 'uni_cfg' in dm
+
+    # Check that the binary data is loaded as expected
+    # TODO
