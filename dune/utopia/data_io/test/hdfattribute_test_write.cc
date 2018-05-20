f7b27693bf428bfdf390f062e1c87bb25d38ee24
Harald Mack <hmack@iup.uni-heidelberg.de>
2018-05-20 18:49:13 +0200

    changed the attribute_write_test to match what it should be able to do when this commit is done

--- dune/utopia/data_io/test/hdfattribute_test_write.cc
+++ dune/utopia/data_io/test/hdfattribute_test_write.cc
@@ -3,34 +3,91 @@
 #include "../hdfgroup.hh"
 #include <cassert>
 #include <iostream>
+#include <random>
 #include <string>
 
 using namespace Utopia::DataIO;
 
-int main() {
-  // make file
-  HDFFile file("testfile.h5", "w");
+int main()
+{
+    std::default_random_engine rng(67584327);
+    std::normal_distribution<double> dist(1., 2.5);
+    std::uniform_int_distribution<std::size_t> idist(20, 50);
+    // make file
+    HDFFile file("testfile.h5", "w");
 
-  // make groups
-  HDFGroup low_group = HDFGroup(*file.get_basegroup(), "/testgroup");
-  HDFGroup low_group2 = HDFGroup(*file.get_basegroup(), "/testgroup2");
+    // make groups
+    HDFGroup low_group = HDFGroup(*file.get_basegroup(), "/testgroup");
 
-  // adding attribute1
-  std::string attributename = "testattribute";
-  std::string attribute_data = "this is a testing attribute";
-  HDFAttribute<HDFGroup> attribute(low_group, attributename);
+    // adding attribute1
+    std::string attributename1 = "stringattribute";
+    std::string attributename2 = "vectorattribute";
+    std::string attributename3 = "integerattribute";
+    std::string attributename4 = "varlenattribute";
+    std::string attributename5 = "charptrattribute";
 
-  // writing a string
-  attribute.write(attribute_data);
+    // make attribute_data1
+    std::string attribute_data1 = "this is a testing attribute";
 
-  // adding attribute2
-  std::string attributename2 = "testattribute2";
+    // make attribute_data2
+    std::vector<double> attribute_data2(20);
+    std::generate(attribute_data2.begin(), attribute_data2.end(),
+                  [&]() { return dist(rng); });
 
-  HDFAttribute<HDFGroup> attribute2(low_group2, "testattribute2");
+    // make attribute_data3
+    int attribute_data3 = 42;
 
-  // writing primitive type attribute
-  int attribute_data2 = 42;
-  attribute2.write(attribute_data2);
+    // make attribute_data4
+    std::vector<std::vector<double>> attribute_data4(5);
+    std::generate(attribute_data4.begin(), attribute_data4.end(), [&]() {
+        std::vector<double> data(idist(rng));
+        std::generate(data.begin(), data.end(), [&]() { return dist(rng); });
+        return data;
+    });
 
-  return 0;
+    // make attributes
+    HDFAttribute<HDFGroup> attribute1(low_group, attributename1);
+    HDFAttribute<HDFGroup> attribute2(low_group, attributename2);
+    HDFAttribute<HDFGroup> attribute3(low_group, attributename3);
+    HDFAttribute<HDFGroup> attribute4(low_group, attributename4);
+    HDFAttribute<HDFGroup> attribute5(low_group, attributename5);
+
+    // writing a string
+    attribute1.write(attribute_data1);
+
+    // writing vector
+    attribute2.write(attribute_data2);
+
+    // writing a simple number
+    attribute3.write(attribute_data3);
+
+    // writing a nested vector
+    attribute4.write(attribute_data4);
+
+    // writing a char*
+    attribute5.write("this is a char* attribute");
+
+    // make attributes
+    HDFAttribute<HDFGroup> attribute7(low_group, attributename1 + "_rvalue");
+    HDFAttribute<HDFGroup> attribute8(low_group, attributename2 + "_rvalue");
+    HDFAttribute<HDFGroup> attribute9(low_group, attributename3 + "_rvalue");
+    HDFAttribute<HDFGroup> attribute10(low_group, attributename4 + "_rvalue");
+
+    // writing rvalue references
+    // writing a string
+    attribute7.write(std::string("this is an rvalue string"));
+
+    // writing vector
+    attribute8.write(std::vector<double>({dist(rng), dist(rng), dist(rng)}));
+
+    // writing a simple number
+    attribute9.write(21);
+
+    // writing a nested vector
+    attribute10.write(std::vector<std::vector<double>>{
+        {dist(rng), dist(rng), dist(rng)},
+        {dist(rng), dist(rng), dist(rng), dist(rng), dist(rng), dist(rng)},
+        {dist(rng), dist(rng), dist(rng), dist(rng), dist(rng)}});
+
+    return 0;
 }
