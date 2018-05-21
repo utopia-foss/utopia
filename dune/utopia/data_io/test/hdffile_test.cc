62d2ff9239e6de5ba4f811ad5ebc027154e0eda3
Harald Mack <hmack@iup.uni-heidelberg.de>
2018-05-21 22:31:24 +0200

    changes in HDFTypeFactory break dataset read, fuck you hdf5...

--- dune/utopia/data_io/test/hdffile_test.cc
+++ dune/utopia/data_io/test/hdffile_test.cc
@@ -3,55 +3,62 @@
 #include <iostream>
 
 using namespace Utopia::DataIO;
-int file_open_tester() {
+int file_open_tester()
+{
     auto file1 = std::make_shared<HDFFile>("hdf5testfile.h5", "w");
     auto file2 = std::make_shared<HDFFile>("hdf5testfile.h5", "r");
     auto file3 = std::make_shared<HDFFile>("hdf5testfile.h5", "r+");
     bool caught = false;
-    try {
+    try
+    {
         HDFFile file4("hdf5testfile.h5", "x");
-
-    } catch (const std::exception &e) {
+    }
+    catch (const std::exception& e)
+    {
         caught = true;
         std::cerr << e.what() << std::endl;
     }
-    if (caught == false) {
+    if (caught == false)
+    {
         return -1;
     }
     auto file5 = std::make_shared<HDFFile>("hdftestfile.h5", "a");
     caught = false;
-    try {
+    try
+    {
         HDFFile("hdftestfile.h5", "n");
-    } catch (const std::exception &e) {
+    }
+    catch (const std::exception& e)
+    {
         caught = true;
         std::cerr << e.what() << std::endl;
     }
-    if (caught == false) {
+    if (caught == false)
+    {
         return -1;
     }
     return 0;
 }
 
-int file_func_tester() {
+int file_func_tester()
+{
     auto file = std::make_shared<HDFFile>("hdf5testfile_func.h5", "w");
 
     // check that group opening works
     auto testgroup2 = file->open_group("/testgroup1/testgroup2");
     file->close();
     file = std::make_shared<HDFFile>("hdf5testfile_func.h5", "r+");
-    hid_t group_test =
-        H5Gopen(file->get_id(), "/testgroup1/testgroup2", H5P_DEFAULT);
+    hid_t group_test = H5Gopen(file->get_id(), "/testgroup1/testgroup2", H5P_DEFAULT);
 
-    if (group_test < 0) {
-        std::cerr << "group opening failed, does not exist at path"
-                  << std::endl;
+    if (group_test < 0)
+    {
+        std::cerr << "group opening failed, does not exist at path" << std::endl;
         return -1;
     }
     H5Gclose(group_test);
 
     file->delete_group("/testgroup1/testgroup2");
-    hid_t group_test2 =
-        H5Gopen(file->get_id(), "/testgroup1/testgroup2", H5P_DEFAULT);
+    hid_t group_test2 = H5Gopen(file->get_id(), "/testgroup1/testgroup2", H5P_DEFAULT);
 
     // group may not exist anymore
     assert(group_test2 < 0);
@@ -63,11 +70,13 @@ int file_func_tester() {
     return 0;
 }
 
-int main() {
+int main()
+{
     int sentinel = 0;
     sentinel = file_open_tester();
     sentinel = file_func_tester();
-    if (sentinel == 0) {
+    if (sentinel == 0)
+    {
         std::cout << "SUCCESSFUL TEST RUN" << std::endl;
     }
     return sentinel;
--- dune/utopia/data_io/test/hdfintegration_test.cc
+++ dune/utopia/data_io/test/hdfintegration_test.cc
@@ -23,82 +23,75 @@ void write(std::vector<Teststruct>& data)
     auto group = file.get_basegroup()->open_group(
         "first_deeper/second_deeper/third_deeper");
 
-    // auto dataset = group->open_dataset("dataset1");
-    // dataset->write(data.begin(), data.end(), [](auto& value) { return value.x; });
-    // // std::string attrdata = "this is a testattribute";
-    // dataset->add_attribute(
-    //     "testattribute",
-    //     std::string("this is an attribute to a double dataset"));
+    auto dataset = group->open_dataset("dataset1");
+    dataset->write(data.begin(), data.end(), [](auto& value) { return value.x; });
+    // std::string attrdata = "this is a testattribute";
+    dataset->add_attribute(
+        "testattribute",
+        std::string("this is an attribute to a double dataset"));
 
-    // // write variable length string
-    // auto dataset2 = group->open_dataset("dataset2");
-    // dataset2->write(data.begin(), data.end(),
-    //                 [](auto& value) -> std::string& { return value.y; });
+    // write variable length string
+    auto dataset2 = group->open_dataset("dataset2");
+    dataset2->write(data.begin(), data.end(),
+                    [](auto& value) -> std::string& { return value.y; });
 
-    // dataset2->add_attribute("stringattribute",
-    //                         "this is an attribute to std::string");
+    dataset2->add_attribute("stringattribute",
+                            "this is an attribute to std::string");
 
-    // auto dataset3 = group->open_dataset("dataset3");
-    // dataset3->write(data.begin(), data.end(),
-    //                 [](auto& value) -> std::vector<int>& { return value.z; });
+    auto dataset3 = group->open_dataset("dataset3");
+    dataset3->write(data.begin(), data.end(),
+                    [](auto& value) -> std::vector<int>& { return value.z; });
 
-    // dataset3->add_attribute("integer vector attribute",
-    //                         "this is an attribute to an int vector");
-
-    // file.close();
+    dataset3->add_attribute("integer vector attribute",
+                            "this is an attribute to an int vector");
 }
 
 void read(std::vector<Teststruct>& data)
 {
-    // HDFFile file("integrationtest_file.h5", "r");
+    HDFFile file("integrationtest_file.h5", "r");
 
-    // // open file and read
-    // std::cout << "opening group" << std::endl;
-    // auto group = file.get_basegroup()
-    //                  ->open_group("first_deeper")
-    //                  ->open_group("second_deeper/third_deeper");
+    // open file and read
+    std::cout << "opening group" << std::endl;
+    auto group = file.get_basegroup()->open_group(
+        "first_deeper/second_deeper/third_deeper");
 
-    // std::cout << "opening dataset1" << std::endl;
-    // auto dataset1 = group->open_dataset("dataset1");
-    // std::cout << "opening dataset2" << std::endl;
-    // auto dataset2 = group->open_dataset("dataset2");
-    // std::cout << "opening dataset3" << std::endl;
-    // auto dataset3 = group->open_dataset("dataset3");
+    std::cout << "opening dataset1" << std::endl;
+    auto dataset1 = group->open_dataset("dataset1");
+    std::cout << "opening dataset2" << std::endl;
+    auto dataset2 = group->open_dataset("dataset2");
+    std::cout << "opening dataset3" << std::endl;
+    auto dataset3 = group->open_dataset("dataset3");
 
-    // auto values = dataset1->read<double>();
+    auto values = dataset1->read<double>();
 
-    // assert(values.size() == data.size());
-    // for (std::size_t i = 0; i < data.size(); ++i)
-    // {
-    //     assert(std::abs(values[i] - data[i].x) < 1e-16);
-    // }
+    assert(values.size() == data.size());
+    for (std::size_t i = 0; i < data.size(); ++i)
+    {
+        assert(std::abs(values[i] - data[i].x) < 1e-16);
+    }
 
-    // auto dataset2 =
-    //     file.open_dataset("first_deeper/second_deeper/third_deeper/dataset2");
-    // auto read_data = dataset2->read<std::string>();
-    // assert(read_data.size() == data.size());
-    // for (std::size_t i = 0; i < read_data.size(); ++i)
-    // {
-    //     assert(std::string(read_data[i]) == data[i].y);
-    // }
+    auto read_data = dataset2->read<std::string>();
+    assert(read_data.size() == data.size());
+    for (std::size_t i = 0; i < read_data.size(); ++i)
+    {
+        assert(std::string(read_data[i]) == data[i].y);
+    }
 
-    // auto dataset3 =
-    //     file.open_dataset("first_deeper/second_deeper/third_deeper/dataset3");
+    auto read_data_int = dataset3->read<std::vector<int>>();
+    for (std::size_t i = 0; i < read_data_int.size(); ++i)
+    {
+        assert(read_data_int[i].size() == data[i].z.size());
+        for (std::size_t j = 0; j < data[i].z.size(); ++j)
+        {
+            assert(read_data_int[i][j] == data[i].z[j]);
+        }
+    }
 
-    // auto read_data_int = dataset3->read<std::vector<int>>();
-    // for (std::size_t i = 0; i < read_data_int.size(); ++i)
-    // {
-    //     assert(read_data_int[i].size() == data[i].z.size());
-    //     for (std::size_t j = 0; j < data[i].z.size(); ++j)
-    //     {
-    //         assert(read_data_int[i][j] == data[i].z[j]);
-    //     }
-    // }
-
-    // HDFAttribute attribute(*dataset, "testattribute");
-    // auto [shape, read_attribute] = attribute.read<std::string>();
-    // assert(read_attribute == "this is an attribute to a double dataset");
+    HDFAttribute attribute(*dataset1, "testattribute");
+    auto [shape, read_attribute] = attribute.read<std::string>();
+    assert(read_attribute == "this is an attribute to a double dataset");
 }
+
 int main()
 {
     // H5Eset_auto(error_stack, NULL, NULL); // turn off automatic error
