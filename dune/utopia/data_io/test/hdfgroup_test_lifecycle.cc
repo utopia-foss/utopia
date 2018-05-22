f2c6f67e4e95b377ccce62209ce2db8e1b876e5a
Lukas Riedel <mail@lukasriedel.com>
2018-05-22 19:55:06 +0200

    Move assertions in hdfgroup_test_lifecycle into separate function

--- dune/utopia/data_io/test/hdfgroup_test_lifecycle.cc
+++ dune/utopia/data_io/test/hdfgroup_test_lifecycle.cc
@@ -9,6 +9,15 @@
 
 using namespace Utopia::DataIO;
 
+/// Asserts if two HDFGroup instances have the same public members
+void assert_hdfgroups(HDFGroup& lhs, HDFGroup& rhs)
+{
+    assert(lhs.get_path() == rhs.get_path());
+    assert(lhs.get_id() == rhs.get_id());
+    assert(lhs.get_address() == rhs.get_address());
+    assert(lhs.get_referencecounter() == rhs.get_referencecounter());
+}
+
 int main()
 {
     HDFFile file("group_test_lifetime.h5", "w");
@@ -19,38 +28,23 @@ int main()
 
     HDFGroup copied_first(first);
     assert((*copied_first.get_referencecounter())[copied_first.get_address()] == 2);
-
-    assert(first.get_path() == copied_first.get_path());
-    assert(first.get_id() == copied_first.get_id());
-    assert(first.get_address() == copied_first.get_address());
-    assert(first.get_referencecounter() == copied_first.get_referencecounter());
+    assert_hdfgroups(first, copied_first);
 
     auto second = first;
     assert((*second.get_referencecounter())[second.get_address()] == 3);
-
-    assert(first.get_path() == second.get_path());
-    assert(first.get_id() == second.get_id());
-    assert(first.get_address() == second.get_address());
-    assert(first.get_referencecounter() == second.get_referencecounter());
+    assert_hdfgroups(first, second);
 
     auto crosscheck(first); // this is needed  for checks
     crosscheck.close();     // but should  not take part in refcount anymore
     auto moveassign_from_first = std::move(first);
     assert((*moveassign_from_first.get_referencecounter())[moveassign_from_first.get_address()] == 3);
 
-    assert(crosscheck.get_path() == moveassign_from_first.get_path());
-    assert(crosscheck.get_id() == moveassign_from_first.get_id());
-    assert(crosscheck.get_address() == moveassign_from_first.get_address());
-    assert(crosscheck.get_referencecounter() == moveassign_from_first.get_referencecounter());
+    assert_hdfgroups(crosscheck, moveassign_from_first);
 
     HDFGroup moveconst_second(std::move(second));
 
     assert((*moveconst_second.get_referencecounter())[moveconst_second.get_address()] == 3);
-
-    assert(crosscheck.get_path() == moveconst_second.get_path());
-    assert(crosscheck.get_id() == moveconst_second.get_id());
-    assert(crosscheck.get_address() == moveconst_second.get_address());
-    assert(crosscheck.get_referencecounter() == moveconst_second.get_referencecounter());
+    assert_hdfgroups(crosscheck, moveconst_second);
 
     return 0;
 }
