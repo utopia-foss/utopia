fc034e8b387ecb68f2fa7b4ba13f55b04f749acc
Yunus Sevinchan <YunusSevinchan@gmail.com>
2018-06-02 19:34:06 +0200

    Also test other constructors

--- dune/utopia/core/test/model_setup_test.cc
+++ dune/utopia/core/test/model_setup_test.cc
@@ -8,17 +8,45 @@ int main(int argc, char *argv[])
     try {
         Dune::MPIHelper::instance(argc,argv);
 
-        // create pseudo model that will be used as "parent" for the model here
-        Utopia::PseudoParent pp("model_setup_test.yml");
+        // create pseudo models that will be used as "parent" for the model
+        // initializations. Do so with the two different possible constructors
+        // only via config file:
+        Utopia::PseudoParent pp1("model_setup_test.yml");
+        
+        // more granular:
+        Utopia::PseudoParent pp2("model_setup_test.yml",         // config
+                                 "model_setup_test_tmpfile2.h5", // output
+                                 23,                             // seed
+                                 "x");                           // access mode
+        
+        // custom RNG via class template deduction
+        Utopia::PseudoParent<std::ranlux48_base> pp3             // RNG class
+                                ("model_setup_test.yml",         // config
+                                 "model_setup_test_tmpfile3.h5", // output
+                                 42,                             // seed
+                                 "x");                           // access mode
+        // NOTE could also use the simple constructor here, but need to specify
+        // a new temporary file for output writing
 
-        // initialize the actual model
-        Utopia::DoNothingModel model("my_model", pp);
-        std::cout << "Initialization succeeded." << std::endl;
+        // initialize the actual models using the different pseudo parents
+        Utopia::DoNothingModel model1("model1", pp1);
+        Utopia::DoNothingModel model2("model2", pp2);
+        Utopia::DoNothingModel model3("model3", pp2);
 
-        // clean up temporary file
-        pp.hdffile.close();
-        std::remove(pp.hdffile.get_path().c_str());
-        std::cout << "Temporary file removed." << std::endl;
+        std::cout << "Initialization of models via pseudo parents succeeded."
+                  << std::endl;
+
+        // clean up temporary files
+        pp1.hdffile.close();
+        std::remove(pp1.hdffile.get_path().c_str());
+        
+        pp2.hdffile.close();
+        std::remove(pp2.hdffile.get_path().c_str());
+        
+        pp3.hdffile.close();
+        std::remove(pp3.hdffile.get_path().c_str());
+
+        std::cout << "Temporary files removed." << std::endl;
     }
     catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
