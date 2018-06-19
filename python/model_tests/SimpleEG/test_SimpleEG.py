9903ec1ff4ef5e0eb3051bde7c672cd2d46888bf
Yunus Sevinchan <YunusSevinchan@gmail.com>
2018-06-19 14:07:16 +0200

    Rework SimpleEG model tests

--- python/model_tests/SimpleEG/test_SimpleEG.py
+++ python/model_tests/SimpleEG/test_SimpleEG.py
@@ -2,42 +2,56 @@
 
 import pytest
 
-from ..testtools import *
+from utopya.testtools import ModelTest
 
-# Configure the ModelTest class for dummy
-mtc = ModelTest("SimpleEG")
+# Configure the ModelTest class
+mtc = ModelTest("SimpleEG", test_file=__file__)
 
 # Fixtures --------------------------------------------------------------------
-# Define fixtures
+# Define fixtures here
+
+
+# Helpers ---------------------------------------------------------------------
+
+def set_initial_state(state: str, *, s1_fraction: float=None) -> dict:
+    """Creates a dict that can be used to set the initial state"""
+    d = dict(parameter_space=dict(SimpleEG=dict(initial_state=state)))
+
+    if s1_fraction is not None:
+        d['parameter_space']['SimpleEG']['s1_fraction'] = s1_fraction
+
+    return d
 
 
 # Tests -----------------------------------------------------------------------
 
-def test_basic():
-    """Test that the SimpleEG model basically runs"""
-    mv = mtc.create_mv_from_cfg("basic")
-    mv.run()
+def test_basics():
+    """Test the most basic features of the model, e.g. that it runs"""
+    # Create a Multiverse using the default model configuration
+    mv = mtc.create_mv()
 
-    # Load data using data manager
+    # Run a single simulation
+    mv.run_single()
+
+    # Load data using the DataManager
+    mv.dm.load_from_cfg(print_tree=True)
+    # The `print_tree` flag creates output of which data was loaded
+
+    # Assert that data was loaded, i.e. that data was written
+    assert len(mv.dm)
+
+
+def test_initial_states(): 
+    """Test that the initial states are"""
+    # Use the config file for common settings, change it via additional kwargs
+    mv1 = mtc.create_mv_from_cfg("initial_state.yml",
+                                 **set_initial_state('random'))
+
+    # Run the simulation (initial step only)
+    mv.run_sweep()
+
+    # Load data
     mv.dm.load_from_cfg(print_tree=True)
 
-    # Assert that data was written
-    assert len(mv.dm) > 0
-
-    # TODO add more assertions here
-
-def test_init():
-    """Test that the initial conditions are implemented as desired"""
-    # Single S0 in the center .................................................
-    # Initialize Multiverse, run simulation, load data using DataManager
-    mv_single_s0 = mtc.create_mv_from_cfg("single_s0")
-    mv_single_s0.run()
-    dm_s0 = mv_single_s0.dm
-    dm_s0.load_from_cfg()
-
-    # Now have all written data available under dict-like dm_s0
-    # TODO do assertions here ...
-
-    # Single S1 in the center .................................................
-    mv_single_s1 = mtc.create_mv_from_cfg("single_s1")    
-    # TODO continue as above ...
+    # TODO add assertions below ...
+    assert False
