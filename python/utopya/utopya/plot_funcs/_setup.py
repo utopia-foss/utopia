fa04505bd23783f88299fa04102f75e6b38f5026
Yunus Sevinchan <yunus.sevinchan@iup.uni-heidelberg.de>
2018-07-04 14:57:14 +0000

    Use the dantro plotting framework

---
+++ python/utopya/utopya/plot_funcs/_setup.py
@@ -0,0 +1,18 @@
+"""This module sets up matplotlib to use in other plotting modules"""
+
+import logging
+
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+# Import some names from utopya
+from .. import DataManager
+
+# Local constants
+log = logging.getLogger(__name__)
+
+
+# -----------------------------------------------------------------------------
+# Can define helper functions here
+
