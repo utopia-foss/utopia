7cd414c2be62993c6d8542f88bb91a5c45b14b7c
Josephine Westermann <jwesterm@iup.uni-heidelberg.de>
2018-07-04 20:18:11 +0200

    Removed unnecessary sleep statement :D

--- dune/utopia/models/vegetation/vegetation.cc
+++ dune/utopia/models/vegetation/vegetation.cc
@@ -1,7 +1,3 @@
-#include <iostream>       // std::cout, std::endl
-#include <thread>         // std::this_thread::sleep_for
-#include <chrono>         // std::chrono::seconds
-
 #include <dune/utopia/core/setup.hh>
 #include <dune/utopia/data_io/types.hh>
 
@@ -12,11 +8,8 @@ int main (int argc, char** argv)
     try {
         Dune::MPIHelper::instance(argc, argv);
 
-        // Read in the config file
-        const std::string config_file = argv[1];
-
-        // Create PseudoParent
-        Utopia::PseudoParent pp(config_file);
+        // Create PseudoParent and config file reference
+        Utopia::PseudoParent pp(argv[1]);
         auto config = pp.get_cfg();
 
         // Create the manager instance
@@ -39,32 +32,6 @@ int main (int argc, char** argv)
         for(int i = 0; i < num_steps; ++i) {
             model.iterate();
         }
-
-        // Test set_boundary_condition
-        /*std::cout << "Testing set_boundary_condition ..." << std::endl;
-        std::normal_distribution<> rain{10.1,1.2};
-        auto bc = std::make_tuple(rain, 0.2, 0.2);
-        model.set_boundary_condition(bc);
-
-        // Test data
-        std::cout << "Testing get data ..." << std::endl;
-        auto d = model.data();
-
-        // Test set_initial_condition
-        std::cout << "Testing set_initial_condition ..." << std::endl;
-        model.set_initial_condition(d);*/
-        
-
-        // Sleep (to be read by frontend)
-        unsigned int sleep_time = 300; // in milliseconds
-        unsigned int num_sleeps = 3;
-
-        for (unsigned int i = 0; i < num_sleeps; ++i) {
-            std::cout << "Sleep #" << (i+1) << " ..." << std::endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
-        }
-
-        std::cout << "All done." << std::endl << "Really." << std::endl;
 
         return 0;
     }
