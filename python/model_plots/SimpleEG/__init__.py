d9d918753f975f6120f5b033d46209862bc6c2bd
Benjamin Herdeanu <herdeanu@iup.uni-heidelberg.de>
2018-07-04 17:41:37 +0200

    Add directory in model_plots

---
+++ python/model_plots/SimpleEG/__init__.py
@@ -0,0 +1,4 @@
+"""Plots that are specific to the SimpleEG model"""
+
+# Make them all available here to allow easier import
+from .state import *
---
+++ python/model_plots/SimpleEG/frequency.py
@@ -0,0 +1,11 @@
+"""Dummy-model specific plot function for the state"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from utopya import DataManager
+
+from ..tools import save_and_close
+
+# -----------------------------------------------------------------------------
+
