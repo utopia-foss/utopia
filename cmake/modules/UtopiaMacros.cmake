3e941453709b798a646ceaf18763a3ba6bffca75
Harald Mack <harald.mack@iup.uni-heidelberg.de>
2018-07-09 16:24:49 +0000

    Resolve "Cmake error "Cannot specify link libraries for target "output_epswriter_test" which is  not built by this project."  when having psgraf installed"

--- cmake/modules/UtopiaMacros.cmake
+++ cmake/modules/UtopiaMacros.cmake
@@ -8,19 +8,6 @@ find_package(HDF5 1.10 REQUIRED COMPONENTS C HL)
 include_directories(${HDF5_INCLUDE_DIRS})
 list(APPEND DUNE_LIBS ${HDF5_LIBRARIES} ${HDF5_HL_LIBRARIES})
 
-# File for module specific CMake tests.
-if(NOT PSGRAF_ROOT)
-	set(PSGRAF_ROOT)
-endif()
-find_package(PSGraf CONFIG
-	HINTS ${PSGRAF_ROOT})
-
-if(PSGRAF_FOUND)
-message(STATUS "Found PSGraf: ${PSGRAF_LIBRARIES}")
-	include_directories(${PSGRAF_INCLUDE_DIRS})
-	list(APPEND DUNE_LIBS ${PSGRAF_LIBRARIES})
-	add_definitions(-DHAVE_PSGRAF)
-endif()
 
 # include Utopia macros
 include(PythonInstallPackageRemote)
