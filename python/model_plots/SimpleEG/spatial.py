1775d08b3aaf5f9501552fc06688057e6f18e37a
Benjamin Herdeanu <herdeanu@iup.uni-heidelberg.de>
2018-07-09 19:45:01 +0200

    Fix and tweak grid_animation function

--- python/model_plots/SimpleEG/spatial.py
+++ python/model_plots/SimpleEG/spatial.py
@@ -16,9 +16,10 @@ def grid_animation( dm: DataManager,
                     *, 
                     out_path: str, 
                     uni: int, 
-                    properties: Union[list, str], 
-                    cmaps: Union[list, str], 
-                    rngs: Union[list, str], 
+                    to_plot: dict,
+                    # properties: Union[list, str], 
+                    # cmaps: Union[list, str], 
+                    # rngs: Union[list, str], 
                     fps: int=1, 
                     step_size: int=1,
                     dpi: int=100):
@@ -31,6 +32,19 @@ def grid_animation( dm: DataManager,
     step -- number of time steps between frames (e.g. if step=5, only each fifth 
             dataset will be used for the animation)
     """    
+    def plot_property(name, *, initial_data, ax, cmap, limits, title=None):
+        # Create imshow
+        im = ax.imshow(initial_data, cmap=cmap, animated=True, origin='lower', vmin=limits[0], vmax=limits[1])
+
+        if title is not None:
+            ax.set_title(title)
+
+        # Create colorbars
+        fig = plt.gcf()
+        fig.colorbar(im ,ax=ax, fraction=0.046, pad=0.04)
+        ax.axis('off')
+        return im
+
     # Get the group that all datasets are in
     grp = dm['uni'][str(uni)]['data/SimpleEG']
 
@@ -40,47 +54,41 @@ def grid_animation( dm: DataManager,
     steps = cfg['num_steps']
     new_shape = (steps+1, grid_size[0], grid_size[1])
 
+    # Extract the data of the strategies in the CA    
+    data_1d = {p: grp[p] for p in to_plot.keys()}
+    data = {k: np.reshape(v, new_shape) for k,v in data_1d.items()}
+        
     # create animation writer
-    writer = manimation.writers['ffmpeg'](fps=4, metadata={'title' : 'Grid Animation for {}'.format("_".join(properties)), 
-                                                           'artist' : 'Utopia',
-                                                           'comment' : 'This is a comment'})
-    
+    writer = manimation.writers['ffmpeg'](fps=4, metadata={'title' : 'Grid Animation for {}'.format("_".join(to_plot.keys())), 
+                                                           'artist' : 'Utopia'})
+
     # Set plot parameters
-    rcParams.update({'font.size': 20})
-    rcParams['figure.figsize'] = (6.0*len(properties), 5.0)
+    # rcParams.update({'font.size': 20})
+    rcParams['figure.figsize'] = (6.0*len(to_plot), 5.0)
 
     # Create figure
-    fig, axs = plt.subplots(1,len(properties))
+    fig, axs = plt.subplots(1,len(to_plot))
 
-    # Assert that the loop can be run even if there is only one property
-    if len(properties) == 1 :
+    # Assert that the axes are stored in a list even if it is only one axis.
+    if len(to_plot) == 1 :
          axs = [axs] 
-
-    # Extract the data of the strategies in the CA    
-    data_1d = {p: grp[p] for p in properties}
-    data = {k: np.reshape(v, new_shape) for k,v in data_1d.items()}
-
+    
+    # store 
+    ims = []
+    
     with writer.saving(fig, out_path, dpi=dpi):
+        
+        for t in range(steps):
 
-        for key, d in data.items():
+            for i, (ax, (key, props)) in enumerate(zip(axs, to_plot.items())):
 
-            for t in range(steps):
-                cbs = []
-                # iterate over all properties
-                for j in range(len(properties)):
-                    axs[j].cla()
-                    
-                    if len(properties) == 1:
-                        im = axs[j].imshow(d[t], cmap=cmaps[j], animated=True, origin='lower', vmin=rngs[j][0], vmax=rngs[j][1])
-                    else:
-                        im = axs[j].imshow(d[t], cmap=cmaps[j], animated=True, origin='lower', vmin=rngs[j][0], vmax=rngs[j][1])
+                    if t == 0:
+                        ims.append(plot_property(key, initial_data=data[key][t], ax=ax, **props))
+                        continue
 
-                    # fraction and pad get magic values that makes the plots look nice
-                    cbs.append(fig.colorbar(im ,ax=axs[j], fraction=0.046, pad=0.04))
-                    axs[j].axis('off')
+                    # update imshow data
+                    ims[i].set_data(data[key][t])
 
-                writer.grab_frame()
-                
-                # clean up the colorbars (needed to prevent the colorbars from piling up from frame to frame)
-                for j in range(len(properties)) :
-                    cbs[j].remove()
+                    # colorbar update data???                        
+
+            writer.grab_frame()
