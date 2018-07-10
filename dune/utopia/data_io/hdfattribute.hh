54adf860709e9008128d9f505a8fb0366716528b
Harald Mack <harald.mack@iup.uni-heidelberg.de>
2018-07-10 13:41:39 +0200

    added functionality to exploit const-size hdf5 array types in attribute  for writing instead of always writing hvl_t

--- dune/utopia/data_io/hdfattribute.hh
+++ dune/utopia/data_io/hdfattribute.hh
@@ -61,6 +61,7 @@ private:
     {
         using value_type_1 = typename Type::value_type;
         using base_type = remove_qualifier_t<value_type_1>;
+
         // we can write directly if we have a plain vector, no nested or stringtype.
         if constexpr (std::is_same_v<Type, std::vector<value_type_1>> &&
                       !is_container_v<value_type_1> && !is_string_v<value_type_1>)
@@ -78,17 +79,37 @@ private:
         // we have to buffer. bufferfactory handles how to do this in detail
         else
         {
-            if (_attribute == -1)
+            // if we want to write std::arrays then we can use
+            // fixed size array types. Else we use variable length arrays.
+            if constexpr (is_array_like_v<result_type>)
             {
-                _attribute = __create_attribute__<base_type>(0);
+                constexpr std::size_t s = std::tuple_size<result_type>::value;
+                if (attribute == -1)
+                {
+                    _attribute = __create_attribute__<base_type>(s);
+                }
+
+                auto buffer = HDFBufferFactory::buffer(
+                    std::begin(attribute_data), std::end(attribute_data),
+                    [](auto& value) -> value_type_1& { return value; });
+
+                return H5Awrite(_attribute, HDFTypeFactory::type<base_type>(s),
+                                buffer.data());
             }
+            else
+            {
+                if (_attribute == -1)
+                {
+                    _attribute = __create_attribute__<base_type>();
+                }
 
-            auto buffer = HDFBufferFactory::buffer(
-                std::begin(attribute_data), std::end(attribute_data),
-                [](auto& value) -> value_type_1& { return value; });
+                auto buffer = HDFBufferFactory::buffer(
+                    std::begin(attribute_data), std::end(attribute_data),
+                    [](auto& value) -> value_type_1& { return value; });
 
-            return H5Awrite(_attribute, HDFTypeFactory::type<base_type>(0),
-                            buffer.data());
+                return H5Awrite(_attribute, HDFTypeFactory::type<base_type>(0),
+                                buffer.data());
+            }
         }
     }
 
@@ -182,13 +203,31 @@ private:
                 throw std::runtime_error("Can only read data from " + _name +
                                          " into vector containers!");
             }
-            else // if we have vectors, the reading process is straight forward
-            {
-                // get type the attribute has internally
-                hid_t type = H5Aget_type(_attribute);
 
-                // assume array data to always be varlen, because
-                // this project can write only varlen data
+            // if we have nested containers of depth larger than 2, throw a
+            // runtime error because we cannot handle this
+            if constexpr (is_container_v<value_type2>)
+            {
+                throw std::runtime_error("Attribute " + _name + " cannot read into nested 
+                                        container with a nesting depth  > 2");
+            }
+
+            // everything is fine.
+
+            // get type the attribute has internally
+            hid_t type = H5Aget_type(_attribute);
+
+            // check if type given in the buffer is std::array.
+            // If it is, the user knew that the data stored there
+            // has always the same length, otherwise she does not
+            // know and thus it is assumed that the data is variable
+            // length.
+            if (is_array_like_v<value_type1>)
+            {
+                return H5Aread(_attribute, type, buffer.data());
+            }
+            else
+            {
                 std::vector<hvl_t> temp_buffer(buffer.size());
 
                 herr_t err = H5Aread(_attribute, type, temp_buffer.data());
--- dune/utopia/data_io/hdfutilities.hh
+++ dune/utopia/data_io/hdfutilities.hh
@@ -53,14 +53,6 @@ struct remove_pointer<T, std::enable_if_t<std::is_array_v<T>, std::void_t<>>>
     using type = typename remove_pointer<std::remove_all_extents_t<T>>::type;
 };
 
-/**
- * @brief Shorthand for 'typename remove_pointer<T>::type'
- *
- * @tparam T
- */
-template <typename T>
-using remove_pointer_t = typename remove_pointer<T>::type;
-
 // remove qualifiers. FIXME: this is not optimal currently, because it does not
 // work recursivly
 
@@ -74,14 +66,6 @@ struct remove_qualifier
 {
     using type = std::remove_cv_t<remove_pointer_t<std::remove_reference_t<T>>>;
 };
-
-/**
- * @brief Shorthand for 'typename remove_qualifier::value'
- *
- * @tparam T
- */
-template <typename T>
-using remove_qualifier_t = typename remove_qualifier<T>::type;
 
 /**
  * @brief Meta-function for checking if a type is a stringtype, that is
@@ -135,15 +119,6 @@ struct is_string<char*> : public std::true_type // public is_string_helper<char*
 };
 
 /**
- * @brief Shorthand for 'is_string<T>::value'
- *
- * @tparam     T     { description }
- * @tparam     U     { description }
- */
-template <typename T>
-constexpr inline bool is_string_v = is_string<T>::value;
-
-/**
  * @brief Metafunction for checking if a type is a containertype, which does
  *        not include string types - prototype
  *
@@ -168,12 +143,68 @@ struct is_container<T, std::void_t<typename remove_qualifier_t<T>::iterator, std
 };
 
 /**
+ * @brief Prototype for is_array_like
+ *
+ * @tparam T
+ * @tparam std::void_t<>
+ */
+template <typename T, typename U = std::void_t<>>
+struct is_array_like : std::false_type
+{
+};
+
+/**
+ * @brief Metafunction for checking if a container is a std::array
+ *        by checking if std::tuple_size can be applied to it.
+ *
+ *
+ * @tparam T
+ */
+template <typename T>
+struct is_array_like<T, std::void_t<decltype(std::tuple_size<T>::value)>> : std::true_type
+{
+};
+
+/**
+ * @brief Shorthand for is_array_like
+ *
+ * @tparam T
+ */
+template <typename T>
+inline constexpr bool is_array_like_v = is_array_like<T>::value;
+
+/**
+ * @brief Shorthand for 'is_string<T>::value'
+ *
+ * @tparam     T     { description }
+ * @tparam     U     { description }
+ */
+template <typename T>
+constexpr inline bool is_string_v = is_string<T>::value;
+
+/**
  * @brief Shorthand for 'is_container::value
  *
  * @tparam T
  */
 template <typename T>
 inline constexpr bool is_container_v = is_container<T>::value;
+
+/**
+ * @brief Shorthand for 'typename remove_qualifier::value'
+ *
+ * @tparam T
+ */
+template <typename T>
+using remove_qualifier_t = typename remove_qualifier<T>::type;
+
+/**
+ * @brief Shorthand for 'typename remove_pointer<T>::type'
+ *
+ * @tparam T
+ */
+template <typename T>
+using remove_pointer_t = typename remove_pointer<T>::type;
 
 } // namespace DataIO
 } // namespace Utopia
