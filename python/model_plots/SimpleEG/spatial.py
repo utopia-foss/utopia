60b566f7c295a521e8bc11731f845345bdb2d561
Yunus Sevinchan <yunussevinchan@gmail.com>
2018-07-10 15:47:17 +0200

    Tweak SimpleEG plots

--- python/model_plots/SimpleEG/spatial.py
+++ python/model_plots/SimpleEG/spatial.py
@@ -1,4 +1,6 @@
 """SimpleEG-model specific plot functions for spatial figures"""
+
+import logging
 from typing import Union
 
 import numpy as np
@@ -8,19 +10,10 @@ from matplotlib import rcParams
 
 from utopya import DataManager
 
+# Get a logger
+log = logging.getLogger(__name__)
+
 # -----------------------------------------------------------------------------
-
-class FileWriterContextManager():
-    def __init__(self, *, fig, out_path, dpi):
-        self.fig = fig
-        self.out_path = out_path
-        self.dpi = dpi
-
-    def __enter__(self):
-        pass
-
-    def __exit__(self, *args):
-        plt.close(self.fig)
 
 class FileWriter():
     """The FileWriter class yields functionality to save individual frames."""
@@ -39,37 +32,46 @@ class FileWriter():
         self.cm.fig.savefig(out_path, format=self.frame_format, dpi=self.cm.dpi, **savefig_kwargs)
         self.index = self.index + 1
 
+class FileWriterContextManager():
+    """This class is needed by the file writer to provide the same interface
+    as the matplotlib movie writers do.
+    """
+
+    def __init__(self, *, fig, out_path, dpi):
+        self.fig = fig
+        self.out_path = out_path
+        self.dpi = dpi
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, *args):
+        plt.close(self.fig)
+
 # -----------------------------------------------------------------------------
 
-def grid_animation( dm: DataManager, 
-                    *, 
-                    out_path: str, 
-                    uni: int, 
-                    to_plot: dict,
-                    writer: str,
-                    frames: dict,
-                    fps: int=1, 
-                    step_size: int=1,
-                    dpi: int=100):
+def grid_animation(dm: DataManager,  *, out_path: str, uni: int, to_plot: dict, writer: str, frames: dict, fps: int=2, step_size: int=1, dpi: int=96) -> None:
     """Create a grid animation of a two dimensional cellular automaton
     
     Arguments:
-        dm {DataManager} -- The DataManager object containing the data
-        out_path {str} -- The output path
-        uni {int} -- The universum
-        to_plot {dict} -- The plotting configuration
-        writer {str} -- The writer that should be used. Additional to the external writers such as 'ffmpeg', 
-                        it is possible to create an dsave the individual frames with 'frames'
-        frames {dict} -- The frames configuration that is used if writer='frames'
+        dm (DataManager): The DataManager object containing the data
+        out_path (str): The output path
+        uni (int): The universum
+        to_plot (dict): The plotting configuration
+        writer (str): The writer that should be used. Additional to the
+            external writers such as 'ffmpeg', it is possible to create an
+            dsave the individual frames with 'frames'
+        frames (dict): The frames configuration that is used if writer='frames'
+        fps (int, optional): The frames per second
+        step_size (int, optional): The step size
+        dpi (int, optional): The dpi setting
     
-    Keyword Arguments:
-        fps {int} -- The frames per second (default: {1})
-        step_size {int} -- The step size (default: {1})
-        dpi {int} -- The dpi setting (default: {100})
+    Returns:
+        None
     """
 
     def plot_property(name, *, initial_data, ax, cmap, limits, title=None):
-        """Plot a property on a given axis"""
+        """Helper function to plot a property on a given axis"""
         # Create imshow
         im = ax.imshow(initial_data, cmap=cmap, animated=True, origin='lower', vmin=limits[0], vmax=limits[1])
 
@@ -96,30 +98,38 @@ def grid_animation( dm: DataManager,
     data_1d = {p: grp[p] for p in to_plot.keys()}
     data = {k: np.reshape(v, new_shape) for k,v in data_1d.items()}
         
-    # create animation writer if the writer is available
+    # Distinguish writer classes
     if anim.writers.is_available(writer):
-        w = anim.writers[writer](fps=4, metadata={'title' : 'Grid Animation for {}'.format("_".join(to_plot.keys())), 
-                                                        'artist' : 'Utopia'})
-    # If the writer is set to 'frames' just create the frames and save them later
+        # Create animation writer if the writer is available
+        wCls = anim.writers[writer]
+        w = WCls(fps=fps,
+                 metadata=dict(title="Grid Animation — {}"
+                                     "".format(", ".join(to_plot.keys()),
+                               artist="Utopia")))
+
     elif writer == 'frames':
-        w = FileWriter(frame_name_padding=frames['name_padding'], frame_format=frames['format'])
-    # Case: no valid writer
+        # Just create the frames, save them later
+        w = FileWriter(frame_name_padding=frames['name_padding'],
+                       frame_format=frames['format'])
+
     else:
-        print("The writer {} is not available on your system!".format(writer))
+        log.error("The writer '%s' is not available on your system!"
+                  "".format(writer))
         return
 
     # Set plot parameters
     rcParams.update({'font.size': 20})
-    rcParams['figure.figsize'] = (6.0*len(to_plot), 5.0)
+    rcParams['figure.figsize'] = (6.0 * len(to_plot), 5.0)
 
     # Create figure
-    fig, axs = plt.subplots(1,len(to_plot))
+    fig, axs = plt.subplots(1, len(to_plot))
 
     # Assert that the axes are stored in a list even if it is only one axis.
-    if len(to_plot) == 1 :
+    if len(to_plot) == 1:
          axs = [axs] 
     
-    # store imshow objects such that only the data has to be updated in a following iteration step
+    # Store the imshow objects such that only the data has to be updated in a
+    # following iteration step
     ims = []
     
     with w.saving(fig, out_path, dpi=dpi):
@@ -129,13 +139,20 @@ def grid_animation( dm: DataManager,
 
             # Loop through the subfigures
             for i, (ax, (key, props)) in enumerate(zip(axs, to_plot.items())):
-
                     # In the first time step create a new imshow object
                     if t == 0:
-                        ims.append(plot_property(key, initial_data=data[key][t], ax=ax, **props))
+                        im = plot_property(key,
+                                           initial_data=data[key][t],
+                                           ax=ax, **props)
+                        ims.append(im)
+
+                        # Important to not continue with the rest
                         continue
 
-                    # In later steps just update imshow data without creating a new object
+                    # In later steps just update imshow data without creating
+                    # a new object
                     ims[i].set_data(data[key][t])                  
             
+            # Updated both subfigures now
+            # Tell the writer that the frame is finished
             w.grab_frame()
