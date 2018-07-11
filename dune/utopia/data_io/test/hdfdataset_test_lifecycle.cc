d941b422afbbc8bafc4bb8b518aae91c2321365d
Harald Mack <hmack@iup.uni-heidelberg.de>
2018-07-11 11:26:05 +0200

    added tests for new constructors in hdfdataset_test_lifecycle

--- dune/utopia/data_io/test/hdfdataset_test_lifecycle.cc
+++ dune/utopia/data_io/test/hdfdataset_test_lifecycle.cc
@@ -20,6 +20,51 @@ using namespace Utopia::DataIO;
  *  \tparam RHS Right hand side dataset type
  *  Template parameters are necessary because HDFDataset is a template
  */
+template <typename T>
+bool operator==(std::vector<T>& a, std::vector<T>& b)
+{
+    if (a.size() == b.size())
+    {
+        return false;
+    }
+    else
+    {
+        if (std::is_floating_point<T>::value)
+        {
+            for (std::size_t i = 0; i < a.size(); ++i)
+            {
+                if (std::abs((a[i] - b[i]) / max(a[i], b[i])) < 1e-16)
+                {
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            for (std::size_t i = 0; i < a.size(); ++i)
+            {
+                if (a[i] != b[i])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+bool operator==(HDFGroup& a, HDFGroup& b)
+{
+    if (a.get_path() != b.get_path() or a.get_address() != b.get_address())
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
 template <class LHS, class RHS>
 void assert_hdfdatasets(LHS& lhs, RHS& rhs)
 {
@@ -28,16 +73,12 @@ void assert_hdfdatasets(LHS& lhs, RHS& rhs)
     assert(lhs.get_address() == rhs.get_address());
     assert(lhs.get_referencecounter() == rhs.get_referencecounter());
     assert(&lhs.get_parent() == &rhs.get_parent());
+    assert(lhs.get_parent() == rhs.get_parent());
     assert(lhs.get_rank() == rhs.get_rank());
-
-    // assert ranked members
-    for (std::size_t i = 0; i < lhs.get_rank(); ++i)
-    {
-        std::cerr << lhs.get_rank() << "," << lhs.get_current_extend()[i] << ","
-                  << rhs.get_current_extend()[i] << std::endl;
-        assert(lhs.get_current_extend()[i] == rhs.get_current_extend()[i]);
-        assert(lhs.get_capacity()[i] == rhs.get_capacity()[i]);
-    }
+    assert(lhs.get_capacity() == rhs.get_capacity());
+    assert(lhs.get_current_extend() == rhs.get_current_extend());
+    assert(lhs.get_chunksizes() == rhs.get_chunksizes());
+    assert(lhs.get_compresslevel() == rhs.get_compresslevel());
 }
 
 int main()
@@ -47,14 +88,24 @@ int main()
     HDFGroup lifecyclegroup(*file.get_basegroup(), "livecycletest");
     std::vector<int> data(100, 42);
 
-    HDFDataset first(lifecyclegroup, "first");
+    HDFDataset first(lifecyclegroup, [](auto& value) { return value; }, "first",
+                     3, {100, 120, 140}, {10, 20, 20}, 5);
 
-    first.write(data.begin(), data.end(), [](auto& value) { return value; });
+    HDFDataset first_simple(lifecyclegroup, [](auto& value) { return value; },
+                            "first", 3);
+
+    first.write(data.begin(), data.end());
+    first_simple.write(data.begin(), data.end());
+
+    assert(H5Iis_valid(first.get_id()));
+    assert(H5Iis_valid(first_simple.get_id()));
+
+    assert((*first_simple.get_referencecounter())[first_simple.get_address()] == 1);
 
     assert((*first.get_referencecounter())[first.get_address()] == 1);
 
     // copy constructor
-    HDFDataset copied_first(first);
+    auto copied_first(first);
     assert((*copied_first.get_referencecounter())[copied_first.get_address()] == 2);
     assert_hdfdatasets(first, copied_first);
 
@@ -71,7 +122,7 @@ int main()
     assert_hdfdatasets(crosscheck, moveassign_from_first);
 
     // move constructor
-    HDFDataset moveconst_second(std::move(second));
+    auto moveconst_second(std::move(second));
     assert((*moveconst_second.get_referencecounter())[moveconst_second.get_address()] == 3);
     assert_hdfdatasets(crosscheck, moveconst_second);
 
@@ -82,8 +133,22 @@ int main()
     }
     assert(H5Iis_valid(moveconst_second.get_id()) == false);
 
-    HDFDataset<HDFGroup> opened_dataset;
-    opened_dataset.open(moveconst_second.get_parent(), moveconst_second.get_name());
+    HDFDataset<HDFGroup, std::function<int(int&)>> opened_dataset;
+    opened_dataset.open(moveconst_second.get_parent(), moveconst_second.get_name(),
+                        [](auto& value) { return value; }, 3, {100, 120, 140},
+                        {10, 20, 20}, 5);
     assert(H5Iis_valid(opened_dataset.get_id()) == true);
+
+    // test simple open method
+    while (H5Iis_valid(first_simple.get_id()))
+    {
+        first_simple.close();
+    }
+    assert(H5Iis_valid(first_simple.get_id()) == false);
+
+    HDFDataset<HDFGroup, std::function<int(int&)>> opened_dataset_simple;
+    opened_dataset_simple.open(first_simple.get_parent(), first_simple.get_name(),
+                               [](auto& value) { return value; }, 3);
+    assert(H5Iis_valid(opened_dataset_simple.get_id()) == true);
     return 0;
 }
