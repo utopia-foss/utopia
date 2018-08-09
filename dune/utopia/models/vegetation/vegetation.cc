11f25578291b6b426c10aabe343161059459547c
Lukas Riedel <mail@lukasriedel.com>
2018-08-09 15:34:09 +0200

    Add setup_manager function for Vegetation model

--- dune/utopia/models/vegetation/vegetation.cc
+++ dune/utopia/models/vegetation/vegetation.cc
@@ -9,20 +9,10 @@ int main (int argc, char** argv)
 
         // Create PseudoParent and config file reference
         Utopia::PseudoParent pp(argv[1]);
-        auto config = pp.get_cfg();
-
-        // Create the manager instance
-        constexpr bool sync = true;
-        using State = double;
-        using Tag = Utopia::DefaultTag;
-        State initial_state = 0.0;
-        int grid_size = Utopia::as_<int>(config["vegetation"]["grid_size"]);
-        auto grid = Utopia::Setup::create_grid(grid_size);
-        auto cells = Utopia::Setup::create_cells_on_grid<sync, State, Tag>(grid, initial_state);
-        auto manager = Utopia::Setup::create_manager_cells<true, true>(grid, cells);
 
         // Set the initial state, then create the model instance
-        Utopia::Models::Vegetation::Vegetation model("vegetation", pp, manager);
+        Utopia::Models::Vegetation::Vegetation model("vegetation", pp,
+            Utopia::Models::Vegetation::setup_manager("vegetation", pp));
 
         // Just run!
         model.run();
