aefb39dac93799823be1fcbb48e204ab6c437413
Benjamin Herdeanu <b.herdeanu@web.de>
2018-08-19 14:50:02 +0200

    Enable save_graph also for arbitrary vertex container

--- dune/utopia/data_io/graph_utils.hh
+++ dune/utopia/data_io/graph_utils.hh
@@ -12,10 +12,18 @@ namespace Utopia {
 namespace DataIO {
 
 /// Write function for a static boost::Graph
+/**
+ * 
+ * @tparam true Saves the edges if true 
+ * @tparam GraphType 
+ * @param g The graph
+ * @param parent_grp The parent HDFGroup
+ * @param name The name of the graph group
+ * @return std::enable_if_t<save_edges, std::shared_ptr<HDFGroup>> The network data in a HDFGroup
+ */
 template<
     bool save_edges = true,
-    typename GraphType,
-    typename id_type=boost::vertex_index_t>
+    typename GraphType>
 std::enable_if_t<save_edges, std::shared_ptr<HDFGroup>>
 save_graph(GraphType &g,
          const std::shared_ptr<HDFGroup>& parent_grp,
@@ -44,7 +52,7 @@ save_graph(GraphType &g,
     // Save vertices list
     auto [v, v_end] = boost::vertices(g);
     dset_vl->write(v, v_end,
-        [&](auto vd){return boost::get(id_type(), g, vd);}
+        [&](auto vd){return boost::get(boost::vertex_index_t(), g, vd);}
     );
 
     // Save adjacency list
@@ -54,8 +62,8 @@ save_graph(GraphType &g,
             // Extract indices of source and target vertex as well as of the
             // edge this corresponds to.
             return std::array<std::size_t, 2>( // TODO use boost::index_type?
-                {boost::get(id_type(), g, boost::source(ed, g)),
-                 boost::get(id_type(), g, boost::target(ed, g))}
+                {boost::get(boost::vertex_index_t(), g, boost::source(ed, g)),
+                 boost::get(boost::vertex_index_t(), g, boost::target(ed, g))}
             );
         }
     );
@@ -64,8 +72,18 @@ save_graph(GraphType &g,
     return grp;
 }
 
+
 /// Write function for a static boost::Graph
-template<
+/**
+ * 
+ * @tparam false Saves the edges if true 
+ * @tparam GraphType 
+ * @param g The graph
+ * @param parent_grp The parent HDFGroup
+ * @param name The name of the graph group
+ * @return std::enable_if_t<save_edges, std::shared_ptr<HDFGroup>> The network data in a HDFGroup
+ */
+ template<
     bool save_edges = true,
     typename GraphType>
 std::enable_if_t<!save_edges, std::shared_ptr<HDFGroup>>
@@ -104,6 +122,133 @@ save_graph(GraphType &g,
     return grp;
 }
 
+
+/// Write function for a static boost::Graph
+/**
+ * @tparam true Saves the edges if true 
+ * @tparam PropertyMap The property map of the vertex ids
+ * @tparam GraphType 
+ * @param g The graph
+ * @param parent_grp The parent HDFGroup
+ * @param name The name of the graph group
+ * @return std::enable_if_t<save_edges, std::shared_ptr<HDFGroup>> The network data in a HDFGroup
+ */
+template<
+    bool save_edges = true,
+    typename PropertyMap,
+    typename GraphType>
+std::enable_if_t<save_edges, std::shared_ptr<HDFGroup>>
+save_graph(GraphType &g,
+         const std::shared_ptr<HDFGroup>& parent_grp,
+         const std::string& name,
+         const PropertyMap ids)
+{
+    // Create the group for the graph
+    auto grp = parent_grp->open_group(name);
+
+    // Store in attributes that this is supposed to be a Graph
+    grp->add_attribute("is_static_graph_group", true);
+
+    // Collect some metadata
+    auto num_vertices = boost::num_vertices(g);
+    auto num_edges = boost::num_edges(g);
+
+    // Also store some metadata
+    grp->add_attribute("directed", boost::is_directed(g));
+    grp->add_attribute("num_vertices", num_vertices);
+    grp->add_attribute("num_edges", num_edges);
+    // TODO _could_ add more attributes here, should be general though
+
+    // Initialize datasets to store vertices list and adjacency list in
+    auto dset_vl = grp->open_dataset("_vertex_list", {num_vertices});
+    auto dset_al = grp->open_dataset("_adjacency_list", {num_edges});
+
+    // Save vertices list
+    auto [v, v_end] = boost::vertices(g);
+    dset_vl->write(v, v_end,
+        [&](auto vd){return boost::get(ids, vd);}
+    );
+
+    // Save adjacency list
+    auto [e, e_end] = boost::edges(g);
+    dset_al->write(e, e_end,
+        [&](auto ed){
+            // Extract indices of source and target vertex as well as of the
+            // edge this corresponds to.
+            return std::array<std::size_t, 2>( // TODO use boost::index_type?
+                {boost::get(ids, boost::source(ed, g)),
+                 boost::get(ids, boost::target(ed, g))}
+            );
+        }
+    );
+
+    // Return newly created group
+    return grp;
+}
+
+
+/// Write function for a static boost::Graph
+/**
+ * @tparam true Saves the edges if true 
+ * @tparam PropertyMap The property map of the vertex ids
+ * @tparam GraphType 
+ * @param g The graph
+ * @param parent_grp The parent HDFGroup
+ * @param name The name of the graph group
+ * @return std::enable_if_t<save_edges, std::shared_ptr<HDFGroup>> The network data in a HDFGroup
+ */
+ template<
+    bool save_edges = false,
+    typename PropertyMap,
+    typename GraphType>
+std::enable_if_t<!save_edges, std::shared_ptr<HDFGroup>>
+save_graph(GraphType &g,
+         const std::shared_ptr<HDFGroup>& parent_grp,
+         const std::string& name,
+         const PropertyMap ids)
+{
+    // Create the group for the graph
+    auto grp = parent_grp->open_group(name);
+
+    // Store in attributes that this is supposed to be a Graph
+    grp->add_attribute("is_static_graph_group", true);
+
+    // Collect some metadata
+    auto num_vertices = boost::num_vertices(g);
+    auto num_edges = boost::num_edges(g);
+
+    // Also store some metadata
+    grp->add_attribute("directed", boost::is_directed(g));
+    grp->add_attribute("num_vertices", num_vertices);
+    grp->add_attribute("num_edges", num_edges);
+    // TODO _could_ add more attributes here, should be general though
+
+    // Initialize datasets to store vertices list and adjacency list in
+    auto dset_vl = grp->open_dataset("_vertex_list", {num_vertices});
+    auto dset_al = grp->open_dataset("_adjacency_list", {num_edges});
+
+    // Save vertices list
+    auto [v, v_end] = boost::vertices(g);
+    dset_vl->write(v, v_end,
+        [&](auto vd){return boost::get(ids, vd);}
+    );
+
+    // Save adjacency list
+    auto [e, e_end] = boost::edges(g);
+    dset_al->write(e, e_end,
+        [&](auto ed){
+            // Extract indices of source and target vertex as well as of the
+            // edge this corresponds to.
+            return std::array<std::size_t, 2>( // TODO use boost::index_type?
+                {boost::get(ids, boost::source(ed, g)),
+                 boost::get(ids, boost::target(ed, g))}
+            );
+        }
+    );
+
+    // Return newly created group
+    return grp;
+}
 
 
 } // namespace DataIO
--- dune/utopia/data_io/test/graph_utils_test.cc
+++ dune/utopia/data_io/test/graph_utils_test.cc
@@ -18,22 +18,31 @@ using namespace Utopia;
 
 /// Creates a small test graph
 template<typename GraphType>
-GraphType create_test_graph(const int num_vertices, const int num_edges){
+GraphType create_and_initialize_test_graph(const int num_vertices, const int num_edges){
     GraphType g;
 
     std::mt19937 rng(42);
 
-    // Add vertices
+    // Add vertices and initialize
     for (int i = 0; i < num_vertices; i++){
-        add_vertex(g);
+        // Add vertex
+        auto v = add_vertex(g);
+        
+        // Initialize vertex
+        g[v].test_int = 42;
+        g[v].test_double = 2.3;
+        g[v].id = 10;
     }
 
     // Randomly add edges
     for (int i = 0; i < num_edges; i++){
+        // Add random edge
         auto v1 = random_vertex(g, rng);
         auto v2 = random_vertex(g, rng);
-
-        add_edge(v1,v2,g);
+        auto e = add_edge(v1,v2,g);
+        
+        // Initialize edge
+        g[e.first].weight = 0.5;
     }
 
     // Return the graph
@@ -56,52 +65,51 @@ void test_save_graph()
     };
 
 
-    std::size_t id;
     // Create different graph types to be tested.
     using Graph1 =  boost::adjacency_list<
-                        boost::listS,            // edge container
-                        boost::vecS,            // vertex container
+                        boost::vecS,             // edge container
+                        boost::listS,            // vertex container
                         boost::undirectedS,
+                        // boost::property<boost::vertex_index_t, std::size_t>,
+                        // boost::no_property,
                         // boost::property<std::size_t(), std::size_t>,
                         Vertex, Edge>;
 
-    using Graph_vv_u = boost::adjacency_list<
-                        boost::vecS,            // edge container
-                        boost::vecS,            // vertex container
-                        boost::undirectedS,
-                        Vertex, Edge>;
+    // using Graph_vv_u = boost::adjacency_list<
+    //                     boost::vecS,            // edge container
+    //                     boost::vecS,            // vertex container
+    //                     boost::undirectedS,
+    //                     Vertex, Edge>;
 
-    using Graph_ss_u = boost::adjacency_list<
-                        boost::setS,            // edge container
-                        boost::setS,            // vertex container
-                        boost::undirectedS,
-                        Vertex, Edge>;
+    // using Graph_ss_u = boost::adjacency_list<
+    //                     boost::setS,            // edge container
+    //                     boost::setS,            // vertex container
+    //                     boost::undirectedS,
+    //                     Vertex, Edge>;
     
-    using Graph_vv_d =  boost::adjacency_list<
-                        boost::vecS,            // edge container
-                        boost::vecS,            // vertex container
-                        boost::directedS,
-                        Vertex, Edge>;
+    // using Graph_vv_d =  boost::adjacency_list<
+    //                     boost::vecS,            // edge container
+    //                     boost::vecS,            // vertex container
+    //                     boost::directedS,
+    //                     Vertex, Edge>;
 
-    auto g1 = create_test_graph<Graph1>(10,3);
 
-    for (auto v : boost::make_iterator_range(vertices(g1))){
-        std::cout << boost::get(boost::vertex_index, g1, v) << std::endl; 
-    }
+    auto g1 = create_and_initialize_test_graph<Graph1>(10,3);
+
+
+    using VertexIdMap = boost::property_map<Graph1, std::size_t Vertex::*>::type;
+    VertexIdMap vertex_id = get(&Vertex::id, g1);
+
+    // for (auto v : boost::make_iterator_range(vertices(g1))){
+    //     std::cout << boost::get(boost::vertex_index, g1, v) << std::endl; 
+    // }
 
     // Create a test HDFFile and a HDFGroup 
     auto hdf = Utopia::DataIO::HDFFile("graphtestfile.h5","a");
     auto grp = hdf.open_group("testgroup");
 
-    // boost::BOOST_INSTALL_PROPERTY()
-
-
-
-    // boost::static_property_map dp;
-    // dp.property(boost::vertex_index_t(), boost::get(&Vertex::id, g1));
-
-    Utopia::DataIO::save_graph<true, id>(g1, grp, "test1");
-    // Utopia::DataIO::save_graph<true, id>(g1, grp, "test1");
+    Utopia::DataIO::save_graph<true>(g1, grp, "test1", vertex_id);
+    Utopia::DataIO::save_graph<true>(g1, grp, "test1");
 
 }
 
