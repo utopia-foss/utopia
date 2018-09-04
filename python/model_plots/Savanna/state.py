e20c5128d2ee31b681ff0fe11d205c483a7caa13
Julian <julian@pc225.iup.uni-heidelberg.de>
2018-09-04 16:48:18 +0200

    added state_trajectory

--- python/model_plots/Savanna/state.py
+++ python/model_plots/Savanna/state.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 from utopya import DataManager
 
@@ -20,25 +21,89 @@ def state_time(dm: DataManager, *, out_path: str, uni: int, fmt: str=None, save_
         save_kwargs (dict, optional): kwargs to the plt.savefig function
         **plot_kwargs: Passed on to plt.plot
     """
-    for uni in dm['uni']:
+    # Get the group that all datasets are in
+    grp = dm['uni'][uni]['data/Savanna']
 
-        # Get the group that all datasets are in
-        grp = dm['uni'][uni]['data/Savanna']
+    # Get the shape of the data
+    uni_cfg = dm['uni'][uni]['cfg']
+    num_steps = uni_cfg['num_steps']
+    grid_size = uni_cfg['Savanna']['grid_size']
 
-        # Get the shape of the data
-        uni_cfg = dm['uni'][uni]['cfg']
-        num_steps = uni_cfg['num_steps']
-        grid_size = uni_cfg['Savanna']['grid_size']
+    # Extract the y data which is 'some_state' avaraged over all grid cells for every time step
+    G_data = grp['density_G']
+    T_data = grp['density_T']
+    F_data = grp['density_F']
+    S_data = np.empty(G_data.shape)
+    for i in range(G_data.shape[0]):
+        for j in range(G_data.shape[1]):
+            S_data[i][j] = 1-G_data[i][j]-T_data[i][j]-F_data[i][j]
 
-        # Extract the y data which is 'some_state' avaraged over all grid cells for every time step
-        G_data = grp['density_G']
-        S_data = grp['density_S']
-        T_data = grp['density_T']
+    # Call the plot function
+    plt.plot(G_data, "green",label="grass")
+    plt.plot(S_data, "yellow",label="sappling")
+    plt.plot(T_data, "orange",label="tree")
+    plt.plot(F_data, "red",label="forest tree")
+    plt.legend()
 
-        # Call the plot function
-        plt.plot(G_data, "green")
-        plt.plot(S_data, "blue")
-        plt.plot(T_data, "black")
+    # Save and close figure
+    save_and_close(out_path, save_kwargs=save_kwargs)
+
+def state_trajectory(dm: DataManager, *, out_path: str, uni: int, fmt: str=None, save_kwargs: dict=None, **plot_kwargs):
+    """Performs a lineplot on the state 
+    
+    Args:
+        dm (DataManager): The data manager from which to retrieve the data
+        out_path (str): Where to store the plot to
+        uni (int): The universe to use
+        fmt (str, optional): the plt.plot format argument
+        save_kwargs (dict, optional): kwargs to the plt.savefig function
+        **plot_kwargs: Passed on to plt.plot
+    """
+    # Get the group that all datasets are in
+    grp = dm['uni'][uni]['data/Savanna']
+
+    # Get the shape of the data
+    uni_cfg = dm['uni'][uni]['cfg']
+    num_steps = uni_cfg['num_steps']
+    grid_size = uni_cfg['Savanna']['grid_size']
+
+    # Extract the y data which is 'some_state' avaraged over all grid cells for every time step
+    G_data = grp['density_G']
+    T_data = grp['density_T']
+    F_data = grp['density_F']
+    S_data = np.empty(G_data.shape)
+    for i in range(G_data.shape[0]):
+        for j in range(G_data.shape[1]):
+            S_data[i][j] = 1-G_data[i][j]-T_data[i][j]-F_data[i][j]
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+
+
+    forest_exists = False
+    for f in F_data[:,0]:
+        if (f != 0):
+            forest_exists = True
+    if (forest_exists):
+        ax.scatter(G_data[:,0],T_data[:,0],F_data[:,0],c=range(G_data.shape[0]),cmap=plt.cm.viridis)
+        ax.scatter(G_data[:,0],T_data[:,0],np.zeros(G_data.shape[0]),c=range(G_data.shape[0]),cmap=plt.cm.viridis,alpha=0.3)
+        ax.scatter(G_data[:,0],np.ones(G_data.shape[0]) ,F_data[:,0],c=range(G_data.shape[0]),cmap=plt.cm.viridis,alpha=0.3)
+        ax.scatter(np.zeros(G_data.shape[0]),T_data[:,0],F_data[:,0],c=range(G_data.shape[0]),cmap=plt.cm.viridis,alpha=0.3)
+        ax.set_xlabel("Grass")
+        ax.set_ylabel("Trees")
+        ax.set_zlabel("Forest")
+    else:
+        ax.scatter(G_data[:,0],S_data[:,0],T_data[:,0],c=range(G_data.shape[0]),cmap=plt.cm.viridis)
+        ax.scatter(G_data[:,0],S_data[:,0],np.zeros(G_data.shape[0]),c=range(G_data.shape[0]),cmap=plt.cm.viridis,alpha=0.3)
+        ax.scatter(G_data[:,0],np.ones(G_data.shape[0]) ,T_data[:,0],c=range(G_data.shape[0]),cmap=plt.cm.viridis,alpha=0.3)
+        ax.scatter(np.zeros(G_data.shape[0]),S_data[:,0],T_data[:,0],c=range(G_data.shape[0]),cmap=plt.cm.viridis,alpha=0.3)
+        ax.set_xlabel("Grass")
+        ax.set_ylabel("Saplings")
+        ax.set_zlabel("Trees")
+    ax.set_xlim(0,1)
+    ax.set_ylim(0,1)
+    ax.set_zlim(0,1)
 
     # Save and close figure
     save_and_close(out_path, save_kwargs=save_kwargs)
