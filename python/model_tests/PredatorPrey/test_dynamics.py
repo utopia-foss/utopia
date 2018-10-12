4dac67970fb52c19658c71a70e07fb8e2e42e201
Daniel Lake <daniellake@pc25.iup.uni-heidelberg.de>
2018-10-12 17:44:02 +0200

    Make some changes to the tests of the PredatorPrey model

--- python/model_tests/PredatorPrey/test_dynamics.py
+++ python/model_tests/PredatorPrey/test_dynamics.py
@@ -24,7 +24,7 @@ def assert_eq(a, b, *, epsilon=1e-6):
 # - taking up resources
 # - reproduction
 
-def test_basic_interactions():
+def test_basic_interactions(): 
     """Test the basic interaction functions of the PredatorPrey model"""
 
     # Test Cost of Living for Predator and Prey
@@ -73,67 +73,64 @@ def test_basic_interactions():
     assert resource_prey[2, 0, 0] == 4
     assert np.all(pop[ : , 0, 0] == [1, 1, 1])
 
-    # Predator should move from one cell to the next until it succumbs to starvation
+    # Predator should move to prey in his neighborhood and afterwards move around
+    # randomly to find prey
+
+    # Create the model_config dict to be able to update the parameter_space dict
+    model_conf = model_cfg(grid_size=[2, 1], prey_frac=0.5, pred_frac=0.5, 
+                            delta_e=39, p_repro=0.0, p_flee=0.0)
+
+    # Add a specific number of steps for this test
+    model_conf['parameter_space'].update({'num_steps':41})
 
     # Create a multiverse, run a single universe and save the data in the DataManager dm
     mv, dm = mtc.create_run_load(from_cfg="specific_scenario.yml", perform_sweep=False, 
-                                **model_cfg(grid_size=[2, 1], prey_frac=0.0, pred_frac=0.5,
-                                            delta_e=2, p_repro=0.0))
+                                **model_conf)
 
-    pop = dm['uni'][0]['data']['PredatorPrey']['Population'].reshape(3, 2, 1)
-    resource_pred = dm['uni'][0]['data']['PredatorPrey']['resource_pred'].reshape(3, 2, 1)
-    resource_prey = dm['uni'][0]['data']['PredatorPrey']['resource_prey'].reshape(3, 2, 1)
-
-    assert np.all(resource_pred[: , : , :] == [[[2], [0]], [[0],[1]], [[0], [0]]]) \
-            or np.all(resource_pred[: , : , :] == [[[0], [2]], [[1],[0]], [[0], [0]]])
-
-    assert np.all(resource_prey[: , :, :] == 0)
-
-    assert np.all(pop[ : , :, :] == [[[2], [0]], [[0],[2]], [[0], [0]]]) \
-            or np.all(pop[ : , :, :] == [[[0], [2]], [[2], [0]], [[0], [0]]])
-
-    # Predator should move to prey in his neighborhood, as the order of the update is random, 
-    # two cases are to be considered
-
-    # Create a multiverse, run a single universe and save the data in the DataManager dm
-    mv, dm = mtc.create_run_load(from_cfg="specific_scenario.yml", perform_sweep=False, 
-                                **model_cfg(grid_size=[2, 1], prey_frac=0.5, pred_frac=0.5, 
-                                            delta_e=2, p_repro=0.0, p_flee=0.0))
-
-    pop = dm['uni'][0]['data']['PredatorPrey']['Population'].reshape(3, 2, 1)
-    resource_prey = dm['uni'][0]['data']['PredatorPrey']['resource_prey'].reshape(3, 2, 1)
-    resource_pred = dm['uni'][0]['data']['PredatorPrey']['resource_pred'].reshape(3, 2, 1)
+    pop = dm['uni'][0]['data']['PredatorPrey']['Population'].reshape(42, 2, 1).astype(int)
+    resource_prey = dm['uni'][0]['data']['PredatorPrey']['resource_prey'].reshape(42, 2, 1).astype(int)
+    resource_pred = dm['uni'][0]['data']['PredatorPrey']['resource_pred'].reshape(42, 2, 1).astype(int)
 
     if pop[0, 0, 0] == 2:
-        assert np.all(pop[ : , :, :] == [[[2], [1]], [[0],[2]], [[2], [0]]]) 
-        assert np.all(resource_pred[: , : , :] == [[[2], [0]], [[0],[3]], [[2], [0]]])
-        assert np.all(resource_prey[: , : , :] == [[[0], [2]], [[0],[0]], [[0], [0]]])
+        # in the last time step, depending on the update order, 
+        # the predator may move twice
+        assert np.all(pop[ :2, :, :] == [[[2], [1]], [[0],[2]]])
+        assert np.all(resource_pred[ :2, : , :] == [[[2], [0]], [[0],[40]]])
+        assert np.all(resource_prey[ :2, : , :] == [[[0], [2]], [[0],[0]]])
     
     else:
         # in the last time step, depending on the update order, 
         # the predator may move twice
-        assert np.all(pop[ : , :, :] == [[[1], [2]], [[2],[0]], [[0], [2]]]) \
-                or np.all(pop[ : , :, :] == [[[1], [2]], [[2],[0]], [[2], [0]]]) 
-        assert np.all(resource_pred[: , : , :] == [[[0], [2]], [[3],[0]], [[0], [2]]]) \
-                or np.all(resource_pred[: , : , :] == [[[0], [2]], [[3],[0]], [[2], [0]]])
-        assert np.all(resource_prey[: , : , :] == [[[2], [0]], [[0],[0]], [[0], [0]]])
+        assert np.all(pop[ :2, :, :] == [[[1], [2]], [[2],[0]]])
+        assert np.all(resource_pred[ :2, : , :] == [[[0], [2]], [[40],[0]]]) 
+        assert np.all(resource_prey[ :2, : , :] == [[[2], [0]], [[0],[0]]])
+
+    # Calculate the difference between succesive steps after the prey has been eaten
+    diff = np.diff(pop[2:], axis=0)
+
+    # Check wether at some point the predator moved to another cell
+    assert np.any(diff == 2)
+    # Check that the predator starves in the last step
+    assert np.sum(diff, axis=1)[-1] == -2
+    
+
 
     # Test wether the prey flees from the predator. Unfortunately, as the update
     # order is random, the prey can only flee, if its own cell has not been updated,
     # before a predator invades it.
 
     # Create the model_config dict to be able to update the parameter_space dict
-    model_config = model_cfg(grid_size=[20, 1], prey_frac=0.95, pred_frac=0.05, delta_e=2, 
+    model_config = model_cfg(grid_size=[30, 1], prey_frac=0.95, pred_frac=0.05, delta_e=2, 
                              p_repro=0.0, p_flee=1.0)
 
     # Add a specific number of steps for this test
-    model_config['parameter_space'].update({'num_steps':20})
+    model_config['parameter_space'].update({'num_steps':30})
 
     # Create a multiverse, run a single universe and save the data in the DataManager dm
     mv, dm = mtc.create_run_load(from_cfg="specific_scenario.yml", perform_sweep=False, 
-                                num_steps=20, **model_config)
+                                **model_config)
 
-    pop = dm['uni'][0]['data']['PredatorPrey']['Population'].reshape(21, 20, 1)
+    pop = dm['uni'][0]['data']['PredatorPrey']['Population'].reshape(31, 30, 1)
     pop = pop.astype(int)
 
     diff = np.diff(pop, axis=0)
