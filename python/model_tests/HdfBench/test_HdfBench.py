d66e332715003ea08c3aab84642d8848b85db575
Yunus Sevinchan <yunussevinchan@gmail.com>
2018-10-17 10:40:21 +0200

    Add test for write function

--- python/model_tests/HdfBench/test_HdfBench.py
+++ python/model_tests/HdfBench/test_HdfBench.py
@@ -21,5 +21,32 @@ def test_default():
     mv, dm = mtc.create_run_load()
 
     for uni_no, uni in dm['uni'].items():
-        assert uni['data/HdfBench/times'].shape == (4, 3)
+        times = uni['data/HdfBench/times']
+        assert times.shape == (4, 3)
+        assert np.min(times) > 0.
         assert len(uni['data/HdfBench']) == 3 + 1
+
+def test_write():
+    """Test the write functions"""
+    mv, dm = mtc.create_run_load(from_cfg="write_funcs.yml",
+                                 perform_sweep=True)
+
+    for uni_no, uni in dm['uni'].items():
+        times = uni['data/HdfBench/times']
+        bench_cfg = uni['cfg']['HdfBench']
+        benchmarks = bench_cfg['benchmarks']
+        
+        assert times.shape == (11, len(benchmarks))
+
+        for i, bname in enumerate(benchmarks):
+            # Assert that all configured benchmarks were performed
+            assert (times.attrs['coords_benchmark'][i]).decode('utf8') == bname
+
+        # Checks for specific benchmarks
+        # write_const
+        dset = uni['data/HdfBench/write_const']
+        const_val = bench_cfg['write_const']['const_val']
+
+        assert dset.shape == (11, 7)
+        assert np.min(dset) == np.max(dset) == const_val
+        assert dset.dtype == float
