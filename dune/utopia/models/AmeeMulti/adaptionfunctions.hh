b77db53d285e78567b99d3c06eb54c8ffb658a0d
Harald Mack <hmack@iup.uni-heidelberg.de>
2018-11-15 17:53:23 +0100

    removed superfluous if condition in adaptionfunction

--- dune/utopia/models/AmeeMulti/adaptionfunctions.hh
+++ dune/utopia/models/AmeeMulti/adaptionfunctions.hh
@@ -18,35 +18,26 @@ auto multi_notnormed = [](auto& agent) -> std::vector<double> {
     const auto& celltrait = state.habitat->state().celltrait;
     const auto& trait = state.phenotype;
 
-    if (start >= 0 and end > 0 and end > start and trait.size() > 0 and
-        (int) celltrait.size() > 0 and start < (int)trait.size() and
-        start < (int)celltrait.size())
-    {
-        std::vector<double> adaption(end - start, 0.);
+    std::vector<double> adaption(end - start, 0.);
 
-        int i = start;
-        int j = 0;
-        for (; i < end && i < (int)celltrait.size() && i < (int)trait.size(); ++i, ++j)
+    int i = start;
+    int j = 0;
+    for (; i < end && i < (int)celltrait.size() && i < (int)trait.size(); ++i, ++j)
+    {
+        adaption[j] =
+            (trait[i] * celltrait[i]) / (1. + (std::abs(trait[i] - celltrait[i])));
+
+        if (std::isnan(adaption[j]) or (adaption[j] < 0.))
         {
-            adaption[j] = (trait[i] * celltrait[i]) /
-                          (1. + (std::abs(trait[i] - celltrait[i])));
-
-            if (std::isnan(adaption[j]) or (adaption[j] < 0.))
-            {
-                adaption[j] = 0.;
-            }
-
-            if (std::isinf(adaption[j]))
-            {
-                throw std::runtime_error("Inf found in adaption");
-            }
+            adaption[j] = 0.;
         }
-        return adaption;
+
+        if (std::isinf(adaption[j]))
+        {
+            throw std::runtime_error("Inf found in adaption");
+        }
     }
-    else
-    {
-        return std::vector<double>();
-    }
+    return adaption;
 };
 
 auto multi_normed = [](auto& agent) -> std::vector<double> {
@@ -56,36 +47,27 @@ auto multi_normed = [](auto& agent) -> std::vector<double> {
     const auto& celltrait = state.habitat->state().celltrait;
     const auto& trait = state.phenotype;
 
-    if (start >= 0 and end > 0 and end > start and trait.size() > 0 and
-        (int) celltrait.size() > 0 and start < (int)trait.size() and
-        start < (int)celltrait.size())
-    {
-        std::vector<double> adaption(end - start, 0.);
+    std::vector<double> adaption(end - start, 0.);
 
-        int i = start;
-        int j = 0;
-        for (; i < end && i < (int)celltrait.size() && i < (int)trait.size(); ++i, ++j)
+    int i = start;
+    int j = 0;
+    for (; i < end && i < (int)celltrait.size() && i < (int)trait.size(); ++i, ++j)
+    {
+        adaption[j] =
+            ((trait[i] * celltrait[i]) / (1. + (std::abs(trait[i] - celltrait[i])))) /
+            (end - start);
+
+        if (std::isnan(adaption[j]) or (adaption[j] < 0.))
         {
-            adaption[j] = ((trait[i] * celltrait[i]) /
-                           (1. + (std::abs(trait[i] - celltrait[i])))) /
-                          (end - start);
-
-            if (std::isnan(adaption[j]) or (adaption[j] < 0.))
-            {
-                adaption[j] = 0.;
-            }
-
-            if (std::isinf(adaption[j]))
-            {
-                throw std::runtime_error("Inf found in adaption");
-            }
+            adaption[j] = 0.;
         }
-        return adaption;
+
+        if (std::isinf(adaption[j]))
+        {
+            throw std::runtime_error("Inf found in adaption");
+        }
     }
-    else
-    {
-        return std::vector<double>();
-    }
+    return adaption;
 };
 
 auto simple_notnormed = [](auto& agent) -> std::vector<double> {
@@ -95,32 +77,22 @@ auto simple_notnormed = [](auto& agent) -> std::vector<double> {
     const auto& celltrait = state.habitat->state().celltrait;
     const auto& trait = state.phenotype;
 
-    if (start >= 0 and end > 0 and end > start and trait.size() > 0 and
-        (int) celltrait.size() > 0 and start < (int)trait.size() and
-        start < (int)celltrait.size())
-    {
-        std::vector<double> adaption(end - start, 0.);
+    std::vector<double> adaption(end - start, 0.);
 
-        for (int i = start;
-             i < end && i < (int)celltrait.size() && i < (int)trait.size(); ++i)
+    for (int i = start; i < end && i < (int)celltrait.size() && i < (int)trait.size(); ++i)
+    {
+        adaption[i] = 1. / (1. + (std::abs(trait[i] - celltrait[i])));
+
+        if (std::isnan(adaption[i]) or (adaption[i] < 0.))
         {
-            adaption[i] = 1. / (1. + (std::abs(trait[i] - celltrait[i])));
-
-            if (std::isnan(adaption[i]) or (adaption[i] < 0.))
-            {
-                adaption[i] = 0.;
-            }
-            if (std::isinf(adaption[i]))
-            {
-                throw std::runtime_error("Inf found in adaption");
-            }
+            adaption[i] = 0.;
         }
-        return adaption;
+        if (std::isinf(adaption[i]))
+        {
+            throw std::runtime_error("Inf found in adaption");
+        }
     }
-    else
-    {
-        return std::vector<double>();
-    }
+    return adaption;
 };
 
 auto simple_normed = [](auto& agent) -> std::vector<double> {
@@ -130,32 +102,22 @@ auto simple_normed = [](auto& agent) -> std::vector<double> {
     const auto& celltrait = state.habitat->state().celltrait;
     const auto& trait = state.phenotype;
 
-    if (start >= 0 and end > 0 and end > start and trait.size() > 0 and
-        (int) celltrait.size() > 0 and start < (int)trait.size() and
-        start < (int)celltrait.size())
-    {
-        std::vector<double> adaption(end - start, 0.);
+    std::vector<double> adaption(end - start, 0.);
 
-        for (int i = start;
-             i < end && i < (int)celltrait.size() && i < (int)trait.size(); ++i)
+    for (int i = start; i < end && i < (int)celltrait.size() && i < (int)trait.size(); ++i)
+    {
+        adaption[i] = (1. / (1. + (std::abs(trait[i] - celltrait[i])))) / (end - start);
+
+        if (std::isnan(adaption[i]) or (adaption[i] < 0.))
         {
-            adaption[i] = (1. / (1. + (std::abs(trait[i] - celltrait[i])))) / (end - start);
-
-            if (std::isnan(adaption[i]) or (adaption[i] < 0.))
-            {
-                adaption[i] = 0.;
-            }
-            if (std::isinf(adaption[i]))
-            {
-                throw std::runtime_error("Inf found in adaption");
-            }
+            adaption[i] = 0.;
         }
-        return adaption;
+        if (std::isinf(adaption[i]))
+        {
+            throw std::runtime_error("Inf found in adaption");
+        }
     }
-    else
-    {
-        return std::vector<double>();
-    }
+    return adaption;
 };
 
 } // namespace AmeeMulti
