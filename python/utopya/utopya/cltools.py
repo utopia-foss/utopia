ecbe3476f3f2715d5020d79de072cc158c06e6eb
Yunus Sevinchan <yunussevinchan@gmail.com>
2018-11-26 11:29:15 +0100

    Add cltools func add_entry to improve modularisation

--- python/utopya/utopya/cltools.py
+++ python/utopya/utopya/cltools.py
@@ -3,6 +3,7 @@
 import os
 import re
 import logging
+from typing import Callable
 from pkg_resources import resource_filename
 
 from utopya.multiverse import Multiverse
@@ -15,6 +16,48 @@ USER_CFG_HEADER_PATH = resource_filename('utopya', 'cfg/user_cfg_header.yml')
 BASE_CFG_PATH = resource_filename('utopya', 'cfg/base_cfg.yml')
 
 # -----------------------------------------------------------------------------
+
+def add_entry(value, *, key_path: list, add_to: dict, is_valid: Callable=None, ErrorMsg: Callable=None):
+    """Adds the given value to the `add_to` dict at the given key path.
+
+    If `value` is a callable, that function is called and the return value is
+    what is stored in the dict.
+    """
+    log.debug("Adding CLI entry '%s' at %s ...")
+
+    # Check the value by calling the function; it should raise an error
+    if is_valid is not None:
+        if not is_valid(value):
+            raise ErrorMsg(value)
+
+    # Determine which keys need to be traversed
+    traverse_keys, last_key = key_path[:-1], key_path[-1]
+
+    # Set the starting point
+    d = add_to
+
+    # Traverse keys
+    for _key in traverse_keys:
+        log.debug("Traversing key '%s' ...", _key)
+
+        # Check if a new entry is needed
+        if _key not in d:
+            d[_key] = dict()
+
+        # Select the new entry
+        d = d[_key]
+
+    # Now d is where the value should be added
+    # If applicable
+    if callable(value):
+        value = value()
+        log.debug("Resolved value from callable:  %s", value)
+    
+    # Store in dict, mutable
+    d[last_key] = value
+
+    log.debug("Successfully added entry.")
+
 
 def add_from_kv_pairs(*pairs, add_to: dict, attempt_conversion: bool=True, allow_eval: bool=False):
     """Parses the key=value pairs and adds them to the given dict.
@@ -55,6 +98,7 @@ def add_from_kv_pairs(*pairs, add_to: dict, attempt_conversion: bool=True, allow
         # Just return the string
         return val
 
+    log.debug("Adding entries from key-value pairs ...")
 
     # Go over all pairs and add them to the given base dict
     for kv in pairs:
@@ -85,6 +129,8 @@ def add_from_kv_pairs(*pairs, add_to: dict, attempt_conversion: bool=True, allow
         d[last_key] = val
 
     # No need to return the base dict as it is a mutable!
+    log.debug("Added %d entries from key-value pairs.", len(pairs))
+
 
 def deploy_user_cfg(user_cfg_path: str=Multiverse.USER_CFG_SEARCH_PATH) -> None:
     """Deploys a copy of the full config to the specified location (usually
