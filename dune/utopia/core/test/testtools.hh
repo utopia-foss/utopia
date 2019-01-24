7b501689f7bd8133f3afce84f73cc4d05df164ab
Yunus Sevinchan <YunusSevinchan@gmail.com>
2019-01-24 14:18:11 +0100

    Document testtools
    Hopefully also gets rid of gcc warning re std::string::find

--- dune/utopia/core/test/testtools.hh
+++ dune/utopia/core/test/testtools.hh
@@ -4,10 +4,19 @@
 #include <iostream>
 
 /// Helper method to test whether the expected error type and message is thrown
-template<typename err_t>
+/** \tparam err_t   The expected error type, has to supply .what() method
+  *
+  * \param desc     A describing string that is emitted before the function is
+  *                 called
+  * \param func     The callable whose exception handling is to be checked
+  * \param to_find  The string that is to be matched (via std::string::find)
+  *                 in the produced error message
+  */
+template<typename err_t, typename Callable=std::function<void()>>
 bool check_error_message(std::string desc,
-                         std::function<void()> func,
-                         std::string to_find) {
+                         Callable func,
+                         std::string to_find)
+{
     std::cout << "Checking exceptions for case:  " << desc << std::endl;
     try {
         func();
@@ -16,7 +25,7 @@ bool check_error_message(std::string desc,
         return false;
     }
     catch (err_t& e) {
-        if (((std::string) e.what()).find(to_find) == -1) {
+        if (((std::string) e.what()).find(to_find) < 0) {
             std::cerr << "Did not throw expected error message!" << std::endl;
             std::cerr << "  Expected to find:  " << to_find << std::endl;
             std::cerr << "  But got         :  " << e.what() << std::endl;
