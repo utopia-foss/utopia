b4412254f76a69ceea8e6946a71927cc2c921550
Benjamin Herdeanu <herdeanu@iup.uni-heidelberg.de>
2019-02-19 17:54:03 +0100

    Implement new agent class inherited from entity (without agent specific features)

--- dune/utopia/core/agent_new.hh
+++ dune/utopia/core/agent_new.hh
@@ -1,5 +1,5 @@
-#ifndef UTOPIA_CORE_CELL_HH
-#define UTOPIA_CORE_CELL_HH
+#ifndef UTOPIA_CORE_AGENT_HH
+#define UTOPIA_CORE_AGENT_HH
 
 // NOTE This file's final name will be cell.hh
 
@@ -11,7 +11,7 @@
 
 namespace Utopia {
 /**
- *  \addtogroup CellManager
+ *  \addtogroup AgentManager
  *  \{
  */
 
@@ -21,25 +21,25 @@ namespace Utopia {
  */
 template<typename StateType, 
          bool is_sync=true, 
-         typename CellTags=EmptyTag,
+         typename AgentTags=EmptyTag,
          template<class> class CustomLinkContainers=NoCustomLinks>
-using CellTraits = EntityTraits<StateType, 
+using AgentTraits = EntityTraits<StateType, 
                                 is_sync, 
-                                CellTags, 
+                                AgentTag, 
                                 CustomLinkContainers>;
 
 
 
-/// A cell is a slightly specialized state container
+/// An agent is a slightly specialized state container
 /** \detail  It can be extended with the use of tags and can be associated with
   *          so-called "custom links". These specializations are carried into
-  *          the cell by means of the CellTraits struct.
-  *          A cell is embedded into the CellManager, where the discretization
-  *          allows assigning a position in space to the cell. The cell itself
+  *          the agent by means of the AgentTraits struct.
+  *          An agent is embedded into the AgentManager, where the discretization
+  *          allows assigning a position in space to the agent. The agent itself
   *          does not know anything about that ...
   */
 template<typename Traits>
-class __Cell :   // NOTE Final name will be Cell
+class __Agent :   // NOTE Final name will be Agent
     public __Entity<Traits>
 {
 public:
@@ -47,19 +47,19 @@ public:
     using State = typename Traits::State;
 
     // -- Constructors -- //
-    /// Construct a cell
-    __Cell(const IndexType id, const State initial_state)
+    /// Construct an agent
+    __Agent(const IndexType id, const State initial_state)
     :
         __Entity<Traits>(id, initial_state)
     {}
 };
 
 
-// end group CellManager
+// end group AgentManager
 /**
  *  \}
  */
 
 } // namespace Utopia
 
-#endif // UTOPIA_CORE_CELL_HH
+#endif // UTOPIA_CORE_AGENT_HH
