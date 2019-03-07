58314dee349584085db341c49581b6523bb84301
Lukas Riedel <mail@lukasriedel.com>
2019-03-07 18:24:38 +0100

    Remove DUNE functions from build system
    This makes all models compile without DUNE.
    
    Changes:
    * Raise required CMake version to 3.10 (available in Ubuntu 18.04).
    * Define interface library target 'utopia' which should be linked to
      all executables running Utopia code.
      - Require C++17.
      - Link to dependency libraries.
      - Add include directories of Utopia source code.
      - 'add_model' and 'add_model_test' now only link to 'utopia' target.
    * Adjust functions for registering models and tests
      - Remove usage of 'dune_add_test' by defining executables and
        manually registering them as tests.
      - Remove usage of 'dune_add_python_test' by simply declaring custom
        targets.
    * Adjust macros for finding dependencies.
      - Define proper target for H5 library. Must link against regular and
        high-level libraries in lower CMake versions. Need to sanitize
        compile definitions. Set properties directly because of bug in
        CMake 3.10.
      - Do not append to DUNE libs anymore.
    * Add custom 'doc' target and remove DUNE doxygen target
    * Define target 'build_unit_tests' for doing the obvious

--- cmake/modules/UtopiaAddModelTest.cmake
+++ cmake/modules/UtopiaAddModelTest.cmake
@@ -13,32 +13,24 @@ function(add_model_test)
     include(CMakeParseArguments)
     set(OPTIONS)
     set(SINGLEARGS MODEL_NAME NAME)
-    set(MULTIARGS)
-    cmake_parse_arguments(KWARGS "${OPTIONS}" "${SINGLEARGS}" "${MULTIARGS}" ${ARGN})
+    set(MULTIARGS SOURCES)
+    cmake_parse_arguments(KWARGS
+        "${OPTIONS}" "${SINGLEARGS}" "${MULTIARGS}" ${ARGN})
+
+    if (KWARGS_UNPARSED_ARGUMENTS)
+        message(WARNING "Encountered unparsed arguments in 'add_model_test': "
+                        "${KWARGS_UNPARSED_ARGUMENTS}")
+    endif ()
 
     # Create the target name
     set(target_name MODEL_${KWARGS_MODEL_NAME}_${KWARGS_NAME})
 
     # Add the test
-    dune_add_test(NAME ${target_name} ${KWARGS_UNPARSED_ARGUMENTS})
+    add_executable(${target_name} ${KWARGS_SOURCES})
+    add_test(NAME ${target_name} COMMAND ${target_name})
 
-    # Link the required library targets
-    target_link_libraries(${target_name}
-        spdlog
-        yaml-cpp
-        armadillo
-    )
-
-    # yaml-cpp does not export interface include dirs
-    # NOTE This should be reported as bug or patched via MR in the yaml-cpp
-    #      repository. See https://pabloariasal.github.io/2018/02/19/its-time-to-do-cmake-right/
-    #      as a reference on how to do that.
-    target_include_directories(${target_name}
-        # register as system headers (compilers might ignore warnings)
-        SYSTEM
-        PRIVATE
-            ${PROJECT_SOURCE_DIR}/vendor/plugins/yaml-cpp/include/
-    )
+    # link to Utopia target
+    target_link_libraries(${target_name} PUBLIC utopia)
 
     # Done. Inform about it
     message(STATUS "Added model test target:     ${target_name}")
