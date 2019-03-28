28495142224992a50279e9001a62b187f43ef966
Benjamin Herdeanu <herdeanu@iup.uni-heidelberg.de>
2019-03-28 12:40:42 +0100

    Make tests more granular

--- python/model_tests/PredatorPrey/test_dynamics.py
+++ python/model_tests/PredatorPrey/test_dynamics.py
@@ -24,88 +24,75 @@ def assert_eq(a, b, *, epsilon=1e-6):
 # - taking up resources
 # - reproduction
 
-def test_basic_interactions(): 
-    """Test the basic interaction functions of the PredatorPrey model"""
+def test_cost_of_living_prey():
+    """Test the cost of living of the prey"""
+    # Create, run, and load a universe
+    mv, dm = mtc.create_run_load(from_cfg="cost_of_living_prey.yml", 
+                                 perform_sweep=False)
 
-    # Test Cost of Living for Predator and Prey
-
-    # Prey
-
-    # Create a multiverse, run a single universe and save the data in the 
-    # DataManager dm
-    mv, dm = mtc.create_run_load(from_cfg="specific_scenario.yml", 
-                                 perform_sweep=False,
-                                 **model_cfg(prey_frac=1.0, 
-                                             pred_frac=0.0, delta_e_pred=0,delta_e_prey=0))
-
-    
+    # Get the data
     data = dm['multiverse'][0]['data']
-
-    pop = data['PredatorPrey']['population']#.reshape(3, 1, 1)
     res_prey = data['PredatorPrey']['resource_prey']
-    
-    print(res_prey)
 
+    # Assert that life is costly...
     assert res_prey[0, 0, 0] == 2
     assert res_prey[1, 0, 0] == 1
     assert res_prey[2, 0, 0] == 0
 
-    # Predator
 
+def test_cost_of_living_Predator():
+    """Test the cost of living of the the predator"""
     # Create a multiverse, run a single universe and save the data in the 
     # DataManager dm
-    mv, dm = mtc.create_run_load(from_cfg="specific_scenario.yml", 
-                                 perform_sweep=False,
-                                 **model_cfg(prey_frac=0.0, pred_frac=1.0, 
-                                                    predprey_frac=0.0))
+    mv, dm = mtc.create_run_load(from_cfg="cost_of_living_predator.yml", 
+                                 perform_sweep=False)
 
+    # Get the data
     data = dm['multiverse'][0]['data']
-
     pop = data['PredatorPrey']['population']
     res_pred = data['PredatorPrey']['resource_predator']
 
+    # Assert that life is costly
     assert res_pred[0, 0, 0] == [2]
     assert res_pred[1, 0, 0] == [1]
     assert res_pred[2, 0, 0] == [0]
 
-    # Test the eating and the movement rule
+
+def test_eating_prey():
+    """Test the basic eating functions of the prey"""
 
     # Prey should just take up resources every step and spend 1 resource for 
     # its cost of living
 
     # Create a multiverse, run a single universe and save the data in the 
     # DataManager dm
-    mv, dm = mtc.create_run_load(from_cfg="specific_scenario.yml", 
-                                 perform_sweep=False, 
-                                 **model_cfg(delta_e_pred=2.0,delta_e_prey=2.0, prey_frac=1.0, 
-                                    pred_frac=0.0, predprey_frac=0.0))
+    mv, dm = mtc.create_run_load(from_cfg="test_eating_prey.yml", 
+                                 perform_sweep=False)
 
+    # Get the data
     data = dm['multiverse'][0]['data']
-
-    pop = data['PredatorPrey']['population']
     res_prey = data['PredatorPrey']['resource_prey']
 
+    # Assert that prey takes up resources every step and spends 1 resource
+    # for its cost of living
     assert res_prey[0, 0, 0] == [2]
     assert res_prey[1, 0, 0] == [3]
     assert res_prey[2, 0, 0] == [4]
 
-    # Predator should move to prey in his neighborhood and afterwards move 
-    # around randomly to find prey
 
-    # Create the model_config dict to enable updating the parameter_space dict
-    model_conf = model_cfg(prey_frac=0.5, pred_frac=0.5, 
-                            delta_e_pred=39,delta_e_prey=39, p_repro_pred=0.0,p_repro_prey=0.0, p_flee=0.0,
-                            e_max_pred=200,e_max_prey=200, 
-                            cell_manager=dict(resolution=1))
-
-    # Add a specific number of steps for this test
-    model_conf['parameter_space'].update({'num_steps':41})
-
+def test_predator_movement():
+    """Test the predator movement functions
+    
+        The predator should move to prey in his neighborhood and afterwards move 
+        around randomly to find prey.
+    """
+    
     # Create a multiverse, run a single universe and save the data in the 
     # DataManager dm
-    mv, dm = mtc.create_run_load(from_cfg="specific_scenario.yml", 
-                                 perform_sweep=False, **model_conf)
+    mv, dm = mtc.create_run_load(from_cfg="test_predator_movement.yml", 
+                                 perform_sweep=False)
 
+    # Get the data
     data = dm['multiverse'][0]['data']
     
     pop = data['PredatorPrey']['population'].astype(int)
@@ -126,7 +113,7 @@ def test_basic_interactions():
         assert np.all(res_pred[ :2, : , :] == [[[0], [2]], [[40],[0]]]) 
         assert np.all(res_prey[ :2, : , :] == [[[2], [0]], [[0],[0]]])
 
-    # Calculate the difference between succesive steps after the prey has been 
+    # Calculate the difference between successive steps after the prey has been 
     # eaten
     diff = np.diff(pop[2:], axis=0)
 
@@ -136,50 +123,35 @@ def test_basic_interactions():
     assert np.sum(diff, axis=1)[-1] == -2
     
 
-
+def test_prey_flee():
+    # TODO FIX: assert that there is one predator and one prey
     # Test whether the prey flees from the predator. Unfortunately, as the 
     # update order is random, the prey can only flee, if its own cell has not 
     # been updated, before a predator invades it.
 
-    # Create the model_config dict to enable updating the parameter_space dict
-    model_config = model_cfg(prey_frac=0.95, pred_frac=0.05, delta_e_pred=2,
-                            delta_e_prey=2,
-                             p_repro_pred=0.0,p_repro_prey=0, p_flee=1.0, 
-                             space=dict(extent=[30,1]), 
-                             cell_manager=dict(resolution=1))
-
-    # Add a specific number of steps for this test
-    model_config['parameter_space'].update({'num_steps':30})
-
     # Create a multiverse, run a single universe and save the data in the 
     # DataManager dm
-    mv, dm = mtc.create_run_load(from_cfg="specific_scenario.yml", 
-                                 perform_sweep=False, **model_config)
+    mv, dm = mtc.create_run_load(from_cfg="test_prey_flee.yml", 
+                                 perform_sweep=False)
     
     data = dm['multiverse'][0]['data']
-
     pop = data['PredatorPrey']['population'].astype(int)
 
     diff = np.diff(pop, axis=0)
 
     assert np.any(diff == -1)
+    
 
-    # Test the reproduction
-
-    #Prey
+def test_prey_reproduction(): 
+    """Test the basic interaction functions of the PredatorPrey model"""
 
     # Create a multiverse, run a single universe and save the data in the 
     # DataManager dm
-    mv, dm = mtc.create_run_load(from_cfg="specific_scenario.yml", 
-                                 perform_sweep=False, 
-                                 **model_cfg(prey_frac=0.5, 
-                                             pred_frac=0, delta_e_pred=2,
-                                             delta_e_prey=2, e_min_pred=3,
-                                             e_min_prey=3, 
-                                             p_repro_pred=1.0,p_repro_prey=1.0))
+    mv, dm = mtc.create_run_load(from_cfg="test_prey_reproduction.yml", 
+                                 perform_sweep=False)
 
+    # Get the data
     data = dm['multiverse'][0]['data']
-
     pop = data['PredatorPrey']['population']
     res_prey = data['PredatorPrey']['resource_prey']
                          
@@ -187,7 +159,3 @@ def test_basic_interactions():
         or np.all(res_prey[: , : , :] == [[[0], [2]], [[2],[1]], [[1], [2]]])
     assert np.all(pop[ : , :, :] == [[[1], [0]], [[1],[1]], [[1], [1]]]) \
         or np.all(pop[ : , :, :] == [[[0], [1]], [[1],[1]], [[1], [1]]]) 
-    
-    # The function for the predator is identical
-
-    
