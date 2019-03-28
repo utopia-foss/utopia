8b3f3d9a41c0b86c76a63aea7b37a3e300f8bc46
Peter Manshausen <pmanshau@jfranke.iup.uni-heidelberg.de>
2019-03-28 16:14:51 +0100

    add a multiverse plot function of cumulative area distribution

--- python/model_plots/SandPile/state.py
+++ python/model_plots/SandPile/state.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+import xarray as xr
 from utopya import DataManager, UniverseGroup
 from ..tools import save_and_close
 
@@ -92,6 +92,58 @@ def compl_cum_prob_dist(dm: DataManager, *,
     # Save and close figure
     save_and_close(out_path, save_kwargs=save_kwargs)
 
+def mult_cum_prob(dm: DataManager, *,
+               out_path: str,
+               mv_data: xr.Dataset,     # Here, you get the actual data as an xarray DataSet object
+               #
+               # Below, you can add further model specific arguments
+               save_kwargs: dict=None,
+               **plot_kwargs):
+    """This is the same as above, but for a multiverse run
+    
+    Args:
+        dm (DataManager): The data manager from which to retrieve the data
+        uni (UniverseGroup): The selected universe data
+        out_path (str): Where to store the plot to
+        save_kwargs (dict, optional): kwargs to the plt.savefig function
+        **plot_kwargs: Passed on to plt.plot
+    """
+    # Get the group that all datasets are in
+    print(mv_data['avalanche'].shape)
+    number_of_universes = mv_data['avalanche'].shape[0]
+    x=0
+    for x in range(number_of_universes):
+        # ### Extract the y data 
+        # # Get the avalanche data averaged over all grid cells for each time step
+        y_data = [np.sum(d) for d in mv_data['avalanche'][x,:,:,:]]
+
+        # Remove the first element, ...
+        y_data.pop(0)
+        # ... count the size of the avalanches, ...
+        y = np.bincount(y_data)
+        # ... and cummulatively sum them up
+        y = (np.cumsum(y[::-1])[::-1])[1:]
+        
+        # # Get the index 
+        index = (y - np.roll(y, 1)) != 0
+
+        # # Normalize the cummulated counts
+        y = y / y[0]
+
+        # # Calculate the logarithmic values
+        y_data = np.log10(np.arange(len(y)) + np.min(y_data))[index], np.log10(y)[index]
+
+        # # Call the plot function
+        plt.plot(*y_data, **plot_kwargs)
+
+        # Set labels
+        
+        x += 1
+    plt.xlabel(r'$\log_{10}(A)$')
+    plt.ylabel(r'$\log_{10}(P_A(A))$')
+    # # Save and close figure
+    #save_and_close(out_path, save_kwargs=save_kwargs)
+    save_and_close(out_path, save_kwargs=save_kwargs)
 
 def plot_area_frac_t(dm: DataManager, *, 
                         uni: UniverseGroup, 
@@ -136,4 +188,4 @@ def plot_area_frac_t(dm: DataManager, *,
     plt.ylabel(r'Area fraction $A/l^2$')
 
     # Save and close figure
-    plt.savefig(out_path, save_kwargs=save_kwargs)
+    save_and_close(out_path, save_kwargs=save_kwargs)
