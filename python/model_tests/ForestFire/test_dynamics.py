e3bf4038f39d41aa453206c3b084f8f239137dab
Narek Baghumian <nbaghumi@iup.uni-heidelberg.de>
2019-04-02 12:32:04 +0200

    Adjusted Test to use of Xarray

--- python/model_tests/ForestFire/test_dynamics.py
+++ python/model_tests/ForestFire/test_dynamics.py
@@ -30,12 +30,12 @@ def test_dynamics_two_state_model():
         assert density==1.0
 
         # all cells burned + 1% growth
-        density = np.sum(data[1,:,:])/num_cells
-        assert 0 <= density <= 0.01
+        density = data.sum(dim=['x', 'y'])/num_cells
+        assert 0 <= density[{'time': 1}].values <= 0.01
 
         # 1% growth
-        density = np.sum(data[2,:,:])/num_cells
-        assert 0.01 <= density <= 0.01 + 0.05
+        density = data.sum(dim=['x', 'y'])/num_cells
+        assert 0.01 <= density[{'time': 2}].values <= 0.01 + 0.05
 
 
 def test_percolation_mode():
