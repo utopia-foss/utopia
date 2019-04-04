983f9b7aabe69cf537026abaff21cb2561be134e
Benjamin Herdeanu <herdeanu@iup.uni-heidelberg.de>
2019-04-04 16:55:07 +0200

    Generalize the mean_and_std function

--- python/model_plots/ForestFire/state_mean_multiverse.py
+++ python/model_plots/ForestFire/state_mean_multiverse.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import xarray as xr
 
 from utopya import DataManager, UniverseGroup, MultiverseGroup
-from utopya.plotting import is_plot_func, PlotHelper, UniversePlotCreator
+from utopya.plotting import is_plot_func, PlotHelper, MultiversePlotCreator
 
-@is_plot_func(creator_type=UniversePlotCreator,
+@is_plot_func(creator_type=MultiversePlotCreator,
               # Provide some (static) default values for helpers
               helper_defaults=dict(
                 set_labels=dict(x="Time", y="Density"),
@@ -15,23 +15,40 @@ from utopya.plotting import is_plot_func, PlotHelper, UniversePlotCreator
                 save_figure=dict(bbox_inches="tight")
                 )
               )
-def state_mean_multiverse(  dm: DataManager, *,
-                            mv_data: xr.Dataset,
-                            hlpr: PlotHelper,
-                            save_kwargs: dict=None,
-                            plot_kwargs: dict=None):
-    '''Plots the cluster distribution for multiple universes'''
+def mean_and_std(dm: DataManager, *,
+                 mv_data: xr.Dataset,
+                 hlpr: PlotHelper,
+                 mean_of: str=None,
+                 lines_from: str=None,
+                 plot_kwargs: dict=None):
+    '''Plots the mean and standard deviation of multiverses'''
 
-    data = mv_data['mean_density']
+    if 'mean_density' in mv_data:
+        mv_data = mv_data['mean_density']
 
     # Calculate the mean over all dimensions except for the time
-    data.mean(dim=[d for d in data.dims if (d != 'time' or d!= 'p_lightning')])
+    mv_data.mean(dim=[d for d in mv_data.dims if d in mean_of])
 
-    for p_light in data['p_lightning']:
-        data_sel = data.sel(p_lightning=p_light)
+    for line in mv_data[lines_from]:
+        # Select the correct data that results in one line
+        data_sel = mv_data.sel(**{line.name:line})
 
-        hlpr.ax.plot(data_sel['time'], data_sel, 
-                    label=r'${}$'.format(p_light.values), 
-                    **plot_kwargs)
+        # Get the averaged data
+        mean = data_sel['mean']
+        if 'std' in data_sel:
+            std = data_sel['std']
+        else:
+            std = None
 
-        hlpr.provide_defaults('set_legend', title='{}'.format(p_light.name))
+        # Need to create a dictionary for the plot kwargs if none is given
+        if plot_kwargs is None:
+            plot_kwargs = {} 
+
+        # Get the values of the dimension from which to create the lines
+        plot_kwargs['label'] = '${}$'.format(line.values)
+
+        # Plot the data
+        hlpr.ax.errorbar(mean['time'], mean, yerr=std, **plot_kwargs)
+
+        # As default use the dimension name from which the lines originate
+        hlpr.provide_defaults('set_legend', title='{}'.format(line.name))
