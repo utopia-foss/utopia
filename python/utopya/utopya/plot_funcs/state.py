f83203578cb9e0a014debefc0b6481f169169615
Benjamin Herdeanu <herdeanu@iup.uni-heidelberg.de>
2019-04-04 17:18:21 +0200

    Generalize mean_and_std function for multiverse and universe data

--- python/utopya/utopya/plot_funcs/state.py
+++ python/utopya/utopya/plot_funcs/state.py
@@ -11,8 +11,6 @@ from utopya.plotting import is_plot_func, PlotHelper, MultiversePlotCreator
               # Provide some (static) default values for helpers
               helper_defaults=dict(
                 set_labels=dict(x="Time", y="Density"),
-                set_scale=dict(x='linear', y='log'),
-                set_title=dict(title='Mean tree density'),
                 set_legend=dict(use_legend=True),
                 save_figure=dict(bbox_inches="tight")
                 )
@@ -33,32 +31,43 @@ def mean_and_std(dm: DataManager, *,
         lines_from (str, optional): Plot multiple lines from this dimension
         plot_kwargs (dict, optional): Kwargs that are passed to plt.errorbar
     """
-    if 'mean_density' in mv_data:
-        mv_data = mv_data['mean_density']
+    # Need to create a dictionary for the plot kwargs if none is given
+    if plot_kwargs is None:
+        plot_kwargs = {} 
 
     # Calculate the mean over all dimensions except for the time
     mv_data.mean(dim=[d for d in mv_data.dims if d in mean_of])
 
-    for line in mv_data[lines_from]:
-        # Select the correct data that results in one line
-        data_sel = mv_data.sel(**{line.name:line})
+    # Case: Create multiple lines from a given dimension
+    if lines_from:
+        for line in mv_data[lines_from]:
+            # Select the correct data that results in one line
+            data_sel = mv_data.sel(**{line.name:line})
 
+            # Get the averaged data
+            mean = data_sel['mean']
+            if 'std' in data_sel:
+                std = data_sel['std']
+            else:
+                std = None
+
+            # Get the values of the dimension from which to create the lines
+            plot_kwargs['label'] = '${}$'.format(line.values)
+
+            # Plot the data
+            hlpr.ax.errorbar(mean['time'], mean, yerr=std, **plot_kwargs)
+
+            # As default use the dimension name from which the lines originate
+            hlpr.provide_defaults('set_legend', title='{}'.format(line.name))
+
+    # Case: Create a single line
+    else:
         # Get the averaged data
-        mean = data_sel['mean']
-        if 'std' in data_sel:
-            std = data_sel['std']
+        mean = mv_data['mean']
+        if 'std' in mv_data:
+            std = mv_data['std']
         else:
             std = None
 
-        # Need to create a dictionary for the plot kwargs if none is given
-        if plot_kwargs is None:
-            plot_kwargs = {} 
-
-        # Get the values of the dimension from which to create the lines
-        plot_kwargs['label'] = '${}$'.format(line.values)
-
         # Plot the data
         hlpr.ax.errorbar(mean['time'], mean, yerr=std, **plot_kwargs)
-
-        # As default use the dimension name from which the lines originate
-        hlpr.provide_defaults('set_legend', title='{}'.format(line.name))
