eb4ec1ac731f1dad0d8587245ec94043f7e816e5
Yunus Sevinchan <YunusSevinchan@gmail.com>
2019-04-05 21:49:12 +0200

    Improve time_series plots; add documentation

---
+++ doc/frontend/plot_funcs/distribution.rst
@@ -0,0 +1,12 @@
+Distributions
+=============
+
+.. automodule:: utopya.plot_funcs.distribution
+
+----
+
+Histogram
+---------
+
+.. autofunction:: utopya.plot_funcs.distribution.histogram
+
--- python/utopya/utopya/plot_funcs/time_series.py
+++ python/utopya/utopya/plot_funcs/time_series.py
@@ -1,4 +1,9 @@
-"""This module holds time-series plotting functions"""
+"""This module holds time-series plotting functions.
+
+These plotting functions have in common, that they represent some kind of time
+series. The time may be visualized on the x-axis or in some other way, e.g.
+via the color of scatter points.
+"""
 
 import logging
 from typing import Tuple, Union
@@ -8,6 +13,8 @@ import xarray as xr
 
 from utopya import DataManager, UniverseGroup
 from utopya.plotting import is_plot_func, PlotHelper, UniversePlotCreator
+
+from ._data_processing import create_mask
 
 # Get a logger
 log = logging.getLogger(__name__)
@@ -25,9 +32,25 @@ _density_hlpr_kwargs = dict(set_labels=dict(x="Time [Iteration Steps]",
               )
 def density(dm: DataManager, *, uni: UniverseGroup, hlpr: PlotHelper,
             model_name: str, path_to_data: str,
-            mean_of: Tuple[str], threshold: Union[float, None],
-            sizes_from: str=None, size_factor: float=1., **plot_kwargs):
+            mean_of: Tuple[str]=(),
+            apply_comparison: Tuple[str, float]=None,
+            sizes_from: str=None, size_factor: float=1.,
+            **plot_kwargs) -> None:
     """Plot the density of a mask, i.e. of a dataset holding booleans.
+    
+    This plotting function is useful when creating a plot from data that is
+    encoded in a binary fashion, e.g.: arrays containing True to denote the
+    existence of some entity or some value and False to denote its absence.
+    
+    If the dataset chosen via ``path_to_data`` is not already of boolean data
+    type, the ``apply_comparison`` argument is to be used to generate the
+    array-like boolean. By means of this argument, a binary operation of form
+    ``data <operator> rhs_value`` is carried out, which results in the desired
+    mask.
+
+    Another feature of this plotting function is that it can include another
+    data source to use for the sizes of the plots; in that case, a scatter plot
+    rather than a line plot is carried out.
     
     Args:
         dm (DataManager): The data manager from which to retrieve the data
@@ -37,42 +60,44 @@ def density(dm: DataManager, *, uni: UniverseGroup, hlpr: PlotHelper,
         model_name (str): The name of the model the data resides in, i.e. the
             base path within the UniverseGroup.
         path_to_data (str): Which data to use as the mask
-        mean_of (Tuple[str]): which data dimensions to calculate the density
-            over.
-        threshold (Union[float, None]): If not None, this threshold is applied
-            to the data. All values larger or equal to this value are regarded
-            as 1, all others as 0.
+        mean_of (Tuple[str], optional): which data dimensions to calculate the
+            density over.
+        apply_comparison (Tuple[str, float], optional): If given, this can be
+            used to convert the data into boolean form. It is expected to be
+            a 2-tuple (operator name, rhs value). For valid operator names, you
+            may want to enter an invalid one and check the error message. Hehe,
+            life hack!
         sizes_from (str, optional): If given, this is expected to be the path
             to a dataset that contains size values for a scatter plot. This
             leads to a scatter rather than a line-plot. The sizes are not used
             directly but are normalized by dividing with the maximum size; this
-            makes configuration via the `size_factor` parameter feasible.
+            makes configuration via the ``size_factor`` parameter feasible.
         size_factor (float, optional): The factor by which to scale the sizes
-            given in the `sizes_from` argument.
+            given in the ``sizes_from`` argument.
         **plot_kwargs: Passed on to plt.plot or plt.scatter
     
     Raises:
         ValueError: If the selected data is not a boolean mask. This error can
-            be alleviated by applying a threshold.
+            be alleviated by providing the ``apply_comparison`` argument.
     """
     # Get the data
     data = uni['data'][model_name][path_to_data]
 
-    # Apply a threshold to get the mask.
-    if threshold is not None:
-        data = (data >= threshold)
+    # Apply a mask, if configured
+    if apply_comparison:
+        data = create_mask(data, *apply_comparison)
 
-    # TODO Should have other kinds of comparisons here
-
-    # Now check if the data is binary
+    # Check if the data is binary now, which is required for all below
     if data.dtype is not np.dtype('bool'):
-        raise ValueError("Data at '{}' was not of boolean dtype but "
-                         "{}! Apply a threshold to the data using the "
-                         "`threshold` argument."
+        raise ValueError("Data at '{}' was of dtype {}, which needs to be "
+                         "converted to a binary mask for use in the "
+                         "density plot function. To do so, provide the "
+                         "`comp_operator` and `comp_value` arguments to "
+                         "the method; one or both of them were missing!"
                          "".format(data.path, data.dtype))
 
-    # Calculate the mean over the specified dimensions
-    density = data.mean(dim=mean_of)
+    # Calculate the mean over the specified dimensions, if specified to do so
+    density = data.mean(dim=mean_of) if mean_of else data
 
     # Associate time coordinates, if not already the case
     if 'time' not in density.coords:
@@ -99,7 +124,8 @@ def density(dm: DataManager, *, uni: UniverseGroup, hlpr: PlotHelper,
               )
 def densities(dm: DataManager, *, uni: UniverseGroup, hlpr: PlotHelper,
               model_name: str, to_plot: dict, **common_plot_kwargs):
-    """Like density, but for several specifications given by the `to_plot`.
+    """Like density, but for several specifications given by the ``to_plot``
+    argument.
 
     Args:
         dm (DataManager): The data manager from which to retrieve the data
@@ -109,13 +135,18 @@ def densities(dm: DataManager, *, uni: UniverseGroup, hlpr: PlotHelper,
         model_name (str): The name of the model the data resides in, i.e. the
             base path within the UniverseGroup.
         to_plot (dict): Which data to plot the densities of. The keys of this
-            dict are used as `path_to_data` for the `density` function. The
-            values are unpacked and passed to `density`
-        **common_plot_kwargs: Passed along to the `density` plot function for
+            dict are used as ``path_to_data`` for the ``density`` function. The
+            values are unpacked and passed to ``density``
+        **common_plot_kwargs: Passed along to the ``density`` plot function for
             all calls. Note that this may not contain any keys that are given
-            within `to_plot`!
+            within ``to_plot``!
     """
     for path_to_data, specs in to_plot.items():
+        if not isinstance(specs, dict):
+            log.warning("Parameters for `path_to_data` '%s' were not a dict "
+                        "but '%s'! Skipping this plot.", path_to_data, specs)
+            continue
+
         density(dm, uni=uni, hlpr=hlpr, model_name=model_name,
                 path_to_data=path_to_data, **specs, **common_plot_kwargs)
 
