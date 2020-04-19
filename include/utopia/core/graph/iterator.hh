9633ab215b0631b0e749682ce1d07669db16ea89
Yunus Sevinchan <yunussevinchan@gmail.com>
2020-04-19 19:04:15 +0200

    Let graph iterator tools support boost::subgraph
    Also extends the `graph_iterator_test` to use templated tests, which now
    cover more different graph types.

--- include/utopia/core/graph/iterator.hh
+++ include/utopia/core/graph/iterator.hh
@@ -2,6 +2,9 @@
 #define UTOPIA_CORE_GRAPH_ITERATORS_HH
 
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/adjacency_matrix.hpp>
+#include <boost/graph/subgraph.hpp>
+
 
 namespace Utopia {
 /**
@@ -11,7 +14,7 @@ namespace Utopia {
  */
 
 
-/// Over which graph entity to iterate 
+/// Over which graph entity to iterate
 enum class IterateOver {
     /// Iterate over vertices.
     vertices,
@@ -19,26 +22,26 @@ enum class IterateOver {
     /// Iterate over edges.
     edges,
 
-    /// Iterate over neighbors (adjacent_vertices). 
-    /** This iteration requires a vertex descriptor who's neighbors' to iterate 
+    /// Iterate over neighbors (adjacent_vertices).
+    /** This iteration requires a vertex descriptor who's neighbors' to iterate
      *  over.
      */
     neighbors,
 
-    /// Iterate inversely over neighbors (adjacent_vertices). 
-    /** This iteration requires a vertex descriptor who's neighbors' to iterate 
+    /// Iterate inversely over neighbors (adjacent_vertices).
+    /** This iteration requires a vertex descriptor who's neighbors' to iterate
      *  over.
      */
     inv_neighbors,
 
-    /// Iterate over the in edges of a vertex. 
-    /** This iteration requires a vertex descriptor who's neighbors' to iterate 
+    /// Iterate over the in edges of a vertex.
+    /** This iteration requires a vertex descriptor who's neighbors' to iterate
      *  over.
      */
     in_edges,
 
     /// Iterate over the out edges of a vertex.
-    /** This iteration requires a vertex descriptor who's neighbors' to iterate 
+    /** This iteration requires a vertex descriptor who's neighbors' to iterate
      *  over.
      */
     out_edges
@@ -49,15 +52,15 @@ namespace GraphUtils
 {
 
 /// Get an iterator pair over selected graph entities
-/** 
+/**
  * \tparam iterate_over Specify over which graph entities to iterate
- *                      Valid options: 
+ *                      Valid options:
  *                          - IterateOver::vertices
  *                          - IterateOver::edges
  * \tparam Graph        The graph type
- * 
+ *
  * \param g             The graph
- * 
+ *
  * \return decltype(auto) The iterator pair
  */
 template<IterateOver iterate_over, typename Graph>
@@ -81,20 +84,20 @@ decltype(auto) iterator_pair(const Graph& g){
 /** This function returns the iterator pair wrt. another graph entity.
  *  For example iteration over neighbors (adjacent_vertices) needs a references
  *  vertex.
- * 
+ *
  * \tparam iterate_over Specify over which graph entities to iterate over
- *                      Valid options: 
+ *                      Valid options:
  *                          - IterateOver::neighbors
  *                          - IterateOver::inv_neighbors
  *                          - IterateOver::in_edges
- *                          - IterateOver::OutEgdes
+ *                          - IterateOver::out_egdes
  * \tparam Graph        The graph type
- * \tparam EntityDesc   The graph entity descriptor that is the reference point 
+ * \tparam EntityDesc   The graph entity descriptor that is the reference point
  *                      for the iteration.
- * 
+ *
  * \param e             The graph entity that serves as reference
  * \param g             The graph
- * 
+ *
  * \return decltype(auto) The iterator pair
  */
 template<IterateOver iterate_over, typename Graph, typename EntityDesc>
@@ -126,15 +129,15 @@ decltype(auto) iterator_pair(EntityDesc e, const Graph& g){
 
 
 /// Get the iterator range over selected graph entities
-/** 
+/**
  * \tparam iterate_over Specify over which graph entities to iterate over
- *                      Valid options: 
+ *                      Valid options:
  *                          - IterateOver::vertices
  *                          - IterateOver::edges
  * \tparam Graph        The graph type
- * 
+ *
  * \param g             The graph
- * 
+ *
  * \return decltype(auto) The iterator range
  */
 template<IterateOver iterate_over, typename Graph>
@@ -147,22 +150,22 @@ decltype(auto) range(const Graph& g){
 /// Get the iterator range over selected graph entities
 /** This function returns the iterator range wrt. another graph entity.
  *  For example iterating of the neighbors (adjacent_vertices) of a vertex
- *  requires a vertex descriptor as reference. 
- * 
- * 
+ *  requires a vertex descriptor as reference.
+ *
+ *
  * \tparam iterate_over Specify over which graph entities to iterate over
- *                      Valid options: 
+ *                      Valid options:
  *                          - IterateOver::neighbors
  *                          - IterateOver::inv_neighbors
  *                          - IterateOver::in_edges
- *                          - IterateOver::OutEgdes
+ *                          - IterateOver::out_egdes
  * \tparam Graph        The graph type
- * \tparam EntityDesc   The graph entity descriptor that is the reference point 
+ * \tparam EntityDesc   The graph entity descriptor that is the reference point
  *                      for the iteration.
- * 
+ *
  * \param e             The graph entity that serves as reference
  * \param g             The graph
- * 
+ *
  * \return decltype(auto) The iterator range
  */
 template<IterateOver iterate_over, typename Graph, typename EntityDesc>
