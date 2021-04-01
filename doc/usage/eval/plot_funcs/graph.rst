403c8b8163dde88ee97d143c12e164eb0c6c6095
Jeremias Traub <jtraub@iup.uni-heidelberg.de>
2021-04-01 07:41:32 +0000

    Modularize and enhance graph plotting

--- doc/usage/eval/plot_funcs/graph.rst
+++ doc/usage/eval/plot_funcs/graph.rst
@@ -3,7 +3,7 @@ Graph Plots
 
 .. automodule:: utopya.plot_funcs.dag.graph
 
-For detailed descriptions of the networkx plot functions that are used here, have a look at the `networkx docs <https://networkx.github.io/documentation/stable/reference/drawing.html>`_.
+For detailed descriptions of the networkx plot functions that are used here, refer to the `networkx docs <https://networkx.github.io/documentation/stable/reference/drawing.html>`_.
 
 .. contents::
     :local:
@@ -21,21 +21,12 @@ The ``draw_graph`` plot function combines and extends the following four network
 * `plotting node-labels <https://networkx.github.io/documentation/stable/reference/generated/networkx.drawing.nx_pylab.draw_networkx_labels.html>`_
 * `plotting edge-labels <https://networkx.github.io/documentation/stable/reference/generated/networkx.drawing.nx_pylab.draw_networkx_edge_labels.html>`_
 
-It preserves the full configurability of the above functions while offering additional features. The ``nodes``, ``edges``, ``node_labels``, and ``edge_labels`` dictionaries in ``graph_cfg.graph_drawing`` are passed on to the respective networkx plot functions and may contain any argument accepted by the latter.
-
-.. note::
-
-    Graph animations are only possible for fixed node positions. When performing an animation, the node positions and colorbar(s) are fixed to the ones generated given the specified ``graph_cfg``.
-
-.. note::
-
-    When performing an animation, the values in ``graph_animation.times`` are shown as suptitle by default. As the actual times are only shown when using ``from_property`` or ``sel``, we recommend to use either of those for specifying the animation times.
-
+It preserves the full configurability of the above functions while offering additional features, e.g. plotting directly from a :py:class:`~utopya.datagroup.GraphGroup`, automatically mapping data to layout properties, or creating graph animations.
 
 Exemplary plot configuration
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-Check out an exemplary plot configuration for the ``CopyMeGraph`` model:
+Check out an exemplary single graph plot configuration for the ``CopyMeGraph`` model:
 
 .. literalinclude:: ../../../../python/utopya/test/cfg/plots/graph_plot_cfg.yml
     :language: yaml
@@ -43,12 +34,12 @@ Check out an exemplary plot configuration for the ``CopyMeGraph`` model:
     :end-before:  ### End ---- graph_plot_cfg
 
 
-:py:class:`~utopya.plot_funcs._mpl_helpers.ColorManager` configuration
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ColorManager configuration
+^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 With the :py:class:`~utopya.plot_funcs._mpl_helpers.ColorManager` integrated in ``draw_graph`` you can conveniently configure the colormap, norm, and colorbar. The examples below are meant to give an overview of its configuration possibilities. In the examples, the *node*-specific arguments are used. For configuring *edge*-colors, replace ``cmap``, ``vmin``, and ``vmax`` by ``edge_cmap``, ``edge_vmin``, and ``edge_vmax``. For more details, see :py:class:`~utopya.plot_funcs._mpl_helpers.ColorManager`.
 
-A customized discrete colormap (e.g., for visualizing categorical properties) can be created via the ``cmap.from_values`` argument. The matching colorbar labels can be defined via ``colorbar.labels``:
+A customized discrete colormap (e.g., for visualizing categorical properties) can be created via the ``cmap.from_values`` argument. The matching colorbar labels can be defined via ``colorbar.labels`` (If the categories are ``[0, 1, 2, ...]`` one could also use the shortcut syntax, see :py:class:`~utopya.plot_funcs._mpl_helpers.ColorManager`):
 
 .. literalinclude:: ../../../../python/utopya/test/cfg/plots/graph_plot_cfg.yml
     :language: yaml
@@ -70,7 +61,7 @@ For a nonlinear color-mapping, adjust the ``cmap_norm``:
     (e.g., *viridis*), use the
     `lut <https://matplotlib.org/api/cm_api.html#matplotlib.cm.get_cmap>`_
     argument to resample the colormap to have *lut* entries in the lookup table.
-    Set ``lut = BoundaryNorm.ncolors`` to use the full colormap range.
+    Set ``lut = <BoundaryNorm.ncolors>`` to use the full colormap range.
 
 
 .. autofunction:: utopya.plot_funcs.dag.graph.draw_graph
--- python/utopya/test/test_plotting.py
+++ python/utopya/test/test_plotting.py
@@ -1,5 +1,6 @@
 """Test the plotting module"""
 
+import builtins
 import os
 import sys
 import copy
@@ -7,6 +8,9 @@ import logging
 from pkg_resources import resource_filename
 
 import pytest
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import networkx as nx
 
 import paramspace as psp
 from dantro.plot_mngr import PlotCreatorError
@@ -16,6 +20,7 @@ from utopya import Multiverse
 from utopya.yaml import load_yml
 from utopya.testtools import ModelTest
 from utopya._path_setup import temporary_sys_path as tmp_sys_path
+from utopya.plot_funcs._graph import GraphPlot
 
 
 # Get the test resources ......................................................
@@ -40,6 +45,8 @@ BIFURCATION_DIAGRAM_2D_PLOTS = resource_filename('test',
 GRAPH_RUN = resource_filename('test', 'cfg/graphgroup_cfg.yml')
 
 GRAPH_PLOTS = resource_filename('test', 'cfg/plots/graph_plot_cfg.yml')
+
+GRAPH_PLOT_CLS = resource_filename('test', 'cfg/graphplot_class_cfg.yml')
 
 # Fixtures --------------------------------------------------------------------
 
@@ -360,76 +367,174 @@ def test_graph_plots(tmpdir):
             "custom_node_positioning_model",
             "explicit_node_positions",
             "custom_graph_creation",
+            "custom_graph_arr_creation",
         ),
     )
 
     # Animation plots
-    mv.pm.plot_from_cfg(plots_cfg=GRAPH_PLOTS,
-                        plot_only=["graph_anim1", "graph_anim2", "graph_anim3",
-                                   "graph_anim_external"])
+    mv.pm.plot_from_cfg(
+        plots_cfg=GRAPH_PLOTS,
+        plot_only=[
+            "graph_anim1",
+            "graph_anim2",
+            "graph_anim3",
+            "graph_anim_external",
+            "graph_anim4",
+            "graph_anim_custom_graph_creation",        ],
+    )
 
-    # Test failing cases
-    # Try using a graphviz node layout, which requires pydot
-    try:
-        import pydot
-    except ImportError:
-        with pytest.raises(PlotCreatorError, match="No module named 'pydot'"):
-            mv.pm.plot_from_cfg(plots_cfg=GRAPH_PLOTS, plot_only=["Graphviz"])
-    else:
-        mv.pm.plot_from_cfg(plots_cfg=GRAPH_PLOTS, plot_only=["Graphviz"])
-
-    # Try plotting a colorbar for directed edges with arrows=True
-    with pytest.warns(UserWarning, match="No colorbar can be shown"):
-        mv.pm.plot_from_cfg(plots_cfg=GRAPH_PLOTS,
-                            plot_only=["colorbar_for_directed_edges"])
-
-    # Try using the BoundaryNorm for directed edges with arrows=True
-    with pytest.raises(PlotCreatorError, match="only the "
-                                "matplotlib.colors.Normalize base class is "
-                                "supported."):
-        mv.pm.plot_from_cfg(plots_cfg=GRAPH_PLOTS,
-                            plot_only=["BoundaryNorm_for_directed_edges"])
-
-    # Passing the 'labels' key for edge labels
-    with pytest.raises(PlotCreatorError, match="Received 'labels' key"):
-        mv.pm.plot_from_cfg(plots_cfg=GRAPH_PLOTS,
-                            plot_only=["wrong_edge_label_kwarg"])
-
+    # Test failing cases – if possible these test are done in the (faster)
+    # GraphPlot-class test.
     # Providing invalid dag tag for external property
-    with pytest.raises(PlotCreatorError,
-                       match="No tag 'some_state_transformed' found in the "
-                             "data selected by the DAG!"):
-        mv.pm.plot_from_cfg(plots_cfg=GRAPH_PLOTS,
-                            plot_only=["invalid_ext_prop"])
-
-    # Providing invalid interval for property rescaling
-    with pytest.raises(PlotCreatorError, match="'interval' must be a 2-tuple"):
-        mv.pm.plot_from_cfg(plots_cfg=GRAPH_PLOTS,
-                            plot_only=["invalid_interval"])
-
-    # Trying to map to an invalid layout property
     with pytest.raises(
-        PlotCreatorError, match="can not be mapped to the 'invalid_entry'"
+        PlotCreatorError,
+        match=(
+            "No tag 'some_state_transformed' found in the data selected by "
+            "the DAG!"
+        ),
     ):
-        mv.pm.plot_from_cfg(plots_cfg=GRAPH_PLOTS,
-                            plot_only=["invalid_layout_property"])
-
-    # Invalid time specification for animation
-    with pytest.raises(
-        PlotCreatorError, match="invalid specifications for the animation time"
-    ):
-        mv.pm.plot_from_cfg(plots_cfg=GRAPH_PLOTS,
-                            plot_only=["anim_invalid_time_spec"])
+        mv.pm.plot_from_cfg(
+            plots_cfg=GRAPH_PLOTS, plot_only=["invalid_ext_prop"]
+        )
 
     # Ambiguous time specifications for animation
     with pytest.raises(
-        PlotCreatorError, match="ambiguous time specifications"
+        PlotCreatorError, match="ambiguous animation time specifications"
     ):
-        mv.pm.plot_from_cfg(plots_cfg=GRAPH_PLOTS,
-                            plot_only=["anim_amgiguous_time_spec"])
+        mv.pm.plot_from_cfg(
+            plots_cfg=GRAPH_PLOTS, plot_only=["anim_amgiguous_time_spec"]
+        )
 
-    # Missing time specification for animation
-    with pytest.raises(PlotCreatorError, match="Missing time specifications"):
-        mv.pm.plot_from_cfg(plots_cfg=GRAPH_PLOTS,
-                            plot_only=["anim_missing_time_spec"])
+    # Trying to animate from single nx.Graph
+    with pytest.raises(
+        PlotCreatorError, match="due to invalid type of the 'graph'"
+    ):
+        mv.pm.plot_from_cfg(
+            plots_cfg=GRAPH_PLOTS, plot_only=["anim_not_dataarray"]
+        )
 
+
+def test_graph_plot_class():
+    """Tests the plot_funcs._graph module.
+
+    Mainly tests basic functionality, class attribute management and
+    configuration parsing. Plotting of more complex graph data (using features
+    like property-mapping) is tested in test_plotting.test_graph_plots.
+    """
+    graph = nx.complete_graph(3, create_using=nx.Graph)
+    digraph = nx.complete_graph(3, create_using=nx.DiGraph)
+
+    # Initialize GraphPlot with defaults
+    gp = GraphPlot(graph)
+
+    # Check that all nodes and edges are selected for drawing
+    assert gp._nodes_to_draw == [0, 1, 2]
+    assert len(gp._edges_to_draw) == 3
+    assert gp._nodes_to_shrink == []
+
+    # Check properties
+    assert isinstance(gp.g, nx.Graph)
+    assert gp.g is not gp._g
+
+    # Draw nodes, edges, and default labels
+    gp.draw(node_labels=dict(enabled=True), edge_labels=dict(enabled=True))
+    # Draw again, re-creating the colormanagers
+    gp.draw(nodes=dict(vmin=0.), edges=dict(edge_vmin=0.))
+
+    assert isinstance(gp._mpl_nodes, mpl.collections.PathCollection)
+    assert isinstance(gp._mpl_edges, mpl.collections.LineCollection)
+    assert isinstance(gp._mpl_node_labels, dict)
+    assert isinstance(gp._mpl_edge_labels, dict)
+    assert gp._mpl_node_cbar is None
+    assert gp._mpl_edge_cbar is None
+
+    # Check that the label drawing kwargs are not set permanently
+    assert gp._node_label_kwargs == {}
+    assert gp._edge_label_kwargs == {}
+
+    # Add colorbars
+    gp.add_colorbars()
+    # Doing this twice should be ok and should remove the first colorbars
+    gp.add_colorbars()
+    
+    assert isinstance(gp._mpl_node_cbar, mpl.colorbar.Colorbar)
+    assert isinstance(gp._mpl_edge_cbar, mpl.colorbar.Colorbar)
+
+    # Clear plot
+    gp.clear_plot()
+    # Doing this twice should be ok
+    gp.clear_plot()
+
+    # Done with testing the drawing on this figure.
+    plt.close(gp.fig)
+
+    # Test the subgraph selection
+    # Select two nodes to draw, remove 1 node and 2 edges from the graph
+    gp = GraphPlot(graph, select=dict(nodelist=[0, 1]))
+    assert gp._g.number_of_nodes() == 2
+    assert gp._g.number_of_edges() == 1
+    assert gp._nodes_to_draw == [0, 1]
+    assert len(gp._edges_to_draw) == 1
+    assert gp._nodes_to_shrink == []
+
+    # Select two nodes to draw, but don't remove anything from the graph
+    gp = GraphPlot(graph, select=dict(nodelist=[0, 1], drop=False))
+    assert gp._g.number_of_nodes() == 3
+    assert gp._g.number_of_edges() == 3
+    assert gp._nodes_to_draw == [0, 1]
+    assert len(gp._edges_to_draw) == 1
+    assert gp._nodes_to_shrink == []
+
+    # With open_edges, the non-selected node is still drawn but shrinked
+    gp = GraphPlot(graph, select=dict(nodelist=[0, 1], open_edges=True))
+    assert gp._g.number_of_nodes() == 3
+    assert gp._g.number_of_edges() == 3
+    assert gp._nodes_to_draw == [0, 1, 2]
+    assert len(gp._edges_to_draw) == 3
+    assert gp._nodes_to_shrink == [2]
+
+    # Now go through configurations and test initialization and drawing
+    configs = load_yml(GRAPH_PLOT_CLS)
+
+    for name, cfg in configs.items():
+        fig = plt.figure()
+
+        # Try using a graphviz node layout, which requires pydot
+        if name == "graphviz":
+            try:
+                import pydot
+            except ImportError:
+                with pytest.raises(
+                    ImportError, match="No module named 'pydot'"
+                ):
+                    gp = GraphPlot(digraph, fig=fig, **cfg)
+
+                continue
+
+        # Configurations for which an Error is raised
+        if "_raises" in cfg:
+            exc_type = getattr(builtins, cfg.pop("_raises"))
+            match = cfg.pop("_match")
+
+            with pytest.raises(exc_type, match=match):
+                gp = GraphPlot(digraph, fig=fig, **cfg)
+                gp.draw()
+                gp.clear_plot()
+
+        # Configurations that lead to a warning
+        elif "_warns" in cfg:
+            warn_type = getattr(builtins, cfg.pop("_warns"))
+            match = cfg.pop("_match")
+
+            with pytest.warns(warn_type, match=match):
+                gp = GraphPlot(digraph, fig=fig, **cfg)
+                gp.draw()
+                gp.clear_plot()
+
+        # These should work fine
+        else:
+            gp = GraphPlot(digraph, fig=fig, **cfg)
+            gp.draw()
+            gp.clear_plot()
+
+        plt.close(fig)
