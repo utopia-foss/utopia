9e5f0c161b4d37c7de90834f22b4661d07634590
Yunus Sevinchan <yunussevinchan@gmail.com>
2021-10-05 13:34:30 +0200

    Remove python_install_package_remote cmake function
    No longer needed.

--- cmake/modules/UtopiaPython.cmake
+++ cmake/modules/UtopiaPython.cmake
@@ -98,62 +98,6 @@ endfunction()
 
 
 
-
-# Install a python package located in a remote git repository
-#
-# .. cmake_function:: python_install_package_remote
-#       
-#       .. cmake_param:: URL
-#          :required:
-#          :single:
-#
-#          (HTTPS) URL to the git repository. May be appended with '@'
-#          and a brach or tag identifier.
-#
-#       .. cmake_param:: TRUSTED_HOST
-#          :optional:
-#          :single:
-#
-#          URL of the respective trusted host.
-#
-#   Install a remote python package located in a git repository
-#   into the Utopia venv.
-#
-function(python_install_package_remote)
-    # parse function arguments
-    set(OPTION)
-    set(SINGLE URL TRUSTED_HOST)
-    set(MULTI)
-    include(CMakeParseArguments)
-    cmake_parse_arguments(RINST "${OPTION}" "${SINGLE}" "${MULTI}" "${ARGN}")
-    if(RINST_UNPARSED_ARGUMENTS)
-        message(WARNING "Encountered unparsed arguments in
-            python_install_package_remote!")
-    endif()
-
-    message(STATUS "Installing package at ${RINST_URL} into the virtualenv")
-
-    # set 'git' in front of URL
-    set(RINST_FULL_PATH git+${RINST_URL})
-
-    # add trusted-host command
-    set(TRUSTED_HOST_CMD "")
-    if(RINST_TRUSTED_HOST)
-        set(TRUSTED_HOST_CMD --trusted-host ${RINST_TRUSTED_HOST})
-    endif()
-
-    # execute command
-    set(INSTALL_CMD install ${TRUSTED_HOST_CMD} --upgrade
-        ${RINST_FULL_PATH})
-    execute_process(COMMAND ${UTOPIA_ENV_PIP} ${INSTALL_CMD}
-        RESULT_VARIABLE RETURN_VALUE
-        OUTPUT_QUIET
-    )
-    if (NOT RETURN_VALUE EQUAL "0")
-        message(SEND_ERROR "Error installing remote package: ${RETURN_VALUE}")
-    endif ()
-endfunction()
-
 # Install a local Python package into the Utopia virtual environment
 #
 # This function takes the following arguments:
