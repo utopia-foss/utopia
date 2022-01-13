528c7ee00c49fc7d595c65bf366581ec0e696544
Yunus Sevinchan <yunussevinchan@gmail.com>
2022-01-13 12:20:20 +0100

    Add flocking python test

--- python/model_tests/SimpleFlocking/test_output.py
+++ python/model_tests/SimpleFlocking/test_output.py
@@ -7,7 +7,7 @@ import pytest
 from utopya.testtools import ModelTest
 
 # Configure the ModelTest class
-mtc = ModelTest("SimpleFlocking", test_file=__file__)  # TODO set the model name
+mtc = ModelTest("SimpleFlocking", test_file=__file__)
 
 
 # Fixtures --------------------------------------------------------------------
@@ -28,6 +28,31 @@ def test_that_it_runs():
     mv.dm.load_from_cfg(print_tree=True)
     # The `print_tree` flag creates output of which data was loaded
 
+    # Test that some data was written
+    for uni in mv.dm["multiverse"].values():
+        data = uni["data/SimpleFlocking"]
+
+        assert "agent/orientation" in data
+        assert "agent/x" in data
+        assert "agent/y" in data
+        assert "norm_group_velocity" in data
+        assert "orientation_circmean" in data
+        assert "orientation_circstd" in data
+
+        # Output orientation is in radians and expected range
+        assert (data["agent/orientation"] > -np.pi).all()
+        assert (data["agent/orientation"] < +np.pi).all()
+        assert (data["orientation_circmean"] > -np.pi).all()
+        assert (data["orientation_circmean"] < +np.pi).all()
+
+        # Agent position is non-negative
+        assert (data["agent/x"] >= 0).all()
+        assert (data["agent/y"] >= 0).all()
+
+        # Circular standard deviation and group velocity is positive
+        assert (data["orientation_circstd"] >= 0).all()
+        assert (data["norm_group_velocity"] >= 0).all()
+
 
 def test_run_and_eval_cfgs():
     """Carries out all additional configurations that were specified alongside
@@ -42,10 +67,10 @@ def test_run_and_eval_cfgs():
     See :py:meth:`~utopya.model.Model.run_and_eval_cfg_paths` for more info.
     """
     for cfg_name, cfg_paths in mtc.default_config_sets.items():
-        print("\nRunning '{}' example ...".format(cfg_name))
+        print(f"\nRunning '{cfg_name}' example ...")
 
         mv, _ = mtc.create_run_load(from_cfg=cfg_paths.get('run'),
                                     parameter_space=dict(num_steps=3))
         mv.pm.plot_from_cfg(plots_cfg=cfg_paths.get('eval'))
 
-        print("Succeeded running and evaluating '{}'.\n".format(cfg_name))
+        print(f"Succeeded running and evaluating '{cfg_name}'.\n")
