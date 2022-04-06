3772652cd8ebcd418314c5a51566a658a61beb38
Yunus Sevinchan <yunussevinchan@gmail.com>
2022-04-06 16:21:29 +0200

    Remove utopya package from repo
    From this point onwards, the outsourced utopya package will be used.
    
    For contribution history, see https://gitlab.com/utopia-project/utopya

--- python/utopya/utopya/_yaml.py
+++
@@ -1,7 +0,0 @@
-"""Supplies basic YAML interface, inherited from dantro and paramspace"""
-
-from dantro.tools import yaml, load_yml, write_yml
-import paramspace
-
-# Make sure the yaml instances are really shared
-assert yaml is paramspace.yaml
--- python/utopya/utopya/model_registry/_exceptions.py
+++
@@ -1,7 +0,0 @@
-"""All model registry related error types"""
-
-class ModelRegistryError(ValueError):
-    """Raised on errors with model registry"""
-
-class BundleExistsError(ModelRegistryError):
-    """Raised when a bundle that compared equal already exists."""
